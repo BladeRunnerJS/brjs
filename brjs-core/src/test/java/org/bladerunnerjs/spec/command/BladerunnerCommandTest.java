0b525c28b6f99ebd4f3d7dcb969a34609a7f8a15
ThanhC <thanh.cam@caplin.com>
2013-10-15 22:13:22 +0100

    updating bladerunnercommand spec test now that the command has been moved into brjs-core

--- brjs-core/src/test/java/org/bladerunnerjs/spec/command/BladerunnerCommandTest.java
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/command/BladerunnerCommandTest.java
@@ -1,18 +1,53 @@
 package org.bladerunnerjs.spec.command;
 
+import org.bladerunnerjs.core.plugin.command.standard.BladerunnerCommand;
+import org.bladerunnerjs.model.appserver.ApplicationServer;
+import org.bladerunnerjs.model.exception.command.ArgumentParsingException;
+import org.bladerunnerjs.model.exception.command.CommandArgumentsException;
 import org.bladerunnerjs.specutil.engine.SpecTest;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-
 
 public class BladerunnerCommandTest extends SpecTest
 {
-
-	@Test
-	public void commandIsAutomaticallyLoaded() throws Exception
-	{
-		given(brjs).hasBeenAuthenticallyCreated();
-		when(brjs).runCommand("help", "start");
-		then(exceptions).verifyNoOutstandingExceptions();
-	}
+	ApplicationServer appServer;
 	
+	@Before
+	public void initTestObjects() throws Exception
+	{
+		//TODO:: make test work with a random appServerPort instead of 7070
+		appServerPort = 7070;
+		
+		given(pluginLocator).hasCommand(new BladerunnerCommand())
+			.and(brjs).hasBeenCreated();	
+		appServer = brjs.applicationServer(appServerPort);
+	}
+	@Ignore
+	@Test
+	public void exceptionIsThrownIfThereAreTooManyArguments() throws Exception {
+		when(brjs).runCommand("start", "a");
+		then(exceptions).verifyException(ArgumentParsingException.class, unquoted("Unexpected argument: a"))
+			.whereTopLevelExceptionIs(CommandArgumentsException.class);
+	}
+	@Ignore
+	@Test
+	public void exceptionIsThrownIfAppServerAlreadyStarted() throws Exception
+	{
+		given(appServer).started();
+		when(brjs).runCommand("start");
+//		then(exceptions).verifyException(PortAlreadyInUseException.class, unquoted("'7070'"));
+	}
+
+	@Ignore
+	@Test
+	public void bladerunnerCommandStartsAppServer() throws Exception
+	{
+		when(brjs).runCommand("start");
+		then(output).containsText(
+				"Bladerunner server is now running and can be accessed at http://localhost:7070/",
+				"Press Ctrl + C to stop the server")
+			.and(appServer).requestIsRedirected("/","/dashboard");
+	}
+
 }
