69e2a775e63d14bd8b3137a4d86d3a321191fb57
Andy Berry <andyberry88@gmail.com>
2013-10-17 13:31:39 +0100

    cleaning up the use of the build version calculator and making it a static utility

--- buildSrc/src/main/groovy/org/bladerunnerjs/BuildVersionCalculator.groovy
+++ buildSrc/src/main/groovy/org/bladerunnerjs/BuildVersionCalculator.groovy
@@ -9,48 +9,13 @@ import java.util.Calendar;
 
 class BuildVersionCalculator
 {
-	
-	Project project
-	
-	BuildVersionCalculator(Project project)
-	{
-		this.project = project;
-	}
-	
-	//TODO: make this a static util class and stop passing in the project obj since we wont need it in Git land
-	String calculateMajorVersion()
-	{
-		def branchDir = (project.parent != null) ? project.parent : project;
-		return "WIP"//branchDir.getName()
-	}
-	
-	String calculateMinorVersion()
-	{
-		def envP4Changelist = System.getenv()['GO_REVISION_P4_CHANGELIST']
-		def changelist = ''
-		def minorVersion
-		if (envP4Changelist == null) {
-			minorVersion = 'dev'
-		} else {
-			minorVersion = envP4Changelist
-		}
-		
-		return minorVersion
-	}
 
-	String calculateVersion()
+	static String calculateVersion(Project p)
 	{
-		def majorVersion = calculateMajorVersion()
-		def minorVersion = calculateMinorVersion()
-		
-		return majorVersion + ((minorVersion != '') ? '-'+minorVersion : '')
+		return "WIP"
 	}
 	
-	/* buildDate is the date, buildTimestamp is the current timestamp
-	 * for a dev build the buildDate is used for the timestamp to prevent Gradle
-	 * from running the generateVersionFile task on every build
-	 */
-	String calculateBuildDate()
+	static String calculateBuildDate()
 	{
 		DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy")
 		DateFormat timestampFormat = new SimpleDateFormat("dd MMMM yyyy HH:mm zz")
@@ -58,20 +23,6 @@ class BuildVersionCalculator
 		def buildDate = dateFormat.format(cal.getTime())
 	
 		return buildDate;
-	}
-	
-	String calculateBuildTimestamp()
-	{
-		if (calculateMinorVersion().endsWith("dev"))
-		{
-			Calendar cal = Calendar.getInstance()
-			DateFormat timestampFormat = new SimpleDateFormat("dd MMMM yyyy HH:mm zz")
-			return timestampFormat.format(cal.getTime())
-		}
-		else
-		{
-			calculateBuildDate()
-		}
 	}
 	
 }
