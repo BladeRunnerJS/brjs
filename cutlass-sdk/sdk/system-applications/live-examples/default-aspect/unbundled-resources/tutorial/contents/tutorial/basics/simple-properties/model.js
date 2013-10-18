741530640bbc680a95f4df687c67f411e5529f08
kybernetikos <kybernetikos@gmail.com>
2013-10-18 10:43:33 +0100

    my deletes

--- cutlass-sdk/sdk/system-applications/live-examples/default-aspect/unbundled-resources/tutorial/contents/tutorial/basics/simple-properties/model.js
+++
@@ -1,5 +0,0 @@
-novobank.example.DemoPresentationModel = function()
-{
-	this.amount = new caplin.presenter.property.Property(10000);
-};
-caplin.implement(novobank.example.DemoPresentationModel, caplin.presenter.PresentationModel);
--- cutlass-sdk/sdk/system-applications/live-examples/default-aspect/unbundled-resources/tutorial/contents/tutorial/subtemplates/simple-subtemplates/model.js
+++
@@ -1,5 +0,0 @@
-novobank.example.DemoPresentationModel = function()
-{
-	this.amount = new caplin.presenter.node.Field("10000");
-};
-caplin.extend(novobank.example.DemoPresentationModel, caplin.presenter.PresentationModel);
