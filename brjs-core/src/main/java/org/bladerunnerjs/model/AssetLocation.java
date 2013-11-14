3bfb8d0b868977d821bf917a4b1afc6ac6e5cbdd
Andy Berry <andyberry88@gmail.com>
2013-11-14 13:32:44 +0000

    starting to add some very basic JavaDoc to classes

--- brjs-core/src/main/java/org/bladerunnerjs/model/AssetLocation.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/AssetLocation.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import org.bladerunnerjs.model.file.AliasDefinitionsFile;
 
-
+/**
+ * Represents the parent directory of any AssetFile, so in the example src/a/b/c/someFile.xml it would represent the src/a/b/c directory.
+ * Due to legacy resources the AssetLocation for any file in resources is always the resources directory. So in the example resources/a/b/c/someFile.xml, resouces is the AssetLocation.
+ *
+ */
 public interface AssetLocation {
 	File dir();
 	AliasDefinitionsFile aliasDefinitions();
