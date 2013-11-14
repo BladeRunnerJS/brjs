9f777b1cf64d8eedab2b8580493813adb00728ff
dominicc <dominicc@dominicc-work-laptop.caplin.com>
2013-11-14 09:41:21 +0000

    Split out a ServletPlugin interface from BundlerPlugin, based on feedback from <https://github.com/BladeRunnerJS/brjs/issues/241>.

--- brjs-core/src/main/java/org/bladerunnerjs/core/plugin/bundler/BundlerPlugin.java
+++ brjs-core/src/main/java/org/bladerunnerjs/core/plugin/bundler/BundlerPlugin.java
@@ -1,21 +1,15 @@
 package org.bladerunnerjs.core.plugin.bundler;
 
-import java.io.OutputStream;
 import java.util.List;
 
 import org.bladerunnerjs.core.plugin.bundlesource.FileSetFactory;
 import org.bladerunnerjs.model.BundleSet;
-import org.bladerunnerjs.model.ParsedRequest;
-import org.bladerunnerjs.model.RequestParser;
 import org.bladerunnerjs.model.TagHandlerPlugin;
 import org.bladerunnerjs.model.exception.request.BundlerProcessingException;
 
 
-public interface BundlerPlugin extends TagHandlerPlugin {
-	String getMimeType();
+public interface BundlerPlugin extends TagHandlerPlugin, ServletPlugin {
 	FileSetFactory getFileSetFactory();
-	RequestParser getRequestParser();
 	List<String> generateRequiredDevRequestPaths(BundleSet bundleSet, String locale) throws BundlerProcessingException;
 	List<String> generateRequiredProdRequestPaths(BundleSet bundleSet, String locale) throws BundlerProcessingException;
-	void handleRequest(ParsedRequest request, BundleSet bundleSet, OutputStream os) throws BundlerProcessingException;
 }
---
+++ brjs-core/src/main/java/org/bladerunnerjs/core/plugin/bundler/ServletPlugin.java
@@ -0,0 +1,15 @@
+package org.bladerunnerjs.core.plugin.bundler;
+
+import java.io.OutputStream;
+
+import org.bladerunnerjs.core.plugin.Plugin;
+import org.bladerunnerjs.model.BundleSet;
+import org.bladerunnerjs.model.ParsedRequest;
+import org.bladerunnerjs.model.RequestParser;
+import org.bladerunnerjs.model.exception.request.BundlerProcessingException;
+
+public interface ServletPlugin extends Plugin {
+	String getMimeType();
+	RequestParser getRequestParser();
+	void handleRequest(ParsedRequest request, BundleSet bundleSet, OutputStream os) throws BundlerProcessingException;
+}
