24a623719b25c18f0c600b57c11f6cdd59d01de1
dominicc <dominicc@dominicc-work-laptop.caplin.com>
2013-11-20 16:46:57 +0000

    Updated aliasing to use the file modified checker to avoid re-parsing files unnecessarily -- hope fully this is the last change before bringing some of the existing code across.

--- brjs-core/src/main/java/org/bladerunnerjs/model/aliasing/AliasDefinitionsFile.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/aliasing/AliasDefinitionsFile.java
@@ -4,20 +4,44 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bladerunnerjs.model.utility.FileModifiedChecker;
+
 public class AliasDefinitionsFile extends File {
 	private static final long serialVersionUID = 822434477840572747L;
+	private final FileModifiedChecker fileModifiedChecker;
+	private List<AliasDefinition> aliasDefinitions;
 	
 	public AliasDefinitionsFile(File parent, String child) {
 		super(parent, child);
+		fileModifiedChecker = new FileModifiedChecker(this);
 	}
 	
 	public AliasDefinition getAlias(AliasName aliasName, String scenarioName, List<String> groupNames) {
-		// TODO Auto-generated method stub
-		return null;
+		AliasDefinition aliasDefinition = null;
+		
+		for(AliasDefinition nextAliasDefinition : aliasDefinitions()) {
+			String groupName = nextAliasDefinition.getGroup();
+			
+			if(nextAliasDefinition.getScenario().equals(scenarioName) && ((groupName == null) || groupNames.contains(groupName)) && nextAliasDefinition.getName().equals(aliasName.getName())) {
+				aliasDefinition = nextAliasDefinition;
+				break;
+			}
+		}
+		
+		return aliasDefinition;
 	}
 	
 	public List<AliasDefinition> aliasDefinitions() {
+		if(fileModifiedChecker.fileModifiedSinceLastCheck()) {
+			reparseFile();
+		}
+		
+		return aliasDefinitions;
+	}
+	
+	private void reparseFile() {
+		aliasDefinitions = new ArrayList<>();
+		
 		// TODO: bring aliasing code over from the 'bundlers' project
-		return new ArrayList<>();
 	}
 }
--- brjs-core/src/main/java/org/bladerunnerjs/model/aliasing/AliasesFile.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/aliasing/AliasesFile.java
@@ -4,25 +4,52 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bladerunnerjs.model.utility.FileModifiedChecker;
+
 public class AliasesFile extends File {
 	private static final long serialVersionUID = -3607791607132062852L;
+	private final FileModifiedChecker fileModifiedChecker;
+	private List<AliasDefinition> aliasDefinitions;
+	private List<String> groupNames;
 	
 	public AliasesFile(File parent, String child) {
 		super(parent, child);
+		fileModifiedChecker = new FileModifiedChecker(this);
 	}
 	
 	public AliasDefinition getAlias(AliasName aliasName, String scenarioName) {
-		// TODO Auto-generated method stub
-		return null;
+		AliasDefinition aliasDefinition = null;
+		
+		for(AliasDefinition nextAliasDefinition : aliasDefinitions()) {
+			if(nextAliasDefinition.getScenario().equals(scenarioName) && nextAliasDefinition.getName().equals(aliasName.getName())) {
+				aliasDefinition = nextAliasDefinition;
+				break;
+			}
+		}
+		
+		return aliasDefinition;
 	}
 	
 	public List<String> groupNames() {
-		// TODO Auto-generated method stub
-		return null;
+		if(fileModifiedChecker.fileModifiedSinceLastCheck()) {
+			reparseFile();
+		}
+		
+		return groupNames;
 	}
 	
 	public List<AliasDefinition> aliasDefinitions() {
+		if(fileModifiedChecker.fileModifiedSinceLastCheck()) {
+			reparseFile();
+		}
+		
+		return aliasDefinitions;
+	}
+	
+	private void reparseFile() {
+		aliasDefinitions = new ArrayList<>();
+		groupNames = new ArrayList<>();
+		
 		// TODO: bring aliasing code over from the 'bundlers' project
-		return new ArrayList<>();
 	}
 }
