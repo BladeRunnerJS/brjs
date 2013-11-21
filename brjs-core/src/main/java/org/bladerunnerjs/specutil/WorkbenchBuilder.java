8177e78b097d560f2ce18f5c66bfb8a8a9270da9
Priyank Vyas <priyank.vyas@caplin.com>
2013-11-21 17:16:02 +0000

    Updating WorkbenchBuilder and WorkbenchCommander (similar to Aspect versions now)

--- brjs-core/src/main/java/org/bladerunnerjs/specutil/WorkbenchBuilder.java
+++ brjs-core/src/main/java/org/bladerunnerjs/specutil/WorkbenchBuilder.java
@@ -1,14 +1,43 @@
 package org.bladerunnerjs.specutil;
 
+import org.apache.commons.io.FileUtils;
+import org.bladerunnerjs.model.Aspect;
 import org.bladerunnerjs.model.Workbench;
 import org.bladerunnerjs.specutil.engine.AssetContainerBuilder;
+import org.bladerunnerjs.specutil.engine.BuilderChainer;
 import org.bladerunnerjs.specutil.engine.SpecTest;
 
 public class WorkbenchBuilder extends AssetContainerBuilder<Workbench> {
-	public Workbench workbench;
+	private final Workbench workbench;
 	
-	public WorkbenchBuilder(SpecTest modelTest, Workbench workbench) {
-		super(modelTest,  workbench);
+	public WorkbenchBuilder(SpecTest modelTest, Workbench workbench)
+	{
+		super(modelTest, workbench);
 		this.workbench = workbench;
+	}
+	
+	public BuilderChainer indexPageRefersTo(String className) throws Exception {
+		FileUtils.write(workbench.file("index.html"), className);
+		
+		return builderChainer;
+	}
+	
+	public BuilderChainer resourceFileRefersTo(String resourceFileName, String className) throws Exception {
+		FileUtils.write(workbench.resources().file(resourceFileName), "<root refs='" + className + "'/>");
+		
+		return builderChainer;
+	}
+	
+	public BuilderChainer hasAlias(String aliasName, String classRef) throws Exception {
+		FileUtils.write(workbench.aliasesFile(),
+			"<aliases xmlns='http://schema.caplin.com/CaplinTrader/aliases'>" +
+			"	<alias name='" + aliasName + "' class='" + classRef + "'/>" +
+			"</aliases>");
+		
+		return builderChainer;
+	}
+	
+	public BuilderChainer indexPageHasContent(String content) throws Exception {
+		return indexPageRefersTo(content);
 	}
 }
--- brjs-core/src/main/java/org/bladerunnerjs/specutil/WorkbenchCommander.java
+++ brjs-core/src/main/java/org/bladerunnerjs/specutil/WorkbenchCommander.java
@@ -1,6 +1,18 @@
 package org.bladerunnerjs.specutil;
 
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.apache.commons.io.FileUtils;
+import org.bladerunnerjs.model.Aspect;
+import org.bladerunnerjs.model.Mode;
 import org.bladerunnerjs.model.Workbench;
+import org.bladerunnerjs.model.exception.ConfigException;
+import org.bladerunnerjs.model.exception.ModelOperationException;
+import org.bladerunnerjs.model.utility.IndexPageWriter;
+import org.bladerunnerjs.specutil.engine.CommanderChainer;
 import org.bladerunnerjs.specutil.engine.NodeCommander;
 import org.bladerunnerjs.specutil.engine.SpecTest;
 
@@ -11,5 +23,24 @@ public class WorkbenchCommander extends NodeCommander<Workbench> {
 	public WorkbenchCommander(SpecTest modelTest, Workbench workbench) {
 		super(modelTest, workbench);
 		this.workbench = workbench;
+	}
+	
+	public CommanderChainer getBundledFiles() {
+		fail("the model doesn't yet support bundling!");
+		
+		return commanderChainer;
+	}
+	
+	public BundleInfoCommander getBundleInfo() throws Exception {
+		
+		return new BundleInfoCommander(workbench.getBundleSet());
+	}
+
+	public void pageLoaded(StringBuffer pageResponse, String locale) throws ConfigException, IOException, ModelOperationException {
+		StringWriter writer = new StringWriter();	
+		
+		IndexPageWriter.write(FileUtils.readFileToString(workbench.file("index.html")), workbench.getBundleSet(), writer, Mode.Dev, locale);
+		
+		pageResponse.append(writer.toString());
 	}
 }
