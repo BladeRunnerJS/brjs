c12ab73098f18eae674df24e3b9ee650866b52c7
dominicc <dominicc@localhost>
2013-11-26 08:15:45 +0000

    Slight improvement to the aliasing test.

--- brjs-core/src/test/java/org/bladerunnerjs/spec/aliasing/AliasingTest.java
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/aliasing/AliasingTest.java
@@ -5,6 +5,7 @@ import org.bladerunnerjs.model.Aspect;
 import org.bladerunnerjs.model.Blade;
 import org.bladerunnerjs.model.Bladeset;
 import org.bladerunnerjs.model.aliasing.AliasDefinitionsFile;
+import org.bladerunnerjs.model.aliasing.AliasException;
 import org.bladerunnerjs.model.aliasing.AliasesFile;
 import org.bladerunnerjs.specutil.engine.SpecTest;
 import org.junit.Before;
@@ -46,8 +47,8 @@ public class AliasingTest extends SpecTest {
 	@Test
 	public void weBundleAClassIfTheAliasIsDefinedInABladeAliasDefinitionsXml() throws Exception {
 		given(aspect).hasClass("novox.Class1")
-			.and(bladeAliasDefinitionsFile).hasAlias("the-alias", "novox.Class1")
-			.and(aspect).indexPageRefersTo("the-alias");
+			.and(bladeAliasDefinitionsFile).hasAlias("novox.bs.b1.the-alias", "novox.Class1")
+			.and(aspect).indexPageRefersTo("novox.bs.b1.the-alias");
 		when(app).requestReceived("/default-aspect/js/dev/en_GB/combined/bundle.js", response);
 		then(response).containsClasses("novox.Class1");
 	}
@@ -60,6 +61,11 @@ public class AliasingTest extends SpecTest {
 	@Ignore
 	@Test
 	public void aliasDefinitionsDefinedWithinBladesMustBeNamespaced() throws Exception {
+		given(aspect).hasClass("novox.Class1")
+			.and(bladeAliasDefinitionsFile).hasAlias("the-alias", "novox.Class1")
+			.and(aspect).indexPageRefersTo("the-alias");
+		when(app).requestReceived("/default-aspect/js/dev/en_GB/combined/bundle.js", response);
+		then(exceptions).verifyException(AliasException.class);
 	}
 	
 	@Ignore
@@ -79,12 +85,22 @@ public class AliasingTest extends SpecTest {
 	
 	@Ignore
 	@Test
+	public void theSameAliasCanBeDefinedMultipleTimesUsingDifferentGroupsWithoutAmbiguity() throws Exception {
+	}
+	
+	@Ignore
+	@Test
 	public void settingAGroupChangesTheAliasesThatAreUsed() throws Exception {
 	}
 	
 	@Ignore
 	@Test
 	public void aliasesCanStillBeOverriddenWhenAGroupIsSet() throws Exception {
+	}
+	
+	@Ignore
+	@Test
+	public void usingGroupsCanLeadToAmbiguousAliases() throws Exception {
 	}
 	
 	@Ignore
