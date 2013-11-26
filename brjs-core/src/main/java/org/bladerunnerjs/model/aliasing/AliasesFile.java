d0dc97232cfffa321297c91b385ba293c08b718e
dominicc <dominicc@localhost>
2013-11-26 17:01:18 +0000

    Writing functionality moved from AliasesFileBuilder to AliasesFile.

--- brjs-core/src/main/java/org/bladerunnerjs/model/aliasing/AliasesFile.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/aliasing/AliasesFile.java
@@ -2,30 +2,39 @@ package org.bladerunnerjs.model.aliasing;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.Location;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.TransformerException;
 
+import org.apache.commons.io.FileUtils;
 import org.bladerunnerjs.model.exception.request.BundlerFileProcessingException;
 import org.bladerunnerjs.model.utility.FileModifiedChecker;
 import org.bladerunnerjs.model.utility.XmlStreamReaderFactory;
 import org.bladerunnerjs.model.utility.stax.XmlStreamReader;
+import org.bladerunnerjs.specutil.XmlBuilderSerializer;
 import org.codehaus.stax2.validation.XMLValidationSchema;
 import org.codehaus.stax2.validation.XMLValidationSchemaFactory;
 
 import com.ctc.wstx.msv.RelaxNGSchemaFactory;
+import com.esotericsoftware.yamlbeans.parser.Parser.ParserException;
+import com.google.common.base.Joiner;
+import com.jamesmurty.utils.XMLBuilder;
 
 public class AliasesFile extends File {
 	private static final long serialVersionUID = 1L;
 	private static XMLValidationSchema aliasesSchema;
 	
 	private final FileModifiedChecker fileModifiedChecker;
-	private List<AliasDefinition> aliasDefinitions;
-	private List<String> groupNames;
+	private List<AliasDefinition> aliasDefinitions = new ArrayList<>();
+	private List<String> groupNames = new ArrayList<>();
 	private String scenario;
 	
 	static {
@@ -67,6 +76,10 @@ public class AliasesFile extends File {
 		return scenario;
 	}
 	
+	public void setScenarioName(String scenarioName) {
+		this.scenario = scenarioName;
+	}
+	
 	public List<String> groupNames() throws BundlerFileProcessingException {
 		if(fileModifiedChecker.fileModifiedSinceLastCheck()) {
 			reparseFile();
@@ -75,12 +88,44 @@ public class AliasesFile extends File {
 		return groupNames;
 	}
 	
+	public void setGroupNames(List<String> groupNames) {
+		this.groupNames = groupNames;
+	}
+	
 	public List<AliasDefinition> aliasDefinitions() throws BundlerFileProcessingException {
 		if(fileModifiedChecker.fileModifiedSinceLastCheck()) {
 			reparseFile();
 		}
 		
 		return aliasDefinitions;
+	}
+	
+	// TODO: should we switch away from AliasDefinition, given that there are no interfaces, scenarios or groups at this level?
+	public void addAliasDefinition(AliasDefinition aliasDefinition) {
+		aliasDefinitions.add(aliasDefinition);
+	}
+	
+	public void write() throws IOException {
+		try {
+			XMLBuilder builder = XMLBuilder.create("aliases").ns("http://schema.caplin.com/CaplinTrader/aliases");
+			
+			if(scenario != null) {
+				builder.a("useScenario", scenario);
+			}
+			
+			if(groupNames != null) {
+				builder.a("useGroups", Joiner.on(" ").join(groupNames));
+			}
+			
+			for(AliasDefinition aliasDefinition : aliasDefinitions) {
+				builder.e("alias").a("name", aliasDefinition.getName()).a("class", aliasDefinition.getClassName()).up();
+			}
+			
+			FileUtils.write(this, XmlBuilderSerializer.serialize(builder));
+		}
+		catch(ParserException | TransformerException | ParserConfigurationException | FactoryConfigurationError e) {
+			throw new IOException(e);
+		}
 	}
 	
 	private void reparseFile() throws BundlerFileProcessingException {
--- brjs-core/src/main/java/org/bladerunnerjs/specutil/engine/AliasesFileBuilder.java
+++ brjs-core/src/main/java/org/bladerunnerjs/specutil/engine/AliasesFileBuilder.java
@@ -1,69 +1,43 @@
 package org.bladerunnerjs.specutil.engine;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-import org.apache.commons.io.FileUtils;
 import org.bladerunnerjs.model.aliasing.AliasDefinition;
 import org.bladerunnerjs.model.aliasing.AliasesFile;
-import org.bladerunnerjs.specutil.XmlBuilderSerializer;
-
-import com.google.common.base.Joiner;
-import com.jamesmurty.utils.XMLBuilder;
 
 public class AliasesFileBuilder {
 	private AliasesFile aliasesFile;
 	private BuilderChainer builderChainer;
-	private List<AliasDefinition> aliasDefinitions = new ArrayList<>();
-	private String scenario = null;
-	private List<String> groupNames = null;
 	
 	public AliasesFileBuilder(SpecTest specTest, AliasesFile aliasesFile) {
 		this.aliasesFile = aliasesFile;
 		builderChainer = new BuilderChainer(specTest);
 	}
 	
-	public BuilderChainer usesScenario(String scenario) {
-		this.scenario = scenario;
-		
-		return builderChainer;
-	}
-	
-	public BuilderChainer usesGroups(String... groups) {
-		this.groupNames = Arrays.asList(groups);
-		
-		return builderChainer;
-	}
-	
 	public BuilderChainer exists() throws Exception {
-		writeAliasesFile();
+		aliasesFile.write();
+		
+		return builderChainer;
+	}
+	
+	public BuilderChainer usesScenario(String scenario) throws Exception {
+		aliasesFile.setScenarioName(scenario);
+		aliasesFile.write();
+		
+		return builderChainer;
+	}
+	
+	public BuilderChainer usesGroups(String... groups) throws Exception {
+		aliasesFile.setGroupNames(Arrays.asList(groups));
+		aliasesFile.write();
 		
 		return builderChainer;
 	}
 	
 	public BuilderChainer hasAlias(String aliasName, String classRef) throws Exception {
-		aliasDefinitions.add(new AliasDefinition(aliasName, classRef, null));
-		writeAliasesFile();
+		aliasesFile.addAliasDefinition(new AliasDefinition(aliasName, classRef, null));
+		aliasesFile.write();
 		
 		return builderChainer;
-	}
-	
-	private void writeAliasesFile() throws Exception {
-		XMLBuilder builder = XMLBuilder.create("aliases").ns("http://schema.caplin.com/CaplinTrader/aliases");
-		
-		if(scenario != null) {
-			builder.a("useScenario", scenario);
-		}
-		
-		if(groupNames != null) {
-			builder.a("useGroups", Joiner.on(" ").join(groupNames));
-		}
-		
-		for(AliasDefinition aliasDefinition : aliasDefinitions) {
-			builder.e("alias").a("name", aliasDefinition.getName()).a("class", aliasDefinition.getClassName()).up();
-		}
-		
-		FileUtils.write(aliasesFile, XmlBuilderSerializer.serialize(builder));
 	}
 }
