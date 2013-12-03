d62d791313b8e0ebd6f99d8c257db5d27397d0db
Pre Vyas <priyank.vyas@caplin.com>
2013-12-03 17:35:04 +0000

    Adding some more coverage to NodeJsBundlerPluginTest

--- brjs-core/src/test/java/org/bladerunnerjs/spec/plugin/bundler/NodeJsBundlerPluginTest.java
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/plugin/bundler/NodeJsBundlerPluginTest.java
@@ -10,6 +10,7 @@ public class NodeJsBundlerPluginTest extends SpecTest {
 	private App app;
 	private Aspect aspect;
 	private StringBuffer pageResponse = new StringBuffer();
+	private StringBuffer requestResponse = new StringBuffer();
 	
 	@Before
 	public void initTestObjects() throws Exception
@@ -40,5 +41,30 @@ public class NodeJsBundlerPluginTest extends SpecTest {
 			.and(aspect).indexPageHasContent("<@node-js@/>");
 		when(aspect).indexPageLoadedInProd(pageResponse, "en_GB");
 		then(pageResponse).containsRequests("node-js/bundle.js");
+	}
+
+	@Test
+	public void requiredDependenciesAreAddedToTheResponseWhenNodeJsStyleIsRequested() throws Exception {
+		given(aspect).hasClasses("novox.Class1", "novox.Class2")
+			.and(aspect).resourceFileRefersTo("xml/config.xml", "novox.Class1")
+			.and(aspect).classRequires("novox.Class1", "novox.Class2")
+			.and(aspect).indexPageHasContent("<@node-js@/>");
+		when(app).requestReceived("/default-aspect/node-js/bundle.js", requestResponse);
+		then(requestResponse).textEquals(
+				"// novox/Class2\n" +
+				"novox.Class2 = function() {\n};\n\n\n" +
+				"// novox/Class1\n" +
+				"var Class1 = require('novox/Class2');\n" +
+				"novox.Class1 = function() {\n};\n\n\n");
+	}
+	
+	@Test
+	public void theBundleIsEmptyIfWeDontReferToAnyOfTheClasses() throws Exception {
+		given(aspect).hasClasses("novox.Class1", "novox.Class2")
+			.and(aspect).resourceFileRefersTo("xml/config.xml", "novox.Class1")
+			.and(aspect).classRequires("novox.Class1", "novox.Class2")
+			.and(aspect).indexPageHasContent("<@node-js@/>");
+		when(aspect).indexPageLoadedInDev(pageResponse, "en_GB");
+		then(requestResponse).isEmpty();
 	}
 }
