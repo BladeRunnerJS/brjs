724ec2ed9f8536892cf06d4fc33a91ff30df22d5
Dominic Chambers <dominic.chambers@gmail.com>
2013-12-14 01:00:44 +0000

    Added TODO.

--- brjs-core/src/main/java/org/bladerunnerjs/model/JsLib.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/JsLib.java
@@ -7,6 +7,7 @@ import org.bladerunnerjs.model.exception.ConfigException;
 import org.bladerunnerjs.model.exception.modelupdate.ModelUpdateException;
 
 public interface JsLib extends AssetContainer, NamedNode {
+	// TODO: can this method be deleted now?
 	AssetLocation getRootAssetLocation();
 	JsLibConf libConf() throws ConfigException;
 	void populate(String libNamespace) throws InvalidNameException, ModelUpdateException;
