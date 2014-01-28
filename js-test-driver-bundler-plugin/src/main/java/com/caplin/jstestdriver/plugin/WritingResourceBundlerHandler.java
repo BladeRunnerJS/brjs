4bf5b54d99392763e73766e05156b380aaac778a
Dominic Chambers <dominic.chambers@gmail.com>
2014-01-28 16:59:10 +0000

    Removed unused import.

--- js-test-driver-bundler-plugin/src/main/java/com/caplin/jstestdriver/plugin/WritingResourceBundlerHandler.java
+++ js-test-driver-bundler-plugin/src/main/java/com/caplin/jstestdriver/plugin/WritingResourceBundlerHandler.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.StringReader;
 import java.util.Arrays;
 import java.util.List;
 
