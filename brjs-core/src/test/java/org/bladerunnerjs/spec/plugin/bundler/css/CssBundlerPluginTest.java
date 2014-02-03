cbbaa2d061dd73d0f840b2f3df1a8ad7087099f3
Dominic Chambers <dominic.chambers@gmail.com>
2014-02-03 16:21:44 +0000

    Added some CSS content plug-in tests.

---
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/plugin/bundler/css/CssBundlerPluginTest.java
@@ -0,0 +1,93 @@
+package org.bladerunnerjs.spec.plugin.bundler.css;
+
+import org.bladerunnerjs.model.App;
+import org.bladerunnerjs.model.Aspect;
+import org.bladerunnerjs.model.JsLib;
+import org.bladerunnerjs.testing.specutility.engine.SpecTest;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CssBundlerPluginTest extends SpecTest {
+	private App app;
+	private Aspect aspect;
+	private JsLib nonConformantLib;
+	private StringBuffer requestResponse = new StringBuffer();
+	
+	@Before
+	public void initTestObjects() throws Exception {
+		given(brjs).automaticallyFindsBundlers()
+			.and(brjs).hasBeenCreated();
+			app = brjs.app("app1");
+			aspect = app.aspect("default");
+			nonConformantLib = app.jsLib("non-conformant-lib");
+	}
+	
+	@Test
+	public void cssFilesInResourcesAppearInTheCommonTheme() throws Exception {
+		given(aspect).hasClass("appns.Class1")
+			.and(aspect).indexPageRefersTo("appns.Class1")
+			.and(aspect).containsFile("resources/style.css");
+		when(app).requestReceived("/default-aspect/css/common/bundle.css", requestResponse);
+		then(requestResponse).containsOrderedTextFragments("resources/style.css");
+	}
+	
+	@Test
+	public void cssFilesInResourcesAppearDontAppearInAnyOtherThemes() throws Exception {
+		given(aspect).hasClass("appns.Class1")
+			.and(aspect).indexPageRefersTo("appns.Class1")
+			.and(aspect).containsFile("resources/style.css");
+		when(app).requestReceived("/default-aspect/css/theme1/bundle.css", requestResponse);
+		then(requestResponse).doesNotContainText("resources/style.css");
+	}
+	
+	@Test
+	public void cssFilesDeepWithinResourcesAppearInTheTheme() throws Exception {
+		given(aspect).hasClass("appns.Class1")
+			.and(aspect).indexPageRefersTo("appns.Class1")
+			.and(aspect).containsFile("resources/dir1/dir2/style.css");
+		when(app).requestReceived("/default-aspect/css/common/bundle.css", requestResponse);
+		then(requestResponse).containsOrderedTextFragments("resources/dir1/dir2/style.css");
+	}
+	
+	@Test
+	public void cssFilesInNonConformantLibrariesAppearInTheCommonTheme() throws Exception {
+		given(aspect).hasClass("appns.Class1")
+			.and(aspect).indexPageRequires(nonConformantLib)
+			.and(nonConformantLib).containsFileWithContents("library.manifest", "css: style.css")
+			.and(nonConformantLib).containsFile("style.css");
+		when(app).requestReceived("/default-aspect/css/common/bundle.css", requestResponse);
+		then(requestResponse).containsOrderedTextFragments("style.css");
+	}
+	
+	@Test
+	public void cssFilesInNonConformantLibrariesDontAppearInAnyOtherThemes() throws Exception {
+		given(aspect).hasClass("appns.Class1")
+			.and(aspect).indexPageRequires(nonConformantLib)
+			.and(nonConformantLib).containsFileWithContents("library.manifest", "css: style.css")
+			.and(nonConformantLib).containsFile("style.css");
+		when(app).requestReceived("/default-aspect/css/theme1/bundle.css", requestResponse);
+		then(requestResponse).doesNotContainText("style.css");
+	}
+	
+	@Test
+	public void commonThemeCssFilesAppearInTheCommonTheme() throws Exception {
+		given(aspect).hasClass("appns.Class1")
+			.and(aspect).indexPageRefersTo("appns.Class1")
+			.and(aspect).containsFile("themes/common/style.css");
+		when(app).requestReceived("/default-aspect/css/common/bundle.css", requestResponse);
+		then(requestResponse).containsOrderedTextFragments("themes/common/style.css");
+	}
+	
+	@Test
+	public void commonThemeCssFilesDontAppearInAnyOtherThemes() throws Exception {
+		given(aspect).hasClass("appns.Class1")
+			.and(aspect).indexPageRefersTo("appns.Class1")
+			.and(aspect).containsFile("themes/common/style.css");
+		when(app).requestReceived("/default-aspect/css/theme1/bundle.css", requestResponse);
+		then(requestResponse).doesNotContainText("themes/common/style.css");
+	}
+	
+	// TODO: 'theme1' version of the previous two tests
+	
+	// TODO: tests for locale specific themed css files
+}
