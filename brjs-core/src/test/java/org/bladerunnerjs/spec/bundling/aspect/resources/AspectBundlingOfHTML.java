b8113ecfe074ec4e4a5e08bb6b5634a8e2748572
Pre Vyas <priyank.vyas@caplin.com>
2014-02-07 14:41:02 +0000

    Updating SDK BRJS Lib test for HTML bundling, removing @Ignore

--- brjs-core/src/test/java/org/bladerunnerjs/spec/bundling/aspect/resources/AspectBundlingOfHTML.java
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/bundling/aspect/resources/AspectBundlingOfHTML.java
@@ -7,7 +7,6 @@ import org.bladerunnerjs.model.Bladeset;
 import org.bladerunnerjs.model.JsLib;
 import org.bladerunnerjs.testing.specutility.engine.SpecTest;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class AspectBundlingOfHTML extends SpecTest {
@@ -60,17 +59,18 @@ public class AspectBundlingOfHTML extends SpecTest {
 	}
 	
 	// SDK BRJS Lib
-	@Ignore // This test should pass to prove that the app can bundle sdk html resources
 	@Test
 	public void aspectCanBundleSdkLibHtmlResources() throws Exception {
 		given(sdkLib).hasBeenCreated()
 			.and(sdkLib).hasNamespacedJsPackageStyle()
-			.and(sdkLib).containsFileWithContents("resources/html/view.html", "<div id='tree-view'></div>")
+			.and(sdkLib).containsFileWithContents("resources/html/workbench.html", "<div id='br.workbench-view'></div>")
 			.and(sdkLib).hasClass("br.workbench.ui.Workbench")
-			.and(aspect).containsFileWithContents("resources/workbench-view.html", "<div id='appns.aspect-view'></div>")
+			.and(aspect).containsFileWithContents("resources/aspect.html", "<div id='appns.aspect-view'></div>")
 			.and(aspect).indexPageRefersTo("br.workbench.ui.Workbench");
 		when(app).requestReceived("/default-aspect/bundle.html", response);
-		then(response).containsOrderedTextFragments("<div id='appns.workbench-view'></div>",
-													"<div id='tree-view'></div>");
+		then(response).containsOrderedTextFragments("<!-- workbench.html -->",
+													"<div id='br.workbench-view'></div>",
+													"<!-- aspect.html -->",
+													"<div id='appns.aspect-view'></div>");
 	}
 }
