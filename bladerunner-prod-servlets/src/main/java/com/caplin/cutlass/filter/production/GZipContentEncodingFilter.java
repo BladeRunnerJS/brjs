c16810141d8bb72d5764a9cc5c76a5360280c8a3
Andy Berry <andyberry88@gmail.com>
2014-02-09 20:23:31 +0000

    moving across fix for exception in websphere from CT codebase - fixes #225

--- bladerunner-prod-servlets/src/main/java/com/caplin/cutlass/filter/production/GZipContentEncodingFilter.java
+++ bladerunner-prod-servlets/src/main/java/com/caplin/cutlass/filter/production/GZipContentEncodingFilter.java
@@ -1,5 +1,6 @@
 package com.caplin.cutlass.filter.production;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.servlet.Filter;
@@ -57,6 +58,10 @@ public class GZipContentEncodingFilter implements Filter
 			{
 				chain.doFilter(request, response);
 			}
+		}
+		catch(FileNotFoundException e)
+		{
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_NOT_FOUND);
 		}
 		catch(IOException|ServletException e)
 		{
