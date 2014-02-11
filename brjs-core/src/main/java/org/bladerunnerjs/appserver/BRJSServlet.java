b1a24b90bd05aabf7ef2be648e56cadb99e3c7df
Dominic Chambers <dominic.chambers@gmail.com>
2014-02-11 17:39:54 +0000

    Changed BRJSServlet so that it no longer extends Jetty's default servlet, allowing it to be used on any Java application server.

--- brjs-core/src/main/java/org/bladerunnerjs/appserver/BRJSServlet.java
+++ brjs-core/src/main/java/org/bladerunnerjs/appserver/BRJSServlet.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,10 +16,9 @@ import org.bladerunnerjs.model.BladerunnerUri;
 import org.bladerunnerjs.model.exception.request.ContentProcessingException;
 import org.bladerunnerjs.model.exception.request.MalformedRequestException;
 import org.bladerunnerjs.model.exception.request.ResourceNotFoundException;
-import org.eclipse.jetty.servlet.DefaultServlet;
 
 
-public class BRJSServlet extends DefaultServlet
+public class BRJSServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1964608537461568895L;
 	
