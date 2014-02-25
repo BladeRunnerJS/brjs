acb4a096233ece6a88b3a657a7c16516bf8992b9
Andy Berry <andyberry88@gmail.com>
2014-02-25 11:13:12 +0000

    adding a SourceAsset that contains things common between SourceModule and the new interface TestSourceModule

---
+++ brjs-core/src/main/java/org/bladerunnerjs/model/TestSourceModule.java
@@ -0,0 +1,10 @@
+package org.bladerunnerjs.model;
+
+/**
+ * A test source file, typically JavaScript (.js) files that live in a 'tests' directory.
+ *
+ */
+public interface TestSourceModule extends SourceAsset
+{
+	
+}
