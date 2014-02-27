06b52236a4450cc3b99e7fee0204ae8d74306c74
Andy Berry <andyberry88@gmail.com>
2014-02-27 11:47:10 +0000

    making ChildSourceAssetLocation absract and adding a new ChildSourceAssetLocation and ChildTestAssetLocation that extend it and are both final

--- brjs-core/src/main/java/org/bladerunnerjs/model/ChildTestAssetLocation.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/ChildTestAssetLocation.java
@@ -6,12 +6,13 @@ import org.bladerunnerjs.model.engine.Node;
 import org.bladerunnerjs.model.engine.RootNode;
 
 
-public class ChildTestAssetLocation extends ChildSourceAssetLocation
+public final class ChildTestAssetLocation extends AbstractChildSourceAssetLocation
 {
 
 	public ChildTestAssetLocation(RootNode rootNode, Node parent, File dir, AssetLocation parentAssetLocation)
 	{
 		super(rootNode, parent, dir, parentAssetLocation);
+		registerInitializedNode();
 	}
 
 }
