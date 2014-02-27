211bbedf7c3eada9d4a632f4eb91c69560578462
Dominic Chambers <dominic.chambers@gmail.com>
2014-02-27 16:03:30 +0000

    Fix misleading exception text.

--- brjs-core/src/main/java/org/bladerunnerjs/aliasing/UnresolvableAliasException.java
+++ brjs-core/src/main/java/org/bladerunnerjs/aliasing/UnresolvableAliasException.java
@@ -6,6 +6,6 @@ public class UnresolvableAliasException extends AliasException {
 	private static final long serialVersionUID = 1L;
 	
 	public UnresolvableAliasException(AliasesFile aliasesFile, String aliasName) {
-		super("Alias '" + aliasName + "' has multiple definitions for aliases file 'aliasesFile', or other " + aliasesFile.getUnderlyingFile().getPath() + " files that it inherits from");
+		super("Alias '" + aliasName + "' has not been defined within " + aliasesFile.getUnderlyingFile().getPath() + " or any other files that it inherits from");
 	}
 }
