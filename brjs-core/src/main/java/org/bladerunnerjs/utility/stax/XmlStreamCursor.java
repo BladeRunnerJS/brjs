b0bbc90e5f12c2bc0a90b294578281869abd9e08
Dominic Chambers <dominic.chambers@gmail.com>
2014-02-28 11:41:14 +0000

    Created a new XmlStreamCursor utility that allows Stax XML streaming readers to be conveniently used without the need for peeking, as a replacement for the XmlStreamReader utility that currently has a bug that can only be fixed by introducing peeking if we want to keep it's current API design.

---
+++ brjs-core/src/main/java/org/bladerunnerjs/utility/stax/XmlStreamCursor.java
@@ -0,0 +1,24 @@
+package org.bladerunnerjs.utility.stax;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.codehaus.stax2.XMLStreamReader2;
+
+public class XmlStreamCursor {
+	private final XMLStreamReader2 xmlStreamReader;
+	private final int nodeDepth;
+	
+	public XmlStreamCursor(XMLStreamReader2 xmlStreamReader) throws XMLStreamException {
+		this.xmlStreamReader = xmlStreamReader;
+		nodeDepth = xmlStreamReader.getDepth();
+		xmlStreamReader.nextTag();
+	}
+	
+	public void nextTag() throws XMLStreamException {
+		xmlStreamReader.nextTag();
+	}
+	
+	public boolean isWithinInitialNode() {
+		return xmlStreamReader.getDepth() > nodeDepth;
+	}
+}
