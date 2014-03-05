4c3801957eb9917f69af99993556754616f2fb10
Dominic Chambers <dominic.chambers@gmail.com>
2014-03-05 17:21:00 +0000

    Fixed a bug that meant that aliases defined within the 'aliases.xml' file, as opposed to one of the 'aliasDefinitions.xml' files, did not need to be quoted to match.

--- brjs-core/src/test/java/org/bladerunnerjs/spec/aliasing/AliasBundlingTest.java
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/aliasing/AliasBundlingTest.java
@@ -68,7 +68,7 @@ public class AliasBundlingTest extends SpecTest {
 	public void weBundleAClassIfItsAliasIsReferredToInTheIndexPage() throws Exception {
 		given(aspect).hasClass("appns.Class1")
 			.and(aspectAliasesFile).hasAlias("the-alias", "appns.Class1")
-			.and(aspect).indexPageRefersTo("the-alias");
+			.and(aspect).indexPageRefersTo("'the-alias'");
 		when(app).requestReceived("/default-aspect/js/dev/en_GB/combined/bundle.js", response);
 		then(response).containsClasses("appns.Class1");
 	}
@@ -101,7 +101,7 @@ public class AliasBundlingTest extends SpecTest {
 			.and(aspect).hasClasses("appns.Class1", "appns.Class2")
 			.and(aspectAliasesFile).hasAlias("the-alias", "appns.Class2")
 			.and(aspect).indexPageRefersTo("appns.Class1")
-			.and(aspect).classRefersTo("appns.Class1", "the-alias");
+			.and(aspect).classRefersTo("appns.Class1", "'the-alias'");
 		when(app).requestReceived("/default-aspect/js/dev/en_GB/combined/bundle.js", response);
 		then(response).containsClasses("appns.Class1", "appns.Class2");
 	}
@@ -111,7 +111,7 @@ public class AliasBundlingTest extends SpecTest {
 		given(aspect).hasClasses("appns.Class1", "appns.Class2")
 			.and(aspect).classRequires("appns.Class1", "appns.Class2")
 			.and(aspectAliasesFile).hasAlias("the-alias", "appns.Class1")
-			.and(aspect).indexPageRefersTo("the-alias");
+			.and(aspect).indexPageRefersTo("'the-alias'");
 		when(app).requestReceived("/default-aspect/js/dev/en_GB/combined/bundle.js", response);
 		then(response).containsClasses("appns.Class1", "appns.Class2");
 	}
@@ -158,7 +158,7 @@ public class AliasBundlingTest extends SpecTest {
 		given(aspect).classRequires("appns.Class1", "br/AliasRegistry")
 			.and(brLib).hasClass("br.AliasRegistry")
 			.and(aspectAliasesFile).hasAlias("the-alias", "appns.Class1")
-			.and(aspect).indexPageRefersTo("the-alias");
+			.and(aspect).indexPageRefersTo("'the-alias'");
 		when(app).requestReceived("/default-aspect/aliasing/bundle.js", response);
 		then(response).containsText("setAliasData({'the-alias':{'class':require('appns/Class1'),'className':'appns.Class1'}})");
 	}
