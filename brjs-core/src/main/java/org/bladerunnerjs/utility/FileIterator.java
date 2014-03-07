5fadcab5715d4157ec7367fe3d07ad6e5665c2b9
Dominic Chambers <dominic.chambers@gmail.com>
2014-03-07 09:06:31 +0000

    Added a guard which prevents a non-fatal exception from being output to the console when running the Java tests via gradle.

--- brjs-core/src/main/java/org/bladerunnerjs/utility/FileIterator.java
+++ brjs-core/src/main/java/org/bladerunnerjs/utility/FileIterator.java
@@ -70,9 +70,13 @@ public class FileIterator {
 	
 	private void updateIfChangeDetected() {
 		if((fileModificationChecker.hasChangedSinceLastCheck()) || (files == null)) {
-			files = Arrays.asList(dir.listFiles());
-			dirs = null;
-			Collections.sort(files, NameFileComparator.NAME_COMPARATOR);
+			// TODO: see if this guard can be removed (it currently prevents an exception that can be seen when running gradle testJava, but that doesn't fail the build)
+			// once we've added support for automatically adding and removing watchers and file iterators as directories come and go
+			if(dir.exists()) {
+				files = Arrays.asList(dir.listFiles());
+				dirs = null;
+				Collections.sort(files, NameFileComparator.NAME_COMPARATOR);
+			}
 		}
 	}
 	
