7cdbd11abcbda2516674f216df1fc4f9d51779b1
Dominic Chambers <dominic.chambers@gmail.com>
2014-03-12 15:38:34 +0000

    Fixed a part of a guard that I wasn't easily able to create a test for using our spec test framework.

--- brjs-core/src/main/java/org/bladerunnerjs/aliasing/aliases/AliasesFile.java
+++ brjs-core/src/main/java/org/bladerunnerjs/aliasing/aliases/AliasesFile.java
@@ -98,7 +98,7 @@ public class AliasesFile {
 			aliasDefinition = new AliasDefinition(aliasOverride.getName(), aliasOverride.getClassName(), aliasDefinition.getInterfaceName());
 		}
 		
-		if(aliasDefinition.getClassName().equals(null) || aliasDefinition.getClassName().equals("")) {
+		if((aliasDefinition.getClassName() == null) || aliasDefinition.getClassName().equals("")) {
 			throw new IncompleteAliasException(file, aliasDefinition.getName());
 		}
 		
