3f771e31e54081725fb6bab372fc406dea23ecde
Pre Vyas <priyank.vyas@caplin.com>
2014-03-12 12:41:55 +0000

    Changing AliasRegistry tests to use a more familar test file format. Added tearDown() to uninstall the definitionRegistry as highlighted by @dchambers

--- cutlass-sdk/workspace/sdk/libs/javascript/br-libs/br/tests/test-unit/js-test-driver/tests/AliasRegistryTest.js
+++ cutlass-sdk/workspace/sdk/libs/javascript/br-libs/br/tests/test-unit/js-test-driver/tests/AliasRegistryTest.js
@@ -1,17 +1,18 @@
 (function(){
 	"use strict";
-	
+
 	require("jstestdriverextensions");
 	require("jsmockito");
 	var br = require('br/Core');
 	var Errors = require('br/Errors');
+
 	var AliasRegistry = require('br/AliasRegistry').constructor;
-	
-	var aliasRegistry;
+	var AliasRegistryTest = TestCase("AliasRegistryTest").prototype;
+
 	var testAliasData = {};
+	var aliasRegistry = null;
 
-
-	//Define all classes and interfaces
+	//  ------- Define all classes and interfaces  -------
 	var Alias1Interface = function(){};
 	Alias1Interface.prototype.interfaceFunction = function(){};
 
@@ -21,8 +22,7 @@
 	var Alias2Interface = function(){};
 	Alias2Interface.prototype.interfaceFunction2 = function(){};
 
-	
-	// setup our Alias classes
+	// ------- Setup our Alias classes ------
 	var pkg = {
 		a: {},
 		b: {}
@@ -37,127 +37,128 @@
 	br.hasImplemented(pkg.a.AliasClass1, Alias1Interface);
 	br.hasImplemented(pkg.a.AliasClass1, Alias1AlternateInterface);
 	br.hasImplemented(pkg.b.AliasClass2, Alias2Interface);
-	
-	
-	var onAliasRegistry = defineTestCase("AliasRegistryTest",
-		function() //setUp
-		{
-			JsHamcrest.Integration.JsTestDriver();
-			JsMockito.Integration.JsTestDriver();
 
-			//alias registry data
-			testAliasData = {
-				"some.alias1":
-				{
-					"class":pkg.a.AliasClass1,
-					"className":"pkg.a.AliasClass1",
-					"interface":Alias1Interface,
-					"interfaceName":"Alias1Interface"
-				},
-				"some.alias2":
-				{
-					"class":pkg.b.AliasClass2,
-					"className":"pkg.b.AliasClass2"
-				},
-				"some.alias3":
-				{
-				}
-			};
+	AliasRegistryTest.setUp = function()
+	{
+		JsHamcrest.Integration.JsTestDriver();
+		JsMockito.Integration.JsTestDriver();
 
-			aliasRegistry = new AliasRegistry();
-			aliasRegistry.setAliasData(testAliasData);
+		//alias registry data
+		testAliasData = {
+			"some.alias1":
+			{
+				"class":pkg.a.AliasClass1,
+				"className":"pkg.a.AliasClass1",
+				"interface":Alias1Interface,
+				"interfaceName":"Alias1Interface"
+			},
+			"some.alias2":
+			{
+				"class":pkg.b.AliasClass2,
+				"className":"pkg.b.AliasClass2"
+			},
+			"some.alias3":
+			{
+			}
+		};
 
-			this.definitionRegistry = require('br/TestDefinitionRegistry').install();
-			this.definitionRegistry.define('br/AliasRegistry', aliasRegistry);
-		}
-	);
-	
-	//setAliasData
+		aliasRegistry= new AliasRegistry();
+		aliasRegistry.setAliasData(testAliasData);
 
-	onAliasRegistry.testThat("Service Registry instance can be used", function()
+		this.definitionRegistry = require('br/TestDefinitionRegistry').install();
+		this.definitionRegistry.define('br/AliasRegistry', aliasRegistry);
+	};
+
+	AliasRegistryTest.tearDown = function()
+	{
+		this.definitionRegistry.uninstall();
+	};
+
+	AliasRegistryTest["test Service Registry instance can be used"] = function()
 	{
 		assertNotEquals("The aliases list should be empty", [], require('br/AliasRegistry').getAllAliases());
-	});
+	};
 
-	onAliasRegistry.testThat("Return an empty list of aliases if there is no Alias", function()
+	AliasRegistryTest["test Return an empty list of aliases if there is no Alias"] = function()
 	{
 		aliasRegistry = new AliasRegistry();
 		aliasRegistry.setAliasData({});
 		assertEquals("The aliases list should be empty", [], aliasRegistry.getAllAliases());
-	});
+	};
 
-	onAliasRegistry.testThat("Return the list of aliases from the alias JSON", function()
+	AliasRegistryTest["test Return the list of aliases from the alias JSON"] = function()
 	{
 		assertEquals("Incorrect alias list", ["some.alias1", "some.alias2", "some.alias3"], aliasRegistry.getAllAliases());
-	});
+	};
 
-	onAliasRegistry.testThat("No aliases are returned for an unknown interface", function()
+	AliasRegistryTest["test No aliases are returned for an unknown interface"] = function()
 	{
 		var fSomeInterface = function(){};
 		fSomeInterface.prototype.someMethod = function(){};
 		var pFilteredAliases = aliasRegistry.getAliasesByInterface(fSomeInterface);
 
 		assertEquals("Incorrect alias list", [], pFilteredAliases);
-	});
+	};
 
-	onAliasRegistry.testThat("All aliases that explicitly associate themselves with an interface are returned", function()
+	AliasRegistryTest["test All aliases that explicitly associate themselves with an interface are returned"] = function()
 	{
 		var pFilteredAliases = aliasRegistry.getAliasesByInterface(Alias1Interface);
 		assertEquals("Incorrect alias list", ["some.alias1"], pFilteredAliases);
-	});
+	};
 
-		onAliasRegistry.testThat("All aliases that actually implement an interface are returned", function()
-		{
-			var pFilteredAliases = aliasRegistry.getAliasesByInterface(Alias2Interface);
-			assertEquals("Incorrect alias list", ["some.alias2"], pFilteredAliases);
-		});
+	AliasRegistryTest["test All aliases that actually implement an interface are returned"] = function()
+{
+	var pFilteredAliases = aliasRegistry.getAliasesByInterface(Alias2Interface);
+	assertEquals("Incorrect alias list", ["some.alias2"], pFilteredAliases);
+};
 
-		onAliasRegistry.testThat("All aliases that actually implement an interface are returned, even if they explicitly implement another interface", function()
-		{
-			var pFilteredAliases = aliasRegistry.getAliasesByInterface(Alias1AlternateInterface);
-			assertEquals("Incorrect alias list", ["some.alias1"], pFilteredAliases);
-		});
+	AliasRegistryTest["test All aliases that actually implement an interface are returned, even if they explicitly implement another interface"] = function()
+	{
+		var pFilteredAliases = aliasRegistry.getAliasesByInterface(Alias1AlternateInterface);
+		assertEquals("Incorrect alias list", ["some.alias1"], pFilteredAliases);
+	};
 
-		onAliasRegistry.testThat("Check isAlias returns the correct values", function()
-		{
-			assertTrue(aliasRegistry.isAlias("some.alias1"));
-			assertTrue(aliasRegistry.isAlias("some.alias3"));
-			assertFalse(aliasRegistry.isAlias("some.alias4"));
-		});
+	AliasRegistryTest["test Check isAlias returns the correct values"] = function()
+	{
+		assertTrue(aliasRegistry.isAlias("some.alias1"));
+		assertTrue(aliasRegistry.isAlias("some.alias3"));
+		assertFalse(aliasRegistry.isAlias("some.alias4"));
+	};
 
-		onAliasRegistry.testThat("Check isAliasAssigned returns the correct values", function()
-		{
-			assertTrue(aliasRegistry.isAliasAssigned("some.alias1"));
-			assertFalse(aliasRegistry.isAliasAssigned("some.alias3"));
-		});
+	AliasRegistryTest["test Check isAliasAssigned returns the correct values"] = function()
+	{
+		assertTrue(aliasRegistry.isAliasAssigned("some.alias1"));
+		assertFalse(aliasRegistry.isAliasAssigned("some.alias3"));
+	};
 
-		onAliasRegistry.testThat("Fails fast when alias is not found", function()
-		{
-			var self = this;
-			assertException("Should throw an error if the alias doesn't exist",
-				function() {
-					aliasRegistry.getClass("some.alias3");
-				},
-				Errors.ILLEGAL_STATE
-			);
-		});
+	AliasRegistryTest["test Fails fast when alias is not found"] = function()
+	{
+		var self = this;
+		assertException("Should throw an error if the alias doesn't exist",
+			function() {
+				aliasRegistry.getClass("some.alias3");
+			},
+			Errors.ILLEGAL_STATE
+		);
+	};
 
-		onAliasRegistry.testThat("Returns the correct class when an existing alias is requested", function()
-		{
-			var fClass = aliasRegistry.getClass("some.alias1");
-			assertEquals("The class retireved is incorrect", pkg.a.AliasClass1, fClass);
-		});
+	AliasRegistryTest["test Returns the correct class when an existing alias is requested"] = function()
+	{
+		var fClass = aliasRegistry.getClass("some.alias1");
+		debugger;
+		assertEquals("The class retireved is incorrect", pkg.a.AliasClass1, fClass);
+	};
 
-		onAliasRegistry.testThat("Fails fast if alias is not implementor of the alias interface", function()
-		{
-			testAliasData["some.alias1"]["class"] = function(){};
-			aliasRegistry = new AliasRegistry();
-			assertException("Should throw an error if the alias is not implementor of the alias interface",
-				function() {
-					aliasRegistry.setAliasData(testAliasData);
-				},
-				Errors.ILLEGAL_STATE
-			);
-		});
+	AliasRegistryTest["test Fails fast if alias is not an implementer of the alias interface"] = function()
+	{
+		testAliasData["some.alias1"]["class"] = function(){};
+		aliasRegistry = new AliasRegistry();
+		assertException("Should throw an error if the alias is not implementor of the alias interface",
+			function() {
+				aliasRegistry.setAliasData(testAliasData);
+			},
+			Errors.ILLEGAL_STATE
+		);
+	};
 
 })();
