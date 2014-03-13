e1f07c8f0ac6c101026d6f81cb9ceaf890d70985
Dominic Chambers <dominic.chambers@gmail.com>
2014-03-13 08:09:32 +0000

    Added a 'workbench-deps' command to complement the 'aspect-deps' command that was created previously.

--- brjs-core/src/main/java/org/bladerunnerjs/model/exception/command/NodeDoesNotExistException.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/exception/command/NodeDoesNotExistException.java
@@ -1,12 +1,17 @@
 package org.bladerunnerjs.model.exception.command;
 
 import org.bladerunnerjs.model.engine.NamedNode;
+import org.bladerunnerjs.model.engine.Node;
 import org.bladerunnerjs.plugin.CommandPlugin;
 
 public class NodeDoesNotExistException extends CommandArgumentsException {
 	private static final long serialVersionUID = 1L;
 	
 	public NodeDoesNotExistException(NamedNode node, CommandPlugin commandPlugin) {
-		super(node.getClass().getSimpleName() + " '" + node.getName() + "' does not exist", commandPlugin);
+		this(node, node.getName(), commandPlugin);
+	}
+	
+	public NodeDoesNotExistException(Node node, String nodeName, CommandPlugin commandPlugin) {
+		super(node.getClass().getSimpleName() + " '" + nodeName + "' does not exist", commandPlugin);
 	}
 }
