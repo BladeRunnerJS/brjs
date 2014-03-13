4d060c8ffdf46e63c30ef80961941192b9e2ad96
Pre Vyas <priyank.vyas@caplin.com>
2014-03-13 23:21:07 +0000

    Removing unused import

--- brjs-core/src/test/java/org/bladerunnerjs/spec/aliasing/AliasBundlingTest.java
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/aliasing/AliasBundlingTest.java
@@ -10,7 +10,6 @@ import org.bladerunnerjs.model.Bladeset;
 import org.bladerunnerjs.model.JsLib;
 import org.bladerunnerjs.testing.specutility.engine.SpecTest;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 
