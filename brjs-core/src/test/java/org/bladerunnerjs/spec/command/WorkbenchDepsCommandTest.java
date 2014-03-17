89c0b2733acd872365110631d25fcd91a80badf2
Pre Vyas <priyank.vyas@caplin.com>
2014-03-17 12:42:25 +0000

    Adding @Ignore test for workbench-deps command issue with alias/service reference as seen when you run this command against a freshly created blade's workbench

--- brjs-core/src/test/java/org/bladerunnerjs/spec/command/WorkbenchDepsCommandTest.java
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/command/WorkbenchDepsCommandTest.java
@@ -1,9 +1,11 @@
 package org.bladerunnerjs.spec.command;
 
+import org.bladerunnerjs.aliasing.aliasdefinitions.AliasDefinitionsFile;
 import org.bladerunnerjs.model.App;
 import org.bladerunnerjs.model.Aspect;
 import org.bladerunnerjs.model.Blade;
 import org.bladerunnerjs.model.Bladeset;
+import org.bladerunnerjs.model.JsLib;
 import org.bladerunnerjs.model.Workbench;
 import org.bladerunnerjs.model.exception.command.ArgumentParsingException;
 import org.bladerunnerjs.model.exception.command.CommandArgumentsException;
@@ -11,6 +13,7 @@ import org.bladerunnerjs.model.exception.command.NodeDoesNotExistException;
 import org.bladerunnerjs.plugin.plugins.commands.standard.WorkbenchDepsCommand;
 import org.bladerunnerjs.testing.specutility.engine.SpecTest;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -20,6 +23,8 @@ public class WorkbenchDepsCommandTest extends SpecTest {
 	Bladeset bladeset;
 	Blade blade;
 	Workbench workbench;
+	JsLib brLib;
+	AliasDefinitionsFile brLibAliasDefinitionsFile;
 	
 	@Before
 	public void initTestObjects() throws Exception
@@ -33,6 +38,8 @@ public class WorkbenchDepsCommandTest extends SpecTest {
 			bladeset = app.bladeset("bladeset");
 			blade = bladeset.blade("blade");
 			workbench = blade.workbench();
+			brLib = brjs.sdkLib("br");
+			brLibAliasDefinitionsFile = brLib.assetLocation("resources").aliasDefinitionsFile();
 	}
 	
 	@Test
@@ -108,6 +115,23 @@ public class WorkbenchDepsCommandTest extends SpecTest {
 		given(workbench).indexPageRequires("appns/Class1")
 			.and(aspect).hasClasses("appns.Class1", "appns.Class2")
 			.and(aspect).classRequires("appns.Class1", "./Class2");
+		when(brjs).runCommand("workbench-deps", "app", "bladeset", "blade");
+		then(output).containsText(
+			"Workbench dependencies found:",
+			"    +--- 'bladeset-bladeset/blades/blade/workbench/index.html' (seed file)",
+			"    |    \\--- 'default-aspect/src/appns/Class1.js'",
+			"    |    |    \\--- 'default-aspect/src/appns/Class2.js'");
+	}
+	
+	// TODO understand why this test thorws an UnresolvedAliasException
+	// Update the output string as necessary, verifying that the br.Class2 dependency gets correctly displayed
+	@Ignore
+	@Test
+	public void dependenciesAreShownForWorkbenchUsingAliasWhenAllArgumentsAreValid() throws Exception {
+		given(brLib).hasClasses("br.Class1", "br.Class2")
+			.and(brLibAliasDefinitionsFile).hasAlias("br.alias", "br.Class2")
+			.and(blade).classFileHasContent("appns/bladeset/blade/Class1", "ServiceRegistry.getService('br.alias')")
+			.and(workbench).indexPageRequires("appns/bladeset/blade/Class1");
 		when(brjs).runCommand("workbench-deps", "app", "bladeset", "blade");
 		then(output).containsText(
 			"Workbench dependencies found:",
