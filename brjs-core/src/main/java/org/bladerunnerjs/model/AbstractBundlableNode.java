27f44551854c11e0eeb91ad8c15bf9a7349673c3
Andy Berry <andyberry88@gmail.com>
2014-03-18 11:54:00 +0000

    moving service/alias logic into bundleable node so anyone using aliases can talk about the service prefix - temporary workaround until we get a formalised mechanism)

--- brjs-core/src/main/java/org/bladerunnerjs/model/AbstractBundlableNode.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/AbstractBundlableNode.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.bladerunnerjs.aliasing.AliasDefinition;
 import org.bladerunnerjs.aliasing.AmbiguousAliasException;
 import org.bladerunnerjs.aliasing.IncompleteAliasException;
@@ -93,7 +94,28 @@ public abstract class AbstractBundlableNode extends AbstractAssetContainer imple
 	
 	@Override
 	public AliasDefinition getAlias(String aliasName) throws UnresolvableAliasException, AmbiguousAliasException, IncompleteAliasException, ContentFileProcessingException {
-		return aliasesFile().getAlias(aliasName);
+		
+		//TODO: remove the hack that differs in behaviour if an alias starts with "SERVICE!"
+		
+		boolean isService = aliasName.startsWith("SERVICE!");
+		if (isService)
+		{
+			aliasName = StringUtils.substringAfter(aliasName, "SERVICE!");
+		}
+		
+		try
+		{
+			return aliasesFile().getAlias(aliasName);
+		}
+		catch (UnresolvableAliasException ex)
+		{
+			if (isService)
+			{
+				// do nothing with the exception since a service might be configured at runtime
+				return null;
+			}
+			throw ex;
+		}
 	}
 	
 	@Override
