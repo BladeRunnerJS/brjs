7de442837fe91b8a88b851209b980d29c433a7f4
Pre Vyas <priyank.vyas@caplin.com>
2014-03-28 17:42:43 +0000

    Updating DirectoryDoesNotExistException and InvalidBundlableNodeException to accept string filePath params. The only place this is used is by the BundleDepsCommand, where I've introduced a relativePath string which I normalise to "/" instead of "\\" so that tests don't have to deal with this (File.separators in tests are nasty) Added missing @Test annotation and fixed the test so that it throws the intended exception (needed the app to exist first) Misc: Removing unused import cc: @dchambers

--- brjs-core/src/main/java/org/bladerunnerjs/model/exception/command/DirectoryDoesNotExistException.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/exception/command/DirectoryDoesNotExistException.java
@@ -1,13 +1,11 @@
 package org.bladerunnerjs.model.exception.command;
 
-import java.io.File;
-
 import org.bladerunnerjs.plugin.CommandPlugin;
 
 public class DirectoryDoesNotExistException extends CommandArgumentsException {
 	private static final long serialVersionUID = 1L;
 	
-	public DirectoryDoesNotExistException(File directory, CommandPlugin commandPlugin) {
-		super("The directory '" + directory + "' does not exist.", commandPlugin);
+	public DirectoryDoesNotExistException(String path, CommandPlugin commandPlugin) {
+		super("The directory '" + path + "' does not exist.", commandPlugin);
 	}
 }
--- brjs-core/src/main/java/org/bladerunnerjs/plugin/plugins/bundlers/namespacedjs/NamespacedJsContentPlugin.java
+++ brjs-core/src/main/java/org/bladerunnerjs/plugin/plugins/bundlers/namespacedjs/NamespacedJsContentPlugin.java
@@ -7,7 +7,6 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
