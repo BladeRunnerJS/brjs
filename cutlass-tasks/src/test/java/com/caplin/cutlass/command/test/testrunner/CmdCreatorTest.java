8cbcf5f3e6a315047bc95cac16b208ab0fd933d5
Pre Vyas <priyank.vyas@caplin.com>
2014-03-31 20:26:00 +0100

    Refactoring tests to be cleaner

--- cutlass-tasks/src/test/java/com/caplin/cutlass/command/test/testrunner/CmdCreatorTest.java
+++ cutlass-tasks/src/test/java/com/caplin/cutlass/command/test/testrunner/CmdCreatorTest.java
@@ -9,15 +9,13 @@ public class CmdCreatorTest {
 	@Test
 	public void testBrowserPathsCanBeCreatedUsingDoubleDollarSymbols() throws Exception
 	{
-		String[] x = CmdCreator.cmd("path/to/browser.exe$$--param1=value1");
-		Assert.assertArrayEquals(x, new String[]{"path/to/browser.exe", "--param1=value1"});
+		Assert.assertArrayEquals(new String[]{"path/to/browser.exe", "--param1=value1"}, CmdCreator.cmd("path/to/browser.exe$$--param1=value1"));
 	}
 	
 	@Test
 	public void testBrowserPathsCanBeCreatedUsingSpaces() throws Exception
 	{
-		String[] x = CmdCreator.cmd("path/to/browser.exe --param1=value1");
-		Assert.assertArrayEquals(x, new String[]{"path/to/browser.exe", "--param1=value1"});
+		Assert.assertArrayEquals(new String[]{"path/to/browser.exe", "--param1=value1"}, CmdCreator.cmd("path/to/browser.exe --param1=value1"));
 	}
 
 }
