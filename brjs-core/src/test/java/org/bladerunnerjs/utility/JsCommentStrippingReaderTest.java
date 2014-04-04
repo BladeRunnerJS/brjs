eb665e6fac0c4ea48cd1b315f7c459f64f518d6b
Andy Berry <andyberry88@gmail.com>
2014-04-04 12:17:22 +0100

    changing test to use string literals rather than hidden input/p/output files

--- brjs-core/src/test/java/org/bladerunnerjs/utility/JsCommentStrippingReaderTest.java
+++ brjs-core/src/test/java/org/bladerunnerjs/utility/JsCommentStrippingReaderTest.java
@@ -2,170 +2,281 @@ package org.bladerunnerjs.utility;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
-import java.nio.charset.Charset;
-
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.bladerunnerjs.utility.JsCommentStrippingReader;
 import org.junit.Test;
 
 public class JsCommentStrippingReaderTest
 {
 
-	private static final String TEST_ROOT = "src/test/resources/JsCommentStrippingReaderTest";
-
 	@Test
 	public void sourceWithNoCommentsIsLeftUntouched() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("source-with-no-comments-is-left-untouched");
-
-		assertEquals(accessor.get("input-and-output.js"), stripComments(accessor.getFile("input-and-output.js")));
+		stripCommentsAndAssertEquals( 
+			lines(
+				"line1",
+				"line2" ), 
+			lines(
+				"line1",
+				"line2" ) 
+		);
 	}
 
 	@Test
 	public void singleLineCommentsAreStripped() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("single-line-comments-are-stripped");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+			lines(
+				"line 1",
+				"// comment 1",
+				"// comment 2",
+				"line 2"), 
+			lines(
+				"line 1",
+				"",
+				"",
+				"line 2") 
+		);
 	}
 
 	@Test
 	public void singleLineCommentsCanAppearMidLine() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("single-line-comments-can-appear-mid-line");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+				lines(
+					"line 1",
+					"line 2 // a comment"), 
+				lines(
+					"line 1",
+					"line 2 ") 
+			);
 	}
 
 	@Test
 	public void midLineCommentsDontEatTheNewLineCharacter() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("mid-line-comment-dont-eat-the-new-line-character");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+				lines(
+					"line 1",
+					"line 2 // a comment\n"), 
+				lines(
+					"line 1",
+					"line 2 \n") 
+			);
 	}
 
 	@Test
 	public void singleLineCommentsCanImmediatelyStartWithAForwardSlash() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("single-line-comments-can-immediately-start-with-a-forward-slash");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+				lines(
+					"/// comment"), 
+				lines(
+					"") 
+			);
 	}
 
 	@Test
 	public void multiLineCommentsAreStripped() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("multi-line-comments-are-stripped");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+				lines(
+					"line 1",
+					"/*",
+					" * comment line 1",
+					"* comment line 2",
+					" */",
+					"line 2"), 
+				lines(
+					"line 1",
+					"",
+					"line 2") 
+			);
 	}
 
 	@Test
 	public void jsdocCommentsArePreservedWhenConfigured() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("jsdoc-comments-are-preserved-when-configured");
-
-		assertEquals(accessor.get("input-and-output.js"), stripComments(accessor.getFile("input-and-output.js")));
+		stripCommentsAndAssertEquals( 
+				lines(
+					"line 1",
+					"/**",
+					 " * jsdoc line 1",
+					 " * jsdoc line 2",
+					 " */",
+					"line 2"), 
+				lines(
+					"line 1",
+					"/**",
+					 " * jsdoc line 1",
+					 " * jsdoc line 2",
+					 " */",
+					"line 2") 
+			);
 	}
 
 	@Test
 	public void jsdocCommentsAreStrippedWhenConfigured() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("jsdoc-comments-are-stripped-when-configured");
-
-		assertEquals(accessor.get("output.js"), stripAllComments(accessor.getFile("input.js")));
+		stripAllCommentsAndAssertEquals( 
+				lines(
+					"line 1",
+					"/**",
+					 " * jsdoc line 1",
+					 " * jsdoc line 2",
+					 " */",
+					"line 2"), 
+				lines(
+					"line 1",
+					"",
+					"line 2") 
+			);
 	}
 
 	@Test
 	public void singleLineCommentsCanContainAsterisks() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("single-line-comments-can-contain-asterisks");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+			lines(
+				"line 1",
+				"// *",
+				"line 2"), 
+			lines(
+				"line 1",
+				"",
+				"line 2") 
+		);
 	}
 
 	@Test
 	public void multiLineCommentsCanContainAsterisksAndSlashes() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("multi-line-comments-can-contain-asterisks-and-slashes");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+			lines(
+				"line 1",
+				"/* * / */",
+				"line 2 "),
+			lines(
+				"line 1",
+				"",
+				"line 2 ") 
+		);
 	}
 
 	@Test
 	public void singleLineCommentsCanBeEmbeddedInMultiLineComments() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("single-line-comments-can-be-embedded-in-multi-line-comments");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+			lines(
+				"line 1 /* // single-line comment within a multi-line comment */",
+				"line 2"),
+			lines(
+				"line 1 ",
+				"line 2") 
+		);
 	}
 
 	@Test
 	public void stringContentsArentInterprettedAsComments() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("string-contents-arent-interpretted-as-comments");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+			lines(
+				"'// not a comment'",
+                "\"// not a comment\"",
+                "'/* not a comment */'",
+                "\"/* not a comment */\"",
+                "// a real comment"),
+            lines(
+				"'// not a comment'",
+                "\"// not a comment\"",
+                "'/* not a comment */'",
+                "\"/* not a comment */\"",
+                "")
+		);
 	}
 
 	@Test
 	public void escapingTheCloseStringCharacterDoesntFoolTheStripper() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("escaping-the-close-string-character-doesnt-fool-the-stripper");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+			lines(
+				"'\\'// not a comment'", // double / so the line reads: '\'// not a comment'
+				"\"\\\"// not a comment\"", // triple / so the line reads: "\"// not a comment"
+				"// a real comment"),
+			lines(
+				"'\\'// not a comment'", // double / so the line reads: '\'// not a comment'
+				"\"\\\"// not a comment\"", // triple / so the line reads: "\"// not a comment"
+				"")
+		);
 	}
 
 	@Test
 	public void multiLineSinglyQuotedStringsDontExist() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("multi-line-singly-quoted-strings-dont-exist");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+			lines(
+				"'start of a string...",
+				"// comment"),
+            lines(
+        		"'start of a string...",
+				"")
+		);
 	}
 
 	@Test
 	public void multiLineDoublyQuotedStringsDontExist() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("multi-line-doubly-quoted-strings-dont-exist");
-
-		assertEquals(accessor.get("output.js"), stripComments(accessor.getFile("input.js")));
+		stripCommentsAndAssertEquals( 
+			lines(
+				"\"start of a string...",
+				"// comment"),
+            lines(
+        		"\"start of a string...",
+				"")
+		);
 	}
 
 	@Test
 	public void literalRegularExpressionsEndingWithAnEscapedForwardSlashDoesntFoolTheStripper() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("literal-regular-expressions-ending-with-an-escaped-forward-slash-doesnt-fool-the-stripper");
-
-		assertEquals(accessor.get("input-and-output.js"), stripComments(accessor.getFile("input-and-output.js")));
+		stripCommentsAndAssertEquals( 
+			lines(
+				"var oRegExp = /match\\//g;"),  // double \ so the line reads: var oRegExp = /match\//g;
+            lines(
+        		"var oRegExp = /match\\//g;")
+		);
 	}
 
 	@Test(expected = IOException.class)
 	public void macLineEndingsShouldThowException() throws IOException
 	{
-		FileAccessor accessor = new FileAccessor("mac-line-endings-should-throw-runtime-exception");
-		stripComments(accessor.getFile("input.js"));
+		stripComments( 	"some content \r" + // \r is the line ending - so this would be 2 separate lines in a file
+						"more content", true);
 	}
 
-	private String stripComments(File source) throws IOException
+	
+	private String lines(String... input)
 	{
-		return stripComments(source, true);
+		return StringUtils.join(input, "\n");
 	}
-
-	private String stripAllComments(File source) throws IOException
+	
+	private void stripCommentsAndAssertEquals(String input, String expectedOutput) throws IOException
 	{
-		return stripComments(source, false);
+		assertEquals( expectedOutput, stripComments(input, true) );
 	}
-
-	private String stripComments(File source, boolean preserveJsdoc) throws IOException
+	
+	private void stripAllCommentsAndAssertEquals(String input, String expectedOutput) throws IOException
 	{
-		try(JsCommentStrippingReader commentStrippingReader = new JsCommentStrippingReader(new FileReader(source), preserveJsdoc);
+		assertEquals( expectedOutput, stripComments(input, false) );
+	}
+	
+	private String stripComments(String input, boolean preserveJsdoc) throws IOException
+	{
+		try(JsCommentStrippingReader commentStrippingReader = new JsCommentStrippingReader(new StringReader(input), preserveJsdoc);
 		    StringWriter stringWriter = new StringWriter())
 		{
 			IOUtils.copy(commentStrippingReader, stringWriter);
@@ -173,24 +284,5 @@ public class JsCommentStrippingReaderTest
 			return stringWriter.toString();
 		}
 	}
-
-	private class FileAccessor
-	{
-		private File testResourceDir;
-
-		public FileAccessor(String testName)
-		{
-			testResourceDir = new File(TEST_ROOT, testName);
-		}
-
-		public String get(String fileName) throws IOException
-		{
-			return FileUtils.readFileToString(this.getFile(fileName), String.valueOf(Charset.defaultCharset()));
-		}
-
-		public File getFile(String fileName) throws IOException
-		{
-			return new File(testResourceDir, fileName);
-		}
-	}
+	
 }
