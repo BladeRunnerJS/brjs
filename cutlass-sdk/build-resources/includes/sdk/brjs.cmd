cb9d71e4ce98ada71f3b6015f81bc2b6b4755760
Pre Vyas <priyank.vyas@caplin.com>
2014-04-11 11:33:00 +0100

    Changing brjs internal vars of CUTLASS_CLASSPATH -> BRJS_CLASSPATH

--- cutlass-sdk/build-resources/includes/sdk/brjs.cmd
+++ cutlass-sdk/build-resources/includes/sdk/brjs.cmd
@@ -9,6 +9,6 @@ set filename="%SCRIPT_DIR%"
 FOR /F "delims=" %%I in ('echo %filename%') do set SHORT_SCRIPT_DIR=%%~sI
 
 cd %THIS_DIR%
-set CUTLASS_CLASSPATH="%SCRIPT_DIR%/libs/java/system/*;%SCRIPT_DIR%/../conf/java/*;"
+set BRJS_CLASSPATH="%SCRIPT_DIR%/libs/java/system/*;%SCRIPT_DIR%/../conf/java/*;"
 
-java %JAVA_OPTS% -cp %CUTLASS_CLASSPATH% org.bladerunnerjs.runner.CommandRunner "%SHORT_SCRIPT_DIR% " %*
+java %JAVA_OPTS% -cp %BRJS_CLASSPATH% org.bladerunnerjs.runner.CommandRunner "%SHORT_SCRIPT_DIR% " %*
