513f6fbcc83176bb18a17e3c81945c7d4ca61dcf
Dominic Chambers <dominic.chambers@gmail.com>
2014-04-15 18:22:40 +0100

    Removed the duplicated method definitions from FileInfo, that were already defined in FileIterator.

--- brjs-core/src/main/java/org/bladerunnerjs/model/FileInfo.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/FileInfo.java
@@ -1,18 +1,9 @@
 package org.bladerunnerjs.model;
 
-import java.io.File;
-import java.util.List;
-
-import org.apache.commons.io.filefilter.IOFileFilter;
 import org.bladerunnerjs.utility.FileIterator;
 import org.bladerunnerjs.utility.filemodification.FileModificationInfo;
 
 public interface FileInfo extends FileModificationInfo, FileIterator {
 	boolean exists();
 	boolean isDirectory();
-	List<File> filesAndDirs();
-	List<File> filesAndDirs(IOFileFilter fileFilter);
-	List<File> files();
-	List<File> dirs();
-	List<File> nestedFilesAndDirs();
 }
