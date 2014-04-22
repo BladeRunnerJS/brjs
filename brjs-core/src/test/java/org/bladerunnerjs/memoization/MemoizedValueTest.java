7b3cbfb39b98deac3b1cd864eb131c92e838bd3e
Dominic Chambers <dominic.chambers@gmail.com>
2014-04-22 13:03:25 +0100

    The MemoizedValue class now recomputes the value if an exception was thrown the previous time the computation was performed.

---
+++ brjs-core/src/test/java/org/bladerunnerjs/memoization/MemoizedValueTest.java
@@ -0,0 +1,90 @@
+package org.bladerunnerjs.memoization;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bladerunnerjs.model.BRJS;
+import org.bladerunnerjs.model.FileInfo;
+import org.bladerunnerjs.testing.specutility.engine.ConsoleMessageStore;
+import org.bladerunnerjs.testing.specutility.engine.ConsoleStoreWriter;
+import org.bladerunnerjs.testing.utility.LogMessageStore;
+import org.bladerunnerjs.testing.utility.MockPluginLocator;
+import org.bladerunnerjs.testing.utility.TestLoggerFactory;
+import org.bladerunnerjs.utility.FileUtility;
+import org.bladerunnerjs.utility.filemodification.OptimisticFileModificationService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MemoizedValueTest {
+	private File tempDir;
+	private File sdkDir;
+	private File watchFile;
+	private BRJS brjs;
+	private FileInfo watchFileInfo;
+	
+	@Before
+	public void setUp() throws IOException {
+		tempDir = FileUtility.createTemporaryDirectory(MemoizedValueTest.class.getSimpleName());
+		watchFile = new File(tempDir, "watch-file");
+		sdkDir = new File(tempDir, "sdk");
+		
+		sdkDir.mkdir();
+		brjs = new BRJS(sdkDir, new MockPluginLocator(), new OptimisticFileModificationService(), new TestLoggerFactory(new LogMessageStore()), new ConsoleStoreWriter(new ConsoleMessageStore()));
+		watchFileInfo = brjs.getFileInfo(watchFile);
+	}
+	
+	@After
+	public void tearDown() {
+		brjs.close();
+	}
+	
+	@Test
+	public void valueIsCalculatedOnFirstInvocationButMemoizedForSubsequentInvocations() {
+		MemoizedValue<Integer> memoizedValue = new MemoizedValue<>("id", brjs, watchFile);
+		Getter<RuntimeException> incrementingGetter = new IncrementingGetter();
+		
+		assertEquals(0, (int) memoizedValue.value(incrementingGetter));
+		assertEquals(0, (int) memoizedValue.value(incrementingGetter));
+		assertEquals(0, (int) memoizedValue.value(incrementingGetter));
+	}
+	
+	@Test
+	public void valueIsRecalculatedTheSecondTimeIfTheFileHasChanged() throws IOException {
+		MemoizedValue<Integer> memoizedValue = new MemoizedValue<>("id", brjs, watchFile);
+		Getter<RuntimeException> incrementingGetter = new IncrementingGetter();
+		
+		assertEquals(0, (int) memoizedValue.value(incrementingGetter));
+		watchFileInfo.resetLastModified();
+		assertEquals(1, (int) memoizedValue.value(incrementingGetter));
+		assertEquals(1, (int) memoizedValue.value(incrementingGetter));
+	}
+	
+	@Test
+	public void valueIsRecalculatedTheSecondTimeIfAnExceptionOcurredTheFirstTime() {
+		MemoizedValue<Integer> memoizedValue = new MemoizedValue<>("id", brjs, watchFile);
+		Getter<RuntimeException> incrementingGetter = new IncrementingGetter();
+		
+		try {
+			memoizedValue.value(() -> {throw new RuntimeException("Unexpected error!");});
+		}
+		catch(RuntimeException e) {
+		}
+		
+		assertEquals(0, (int) memoizedValue.value(incrementingGetter));
+		watchFileInfo.resetLastModified();
+		assertEquals(1, (int) memoizedValue.value(incrementingGetter));
+		assertEquals(1, (int) memoizedValue.value(incrementingGetter));
+	}
+	
+	private class IncrementingGetter implements Getter<RuntimeException> {
+		int count = 0;
+		
+		@Override
+		public Object get() {
+			return count++;
+		}
+	}
+}
