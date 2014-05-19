95b7ed15244e61ee99faf8bf699f7e425de3750c
Andy Berry <andyberry88@gmail.com>
2014-05-19 21:17:35 +0100

    'View in Workbench' button now opens workbench in a new tab

--- cutlass-sdk/workspace/sdk/system-applications/dashboard/dashboard-bladeset/blades/app/src/brjs/dashboard/app/DashboardApp.js
+++ cutlass-sdk/workspace/sdk/system-applications/dashboard/dashboard-bladeset/blades/app/src/brjs/dashboard/app/DashboardApp.js
@@ -6,19 +6,19 @@ brjs.dashboard.app.DashboardApp = function(oDashboardService, oPageUrlService, o
 	this.m_eDisplayElement = eDisplayElement;
 	this.m_oBrowserDetector = oBrowserDetector;
 	this.m_oInvalidBrowserDecider = new brjs.dashboard.app.service.browserdetector.InvalidBrowserDecider(oBrowserDetector);
-	
+
 	this.m_oPresentationModel = new brjs.dashboard.app.model.DashboardPresentationModel(oDashboardService, oPageUrlService, oWindowOpenerService, oLocalStorage, oBrowserDetector);
 	this.m_oPresenterComponent = new br.presenter.component.PresenterComponent("brjs.dashboard.app.root", this.m_oPresentationModel);
-	
+
 	// TODO: update dashboard to use new component interface.
 	// TODO: add proper frame support
 	eDisplayElement.appendChild(this.m_oPresenterComponent.getElement());
 
 	//	this.m_oPresenterComponent.onOpen();
-	
+
 	this.m_bAppsLoaded = false;
 	oPageUrlService.addPageUrlListener(this._onPageUrlUpdated.bind(this), true);
-	
+
 	this._showBrowserWarningDialogIfNeeded();
 };
 
@@ -26,7 +26,7 @@ brjs.dashboard.app.DashboardApp = function(oDashboardService, oPageUrlService, o
  * @static
  */
 brjs.dashboard.app.DashboardApp.initializeLibrary = function()
-{	
+{
 };
 
 brjs.dashboard.app.DashboardApp.prototype.getPresentationModel = function()
@@ -46,7 +46,7 @@ brjs.dashboard.app.DashboardApp.prototype.tearDown = function()
 brjs.dashboard.app.DashboardApp.prototype._showBrowserWarningDialogIfNeeded = function()
 {
 	if (!this.m_oInvalidBrowserDecider.isValidBrowser())
-	{		
+	{
 		this.m_oPresentationModel.dialog.browserWarningDialog.setMinimumBrowserVersions(this.m_oInvalidBrowserDecider.getMinimumBrowserVersions());
 		this.m_oPresentationModel.dialog.showDialog("browserWarningDialog");
 	}
@@ -55,16 +55,16 @@ brjs.dashboard.app.DashboardApp.prototype._showBrowserWarningDialogIfNeeded = fu
 brjs.dashboard.app.DashboardApp.prototype._onPageUrlUpdated = function(sPageUrl)
 {
 	this.m_oPresentationModel.dialog.visible.setValue(false);
-	
+
 	if(sPageUrl.match(/^#anchor-/))
 	{
 		// Do nothing because this is a genuine internal anchor
 	}
-	else if(sPageUrl.match(/^#apps\/.*workbench$/))
-	{
-		var pParts = sPageUrl.match(/^#apps\/(.*)workbench$/)[1].split("/");
-		this.m_oPresentationModel.workbenchScreen.displayWorkbench(pParts[0], pParts[1], pParts[2]);
-	}
+	// else if(sPageUrl.match(/^#apps\/.*workbench$/))
+	// {
+	// 	var pParts = sPageUrl.match(/^#apps\/(.*)workbench$/)[1].split("/");
+	// 	this.m_oPresentationModel.workbenchScreen.displayWorkbench(pParts[0], pParts[1], pParts[2]);
+	// }
 	else if(sPageUrl.match(/^#apps\/.*/))
 	{
 		if(!this.m_bAppsLoaded)
@@ -72,10 +72,10 @@ brjs.dashboard.app.DashboardApp.prototype._onPageUrlUpdated = function(sPageUrl)
 			this.m_bAppsLoaded = true;
 			this.m_oPresentationModel.appsScreen.updateApps();
 		}
-		
+
 		var sApp = sPageUrl.split("/")[1];
 		this.m_oPresentationModel.appDetailScreen.displayApp(sApp);
-		
+
 	}
 	else if(sPageUrl == "#note/latest")
 	{
