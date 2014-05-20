376c6c955f5dafb2924c395c33e3307179c4a5a0
Andy Berry <andyberry88@gmail.com>
2014-05-20 10:34:18 +0100

    adding an XML comment stripping readaer

---
+++ brjs-core/src/main/java/org/bladerunnerjs/utility/reader/XmlCommentStrippingReader.java
@@ -0,0 +1,96 @@
+package org.bladerunnerjs.utility.reader;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/*
+ * Note: This class has a lot of code that is duplicated with other comment stripping readers. 
+ * DO NOT try to refactor them to share a single superclass, it leads to performance overheads that have a massive impact whe bundling
+ */
+public class XmlCommentStrippingReader extends Reader
+{
+	public static final String COMMENT_START = "<!--";
+	public static final String COMMENT_END = "-->";	
+	private static final int MIN_BUFFERED_CHARS = COMMENT_START.length();
+	
+	private enum CommentStripperState
+	{
+		WITHIN_SOURCE,
+		WITHIN_COMMENT,
+		POSSIBLE_COMMENT_MATCH
+	}
+	
+	private final Reader sourceReader;
+	private final char[] sourceBuffer = new char[4096];
+	private int nextCharPos = 0;
+	private int lastCharPos = 0;
+	private CommentStripperState state;
+	
+	public XmlCommentStrippingReader(Reader sourceReader)
+	{
+		super();
+		this.sourceReader = sourceReader;
+		state = CommentStripperState.WITHIN_SOURCE;
+	}
+	
+	@Override
+	public int read(char[] destBuffer, int offset, int maxCharacters) throws IOException {
+		if(lastCharPos == -1) {
+			return -1;
+		}
+		
+		int currentOffset = offset;
+		int maxOffset = offset + maxCharacters;
+		char nextChar;
+		
+		while(currentOffset < maxOffset) {
+			if(nextCharPos == lastCharPos) {
+				nextCharPos = 0;
+				lastCharPos = sourceReader.read(sourceBuffer, 0, sourceBuffer.length - 1);
+				
+				if(lastCharPos == -1) {
+					break;
+				}
+			}
+			
+			nextChar = sourceBuffer[nextCharPos++];
+			
+			if (state == CommentStripperState.WITHIN_SOURCE) {
+				destBuffer[currentOffset++] = nextChar;
+			}
+			
+			if (nextChar == COMMENT_START.charAt(COMMENT_START.length()-1)) {
+				if (isCommentStart()) {
+					state = CommentStripperState.WITHIN_COMMENT;
+				}
+			}
+			else if (nextChar == COMMENT_END.charAt(COMMENT_END.length()-1)) {
+				if (isCommentEnd()) {
+					state = CommentStripperState.WITHIN_SOURCE;
+				}
+			}
+		}
+		
+		int charsProvided = (currentOffset - offset);
+		return (charsProvided == 0) ? -1 : charsProvided;
+	}
+	
+	private boolean isCommentStart()
+	{
+		int startPos = Math.max(0, nextCharPos - MIN_BUFFERED_CHARS);
+		String commentPrefix = new String(sourceBuffer, startPos, nextCharPos - startPos);
+		return commentPrefix.contains(COMMENT_START);
+	}
+	
+	private boolean isCommentEnd()
+	{
+		int startPos = Math.max(0, nextCharPos - MIN_BUFFERED_CHARS);
+		String commentPrefix = new String(sourceBuffer, startPos, nextCharPos - startPos);
+		return commentPrefix.contains(COMMENT_END);
+	}
+
+	@Override
+	public void close() throws IOException {
+		sourceReader.close();
+	}
+}
---
+++ brjs-core/src/test/java/org/bladerunnerjs/utility/reader/XmlCommentStrippingReaderTest.java
@@ -0,0 +1,198 @@
+package org.bladerunnerjs.utility.reader;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+public class XmlCommentStrippingReaderTest
+{
+
+	@Test
+	public void sourceWithNoCommentsIsLeftUntouched() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+			lines(
+				"line1",
+				"line2" ), 
+			lines(
+				"line1",
+				"line2" ) 
+		);
+	}
+
+	@Test
+	public void singleLineCommentsAreStripped() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+			lines(
+				"line 1",
+				"<!-- comment 1 -->",
+				"<!-- comment 2 -->",
+				"line 2"), 
+			lines(
+				"line 1",
+				"<!--",
+				"<!--",
+				"line 2") 
+		);
+	}
+
+	@Test
+	public void singleLineCommentsCanAppearMidLine() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+				lines(
+					"line 1",
+					"line 2 <!-- a comment -->"), 
+				lines(
+					"line 1",
+					"line 2 <!--") 
+			);
+	}
+
+	@Test
+	public void midLineCommentsDontEatTheNewLineCharacter() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+				lines(
+					"line 1",
+					"line 2 <!-- a comment -->\n"), 
+				lines(
+					"line 1",
+					"line 2 <!--\n") 
+			);
+	}
+
+	@Test
+	public void singleLineCommentsCanImmediatelyStartWithALeftAngledBracket() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+				lines(
+					"<!--< comment -->"), 
+				lines(
+					"<!--") 
+			);
+	}
+	
+	@Test
+	public void commentsContainingCommentsAreFiltered() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+				lines(
+					"<!--  <!-- comment -->  -->"), 
+				lines(
+					"<!--  -->") 
+			);
+	}
+
+	@Test
+	public void multiLineCommentsAreStripped() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+				lines(
+					"line 1",
+					"<!--",
+					" * comment line 1",
+					"* comment line 2",
+					" -->",
+					"line 2"), 
+				lines(
+					"line 1",
+					"<!--",
+					"line 2") 
+			);
+	}
+
+	@Test
+	public void multiLineCommentsCanContainComments() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+				lines(
+					"line 1",
+					"<!--",
+					" <!-- comment line 1",
+					"* comment line 2 -->",
+					" -->",
+					"line 2"), 
+				lines(
+					"line 1",
+					"<!--",
+					" -->",
+					"line 2") 
+			);
+	}
+	
+	@Test
+	public void invalidCommentStringsAreLeftUntouched() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+				lines(
+						"<! -- not a comment -->",
+						"<!-- a real comment -->"),
+				lines(
+						"<! -- not a comment -->",
+						"<!--")
+			);
+	}
+	
+	@Test
+	public void largeSourceWithNoCommentsIsLeftUntouched() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+			lines(
+				pad(4090),
+				"line1",
+				"line2" ), 
+			lines(
+				pad(4090),
+				"line1",
+				"line2" ) 
+		);
+	}
+	
+	@Test
+	public void largeSourceWithCommentsAreStripped() throws IOException
+	{
+		stripCommentsAndAssertEquals( 
+			lines(
+				pad(4090),
+				"<!-- comment -->",
+				"line 2"), 
+			lines(
+				pad(4090),
+				"<!--",
+				"line 2") 
+		);
+	}
+	
+	private String pad(int size) {
+		return StringUtils.leftPad("", size, ' ');
+	}
+
+	private String lines(String... input)
+	{
+		return StringUtils.join(input, "\n");
+	}
+	
+	private void stripCommentsAndAssertEquals(String input, String expectedOutput) throws IOException
+	{
+		assertEquals( expectedOutput, stripComments(input));
+	}
+	
+	private String stripComments(String input) throws IOException
+	{
+		try(Reader reader = new XmlCommentStrippingReader(new StringReader(input)); StringWriter stringWriter = new StringWriter())
+		{
+			IOUtils.copy(reader, stringWriter);
+			return stringWriter.toString();
+		}
+	}
+	
+}
