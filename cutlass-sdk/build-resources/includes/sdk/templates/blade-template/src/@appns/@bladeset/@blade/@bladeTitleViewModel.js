619fcde6716b5b8ba4e6aad2162421383e1e23c2
Andy Berry <andyberry88@gmail.com>
2014-06-12 21:03:08 +0100

    fixing some i18n properties after previous commit and improving styling for templates

--- cutlass-sdk/build-resources/includes/sdk/templates/blade-template/src/@appns/@bladeset/@blade/@bladeTitleViewModel.js
+++ cutlass-sdk/build-resources/includes/sdk/templates/blade-template/src/@appns/@bladeset/@blade/@bladeTitleViewModel.js
@@ -1,13 +1,19 @@
 'use strict';
 
 var ko = require( 'ko' );
+var i18n = require( 'br/I18n' );
+var ServiceRegistry = require( 'br/ServiceRegistry' );
 
 function @bladeTitleViewModel() {
+	this.eventHub = ServiceRegistry.getService( 'br.event-hub' );
 	this.message = ko.observable( 'Hello World!' );
+	this.helloWorldI18n = ko.observable( i18n( '@appns.@bladeset.@blade.hello.world' ) );
 }
 
 @bladeTitleViewModel.prototype.buttonClicked = function() {
 	console.log( 'button clicked' );
+	var channel = this.eventHub.channel('@blade-channel');
+	channel.trigger( 'hello-event', { some: 'Hello World!' } );
 };
 
 module.exports = @bladeTitleViewModel;
