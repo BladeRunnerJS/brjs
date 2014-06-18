4a8cefc36c81c94fbbea58650fe54719af014adb
Dominic Chambers <dominic.chambers@gmail.com>
2014-06-18 10:43:24 +0100

    Renamed MockLogger to StubLogger, and removed the additional StubLogger implementation from 'cutlass-common'.

--- brjs-core/src/main/java/org/bladerunnerjs/testing/utility/StubLoggerFactory.java
+++ brjs-core/src/main/java/org/bladerunnerjs/testing/utility/StubLoggerFactory.java
@@ -4,7 +4,7 @@ import org.bladerunnerjs.logging.Logger;
 import org.bladerunnerjs.logging.LoggerFactory;
 import org.bladerunnerjs.logging.LoggerType;
 
-public class MockLoggerFactory implements LoggerFactory {
+public class StubLoggerFactory implements LoggerFactory {
 	@Override
 	public Logger getLogger(LoggerType type, Class<?> clazz) {
 		return new MockLogger();
