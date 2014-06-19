3a789292252a028e134444db944a4276c5ba3c36
Dominic Chambers <dominic.chambers@gmail.com>
2014-06-19 15:56:42 +0100

    Updated the help command to provide information about the new flags.

--- brjs-runner/src/main/java/org/bladerunnerjs/runner/CommandRunner.java
+++ brjs-runner/src/main/java/org/bladerunnerjs/runner/CommandRunner.java
@@ -43,7 +43,7 @@ public class CommandRunner {
 		try {
 			argsParser.registerParameter(new Switch("verbose").setShortFlag('v').setLongFlag("verbose").setDefault("false").setHelp("verbose level logging"));
 			argsParser.registerParameter(new Switch("debug").setShortFlag('d').setLongFlag("debug").setDefault("false").setHelp("debug level logging"));
-			argsParser.registerParameter(new FlaggedOption("log").setLongFlag("log").setHelp("The comma delimited list of packages to show messages from, or '*' to show everything"));
+			argsParser.registerParameter(new FlaggedOption("log").setLongFlag("log").setHelp("the comma delimited list of packages to show messages from, or '*' to show everything"));
 			argsParser.registerParameter(new Switch("log-info").setLongFlag("log-info").setDefault("false").setHelp("show which class each log line comes from"));
 		}
 		catch (JSAPException e) {
