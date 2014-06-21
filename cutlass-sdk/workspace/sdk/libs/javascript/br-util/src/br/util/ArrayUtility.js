2c645b36ba8676d986233bb5e16c2a252253ae46
Jan Hancic <jan.hancic@gmail.com>
2014-06-21 11:51:45 +0100

    use correct Error class and throw error in removeArray if arg is not an array

--- cutlass-sdk/workspace/sdk/libs/javascript/br-util/src/br/util/ArrayUtility.js
+++ cutlass-sdk/workspace/sdk/libs/javascript/br-util/src/br/util/ArrayUtility.js
@@ -20,7 +20,9 @@ br.util.ArrayUtility = {}
  */
 br.util.ArrayUtility.inArray = function(pArray, vValueToFind)
 {
-	if (!Array.isArray(pArray)) throw new br.util.Error("TypeError", "ArrayUtility.inArray: IllegalArgument, ("+pArray+") is not an array.");
+	if (!Array.isArray(pArray)) {
+		throw new br.Errors.InvalidParametersError("ArrayUtility.inArray: "+pArray+" is not an array.");
+	}
 
 	// this is only true for NaN.
 	if (vValueToFind !== vValueToFind) {
@@ -47,7 +49,14 @@ br.util.ArrayUtility.inArray = function(pArray, vValueToFind)
  */
 br.util.ArrayUtility.removeItem = function(pArray, vValueToRemove)
 {
-	if (vValueToRemove !== vValueToRemove)  throw new br.util.Error("ArgumentError", "ArrayUtility.removeItem: IllegalArgument, this method cannot remove NaN from an array.");
+	if (!Array.isArray(pArray)) {
+		throw new br.Errors.InvalidParametersError("ArrayUtility.removeItem: "+pArray+" is not an array.");
+	}
+
+	if (vValueToRemove !== vValueToRemove) {
+		throw new br.Errors.InvalidParametersError("ArrayUtility.removeItem: this method cannot remove NaN from an array.");
+	}
+
 	var nItemPosition = pArray.indexOf(vValueToRemove);
 	
 	if(nItemPosition >= 0)
