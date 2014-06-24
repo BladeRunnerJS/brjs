7e2e9ec93d272133f8320fce89dd305765b9e6b3
Andy Berry <andyberry88@gmail.com>
2014-06-24 12:53:14 +0100

    fixing locale forwarder tests in IE

--- cutlass-sdk/workspace/sdk/libs/locale-forwarder/tests/test-unit/js-test-driver/tests/LocaleForwarderTest.js
+++ cutlass-sdk/workspace/sdk/libs/locale-forwarder/tests/test-unit/js-test-driver/tests/LocaleForwarderTest.js
@@ -45,20 +45,20 @@ describe("locale-forwarder", function() {
     });
 
     it("appends the locale to the end end of simple page urls", function() {
-        expect(getLocalizedPageUrl("http://acme.com/app/", "en_GB")).toBe("http://acme.com/app/en_GB/");
+        expect(getLocalizedPageUrl("http://acme.com:1337/app/", "en_GB")).toBe("http://acme.com:1337/app/en_GB/");
     });
 
     it("prepends the locale before any internal page anchor", function() {
-        expect(getLocalizedPageUrl("http://acme.com/app/#anchor", "en_GB")).toBe("http://acme.com/app/en_GB/#anchor");
+        expect(getLocalizedPageUrl("http://acme.com:1337/app/#anchor", "en_GB")).toBe("http://acme.com:1337/app/en_GB/#anchor");
     });
 
     it("prepends the locale before any query string", function() {
-        expect(getLocalizedPageUrl("http://acme.com/app/?query=1", "en_GB")).toBe("http://acme.com/app/en_GB/?query=1");
-        expect(getLocalizedPageUrl("http://acme.com/app?query=1", "en_GB")).toBe("http://acme.com/app/en_GB/?query=1");
+        expect(getLocalizedPageUrl("http://acme.com:1337/app/?query=1", "en_GB")).toBe("http://acme.com:1337/app/en_GB/?query=1");
+        expect(getLocalizedPageUrl("http://acme.com:1337/app?query=1", "en_GB")).toBe("http://acme.com:1337/app/en_GB/?query=1");
     });
 
     it("keeps the query string and anchor in the correct order", function() {
-        expect(getLocalizedPageUrl("http://acme.com/app/?query=1#anchor", "en_GB")).toBe("http://acme.com/app/en_GB/?query=1#anchor");
+        expect(getLocalizedPageUrl("http://acme.com:1337/app/?query=1#anchor", "en_GB")).toBe("http://acme.com:1337/app/en_GB/?query=1#anchor");
     });
 
 });
