248ae17d104c37eddffffb45119b99003bd71da7
Andy Berry <andyberry88@gmail.com>
2014-06-26 08:13:17 +0100

    refactoring some app metadata utils into a seperate util class

---
+++ brjs-core/src/main/java/org/bladerunnerjs/utility/ServedAppMetadataUtility.java
@@ -0,0 +1,28 @@
+package org.bladerunnerjs.utility;
+
+import org.bladerunnerjs.model.App;
+import org.bladerunnerjs.model.exception.ConfigException;
+
+import com.google.common.base.Joiner;
+
+
+public class ServedAppMetadataUtility
+{
+
+	public static String getBundlePathJsData(App app, String version) throws ConfigException {
+		return "window.$BRJS_APP_VERSION = '"+version+"';\n" +
+		"window.$BRJS_BUNDLE_PATH = '../v/"+version+"/';\n" +
+		"window.$BRJS_UNVERSIONED_BUNDLE_PATH = '../v/';\n" +
+		"window.$BRJS_LOCALE_COOKIE_NAME = '"+app.appConf().getLocaleCookieName()+"';\n" +
+		"window.$BRJS_APP_LOCALES = {'" + Joiner.on("':true, '").join(app.appConf().getLocales()) + "':true};\n";
+	}
+	
+	public static String getVersionedBundlePath(String version) {
+		return "../v/"+version+"/";
+	}
+	
+	public static String getUnversionedBundlePath() {
+		return "../v/";
+	}
+	
+}
