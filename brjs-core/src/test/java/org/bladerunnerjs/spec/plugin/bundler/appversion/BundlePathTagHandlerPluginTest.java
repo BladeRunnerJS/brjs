8899c888a5584b8392d7fc54249f648561b634db
Priyank Vyas <priyank.vyas@caplin.com>
2014-07-03 11:47:28 +0200

    #753 Adding more test coverage for workbenches - adding coverage for HTML and XML content plugin for converting @bundlePath@ correctly - adding coverage for the BundlePathTagHandlerPlugin for a workbench index page request

--- brjs-core/src/test/java/org/bladerunnerjs/spec/plugin/bundler/appversion/BundlePathTagHandlerPluginTest.java
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/plugin/bundler/appversion/BundlePathTagHandlerPluginTest.java
@@ -2,6 +2,10 @@ package org.bladerunnerjs.spec.plugin.bundler.appversion;
 
 import org.bladerunnerjs.model.App;
 import org.bladerunnerjs.model.Aspect;
+import org.bladerunnerjs.model.Blade;
+import org.bladerunnerjs.model.Bladeset;
+import org.bladerunnerjs.model.NamedDirNode;
+import org.bladerunnerjs.model.Workbench;
 import org.bladerunnerjs.testing.specutility.engine.SpecTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +16,11 @@ public class BundlePathTagHandlerPluginTest extends SpecTest
 
 	private App app;
 	private Aspect aspect;
+	private Bladeset bladeset;
+	private Blade blade;
+	private Workbench workbench;
+	private NamedDirNode workbenchTemplate;
+	
 	private StringBuffer response;
 	
 	@Before
@@ -22,6 +31,14 @@ public class BundlePathTagHandlerPluginTest extends SpecTest
 			.and(brjs).hasBeenCreated();
 			app = brjs.app("app1");
 			aspect = app.aspect("default");
+			bladeset = app.bladeset("bs");
+			blade = bladeset.blade("b1");
+			workbench = blade.workbench();
+			workbenchTemplate = brjs.template("workbench");
+			
+			given(workbenchTemplate).containsFileWithContents("index.html", "'<html>hello world</html>'")
+				.and(workbenchTemplate).containsFolder("resources")
+				.and(workbenchTemplate).containsFolder("src");
 			
 			response = new StringBuffer();
 	}
@@ -83,6 +100,14 @@ public class BundlePathTagHandlerPluginTest extends SpecTest
 		when(aspect).indexPageLoadedInProd(response, "en_GB");
 		then(response).containsText( "../some/path" )
 			.and(response).doesNotContainText("1234");
+	}
+	
+	@Test
+	public void bundlePathContentIsIncludedForWorkbenches() throws Exception {
+		given(brjs).hasDevVersion("dev")
+			.and(workbench).indexPageHasContent("<@bundle.path@/>/some/path");
+		when(workbench).pageLoaded(response, "en_GB");
+		then(response).containsText( "../v/dev/some/path" );
 	}
 	
 }
