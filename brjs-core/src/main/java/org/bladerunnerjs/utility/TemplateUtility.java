f5166d46ebf42eb70e6a59498a1ef9029af3a902
Dominic Chambers <dominic.chambers@gmail.com>
2014-07-09 14:21:15 +0100

    Start using moveDirectory() rather than copyDirectory() to atomically move directories of files into directories being watched by the model.

--- brjs-core/src/main/java/org/bladerunnerjs/utility/TemplateUtility.java
+++ brjs-core/src/main/java/org/bladerunnerjs/utility/TemplateUtility.java
@@ -46,8 +46,7 @@ public class TemplateUtility
 				transformDir(tempDir, transformations);
 			}
 			
-			
-			FileUtils.copyDirectory(tempDir, node.dir());
+			FileUtility.moveDirectoryContents(tempDir, node.dir());
 		}
 		catch(IOException e) {
 			throw new TemplateInstallationException(e);
