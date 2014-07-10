f32d156a99b53e342b584bb530f55149e2f5d6ca
Andy Berry <andyberry88@gmail.com>
2014-07-10 09:02:29 +0100

    using full classnames in classes nav listing

--- brjs-sdk/build-resources/includes/sdk/jsdoc-toolkit-resources/brjs-template/publish.js
+++ brjs-sdk/build-resources/includes/sdk/jsdoc-toolkit-resources/brjs-template/publish.js
@@ -320,7 +320,7 @@ function buildNav(members) {
     if (members.classes.length) {
         members.classes.forEach(function(c) {
             if ( !hasOwnProp.call(seen, c.longname) ) {
-                classNav += '<li>' + linkto(c.longname, c.name) + '</li>';
+                classNav += '<li>' + linkto(c.longname, c.longname) + '</li>';
             }
             seen[c.longname] = true;
         });
