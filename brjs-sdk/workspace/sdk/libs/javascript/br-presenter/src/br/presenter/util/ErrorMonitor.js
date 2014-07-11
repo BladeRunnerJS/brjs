4a9adf26e7f56ae3cb4cb818046333732844c780
yasserf <yasser.fadl@caplin.com>
2014-07-11 17:53:53 +0200

    Changing ErrorMonitor spaces to tabs for consistency

--- brjs-sdk/workspace/sdk/libs/javascript/br-presenter/src/br/presenter/util/ErrorMonitor.js
+++ brjs-sdk/workspace/sdk/libs/javascript/br-presenter/src/br/presenter/util/ErrorMonitor.js
@@ -1,6 +1,6 @@
 /**
  * ErrorMonitor is responsible for monitoring the status of fields that contain
- * an error and failure message property. It is used to set the tool tip class that 
+ * an error and failure message property. It is used to set the tool tip class that
  * the tooltip control will scan in order to create the tool tip box.
  *
  * @constructor
@@ -9,26 +9,26 @@
 br.presenter.util.ErrorMonitor = function(oTooltipNode)
 {
 	if (!oTooltipNode || !(oTooltipNode instanceof br.presenter.node.ToolTipNode))
-    {
-    	throw new br.Errors.CustomError(br.Errors.INVALID_PARAMETERS, "The ErrorMonitor has to be constructed with an instance of a br.presenter.node.ToolTipNode");
-    } 
-	
+	{
+		throw new br.Errors.CustomError(br.Errors.INVALID_PARAMETERS, "The ErrorMonitor has to be constructed with an instance of a br.presenter.node.ToolTipNode");
+	}
+
 	this.m_oPropertyHelper = new br.presenter.property.PropertyHelper();
 
 	this.oTooltipNode = oTooltipNode;
-	
+
 	this.m_pErrorStack = [];
 };
 
 br.presenter.util.ErrorMonitor.prototype.addErrorListeners = function(pGroups)
 {
-    for (var i=0; i<pGroups.length; i++)
-    {
-        if(pGroups[i].hasError && pGroups[i].failureMessage)
-        {
-            this.monitorField(pGroups[i]);
-        }
-    }
+	for (var i=0; i<pGroups.length; i++)
+	{
+		if(pGroups[i].hasError && pGroups[i].failureMessage)
+		{
+			this.monitorField(pGroups[i]);
+		}
+	}
 };
 
 /**
@@ -37,19 +37,19 @@ br.presenter.util.ErrorMonitor.prototype.addErrorListeners = function(pGroups)
  */
 br.presenter.util.ErrorMonitor.prototype.removeErrorListeners = function(pGroups)
 {
-    for (var i = 0; i < pGroups.length; i++)
-    {
-        if(pGroups[i].hasError && pGroups[i].failureMessage)
-        {
-            this.forgetField(pGroups[i]);
-        }
-    }
+	for (var i = 0; i < pGroups.length; i++)
+	{
+		if(pGroups[i].hasError && pGroups[i].failureMessage)
+		{
+			this.forgetField(pGroups[i]);
+		}
+	}
 };
 
 br.presenter.util.ErrorMonitor.prototype.replaceErrorListeners = function(pGroups)
 {
-    this.removeAllErrors();
-    this.addErrorListeners(pGroups);
+	this.removeAllErrors();
+	this.addErrorListeners(pGroups);
 };
 
 /**
@@ -61,28 +61,28 @@ br.presenter.util.ErrorMonitor.prototype.replaceErrorListeners = function(pGroup
 br.presenter.util.ErrorMonitor.prototype.monitorField = function(oField)
 {
 	if (!oField instanceof br.presenter.node.ToolTipField)
-    {
-    	throw new br.Errors.CustomError(br.Errors.INVALID_PARAMETERS, "The field to monitor has to be an instance of br.presenter.node.ToolTipField");
-    }
-	
+	{
+		throw new br.Errors.CustomError(br.Errors.INVALID_PARAMETERS, "The field to monitor has to be an instance of br.presenter.node.ToolTipField");
+	}
+
 	var oTicketErrorProperty = this;
-    var fValidationSuccessHandler = function()
-    {
-    	if(!this.hasError.getValue())
-        {
-            oTicketErrorProperty._removeError(this);
-        }
-    };
+	var fValidationSuccessHandler = function()
+	{
+		if(!this.hasError.getValue())
+		{
+			oTicketErrorProperty._removeError(this);
+		}
+	};
 
-    this.m_oPropertyHelper.addChangeListener(oField.hasError, oField, fValidationSuccessHandler);
-    this.m_oPropertyHelper.addValidationErrorListener(oField.value, oField, this._addError.bind(this, oField));
+	this.m_oPropertyHelper.addChangeListener(oField.hasError, oField, fValidationSuccessHandler);
+	this.m_oPropertyHelper.addValidationErrorListener(oField.value, oField, this._addError.bind(this, oField));
 
-    //We don't put a notify immediately on the add listeners as it triggers a force revalidation which is not needed
-    //for async validation
-    if(oField.hasError.getValue() && oField.failureMessage.getValue())
-    {
-    	this._addError(oField);
-    }
+	//We don't put a notify immediately on the add listeners as it triggers a force revalidation which is not needed
+	//for async validation
+	if(oField.hasError.getValue() && oField.failureMessage.getValue())
+	{
+		this._addError(oField);
+	}
 };
 
 /**
@@ -93,61 +93,61 @@ br.presenter.util.ErrorMonitor.prototype.monitorField = function(oField)
  */
 br.presenter.util.ErrorMonitor.prototype.forgetField = function(oField)
 {
-    this._removeError(oField);
-    this.m_oPropertyHelper.clearProperty(oField.hasError);
-    this.m_oPropertyHelper.clearProperty(oField.value);
+	this._removeError(oField);
+	this.m_oPropertyHelper.clearProperty(oField.hasError);
+	this.m_oPropertyHelper.clearProperty(oField.value);
 };
 
 br.presenter.util.ErrorMonitor.prototype.removeAllErrors = function()
 {
-    this.oTooltipNode.move(false);
-    this.m_pErrorStack = [];
-    this.m_oPropertyHelper.removeAllListeners();
-    this._removeLastError();
+	this.oTooltipNode.move(false);
+	this.m_pErrorStack = [];
+	this.m_oPropertyHelper.removeAllListeners();
+	this._removeLastError();
 };
 
 br.presenter.util.ErrorMonitor.prototype._addError = function(oField)
 {
 	this._removeFromStack(oField);
 
-    var sFailureMessage = oField.failureMessage.getValue();
-    this.m_pErrorStack.push({"field": oField, "failureMessage": sFailureMessage});
+	var sFailureMessage = oField.failureMessage.getValue();
+	this.m_pErrorStack.push({"field": oField, "failureMessage": sFailureMessage});
 
-    var nTopOfStack = this.m_pErrorStack.length;
-    if (nTopOfStack === 1)
-    {
-        this._addTooltipToTopOfStack();
-    }
-    else //if (this.m_pErrorStack.length > 1)
-    {
-        var oFieldWithTooltipToRemove = this.m_pErrorStack[nTopOfStack - 2].field;
-        this._moveTooltip(oFieldWithTooltipToRemove);
-    }
+	var nTopOfStack = this.m_pErrorStack.length;
+	if (nTopOfStack === 1)
+	{
+		this._addTooltipToTopOfStack();
+	}
+	else //if (this.m_pErrorStack.length > 1)
+	{
+		var oFieldWithTooltipToRemove = this.m_pErrorStack[nTopOfStack - 2].field;
+		this._moveTooltip(oFieldWithTooltipToRemove);
+	}
 
-    this.oTooltipNode.setMessage(sFailureMessage);
+	this.oTooltipNode.setMessage(sFailureMessage);
 	this._notifyObserversOfErrorChange();
 };
 
 br.presenter.util.ErrorMonitor.prototype._removeError = function(oField)
 {
 	var mFieldAndMessage = this._removeFromStack(oField);
-    if (mFieldAndMessage)
-    {
-        var oFieldNoLongerInError = mFieldAndMessage.field;
+	if (mFieldAndMessage)
+	{
+		var oFieldNoLongerInError = mFieldAndMessage.field;
 
-        if (this.m_pErrorStack.length > 0)
-        {
-            this._updateTooltipOnRemove(oFieldNoLongerInError);
-            this._updateErrorMessage();
-        }
-        else
-        {
-            this._removeTooltipFrom(oFieldNoLongerInError);
-            this._removeLastError();
-        }
+		if (this.m_pErrorStack.length > 0)
+		{
+			this._updateTooltipOnRemove(oFieldNoLongerInError);
+			this._updateErrorMessage();
+		}
+		else
+		{
+			this._removeTooltipFrom(oFieldNoLongerInError);
+			this._removeLastError();
+		}
 
-        this._notifyObserversOfErrorChange();
-    }
+		this._notifyObserversOfErrorChange();
+	}
 };
 
 /**
@@ -174,7 +174,7 @@ br.presenter.util.ErrorMonitor.prototype._removeFromStack = function(oField)
  */
 br.presenter.util.ErrorMonitor.prototype._notifyObserversOfErrorChange = function()
 {
-    this.oTooltipNode.move(false);
+	this.oTooltipNode.move(false);
 	this.oTooltipNode.move(this.m_pErrorStack.length !== 0);
 };
 
@@ -259,5 +259,5 @@ br.presenter.util.ErrorMonitor.prototype._addTooltipTo = function(oField)
  */
 br.presenter.util.ErrorMonitor.prototype._removeTooltipFrom = function(oField)
 {
-        oField.tooltipClassName.setValue("")
+		oField.tooltipClassName.setValue("")
 };
