2877c2e6277532d88779f679f71abf0f4a5b9903
Jan Hancic <jan.hancic@gmail.com>
2014-07-14 19:59:42 +0100

    Fix failing `services` tests.
    The tests ware failing due to #782

--- brjs-sdk/workspace/sdk/libs/javascript/br-services/tests/test-unit/js-test-driver/tests/BRHtmlResourceServiceTest.js
+++ brjs-sdk/workspace/sdk/libs/javascript/br-services/tests/test-unit/js-test-driver/tests/BRHtmlResourceServiceTest.js
@@ -1,18 +1,32 @@
+
+var BRHtmlResourceService = require('br/services/html/BRHtmlResourceService');
+var ServiceRegistry = require('br/ServiceRegistry');
+
 BRHtmlResourceServiceTest = TestCase("BRHtmlResourceServiceTest");
 
 BRHtmlResourceServiceTest.prototype.test_scriptTagsAreParsedByTheBrowser = function()
 {
-	var oService = br.ServiceRegistry.getService("br.html-service");
+	var oService = ServiceRegistry.getService("br.html-service");
 	var eTemplate = oService.getHTMLTemplate('br.services.testing-template');
 	assertEquals(eTemplate.innerHTML.toLowerCase(), "<div>script</div>");
 };
 
 BRHtmlResourceServiceTest.prototype.test_templatesInBundle = function()
 {
-	var oService = br.ServiceRegistry.getService("br.html-service");
+	var oService = ServiceRegistry.getService("br.html-service");
 	assertEquals(oService.getHTMLTemplate("br.services.template1").innerHTML.toLowerCase(), "some html1");
 	assertEquals(oService.getHTMLTemplate("br.services.template2").innerHTML.toLowerCase(), "some html2");
 };
+
+/* Failing in IE8 resulting in red build. See #678
+BRHtmlResourceServiceTest.prototype.test_templatesInTemplateTagBundle = function()
+{
+	assertTemplateContentsMatch("br.services.template3", "some html3");
+	assertTemplateContentsMatch("br.services.template4", "<div>some html4</div>");
+	assertTemplateContentsMatch("br.services.template5", "<div>some html5</div><div>some html51</div>");
+	assertTemplateContentsMatch("br.services.template6", "<div>some html6<div>some html61</div></div>");
+};
+*/
 
 BRHtmlResourceServiceTest.prototype.test_loadHTMLFilesDoesNotExist = function()
 {
@@ -38,14 +52,14 @@ BRHtmlResourceServiceTest.prototype.test_loadHTMLFilesDoesNotExist = function()
 };
 
 /**
- *
+ * 
  */
 var assertTemplateContentsMatch = (function(){
-	var oService = br.ServiceRegistry.getService("br.html-service");
+	var oService = ServiceRegistry.getService("br.html-service");
 	var tempDiv = document.createElement("div"); // Needed as you cannot call innerHTML on a document fragment.
 	return function assertTemplateContentsMatch(templateId, expected) {
 		var templateDocFrag = oService.getHTMLTemplate(templateId);
-
+		
 		tempDiv.innerHTML = "";
 		tempDiv.appendChild(templateDocFrag);
 		assertEquals(expected, tempDiv.innerHTML.toLowerCase());
@@ -54,5 +68,5 @@ var assertTemplateContentsMatch = (function(){
 
 BRHtmlResourceServiceTest.getService = function(sUrl)
 {
-	return new br.services.html.BRHtmlResourceService(sUrl);
+	return new BRHtmlResourceService(sUrl);
 };
