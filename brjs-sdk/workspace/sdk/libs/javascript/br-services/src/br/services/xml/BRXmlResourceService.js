733f96a8741021580ecea1daf8849571d8c73bcf
thecapdan <daniel.owen@caplin.com>
2014-07-19 16:19:01 +0100

    minor correction to incorrect parameter name following work done on #803 that caused failing js tests

--- brjs-sdk/workspace/sdk/libs/javascript/br-services/src/br/services/xml/BRXmlResourceService.js
+++ brjs-sdk/workspace/sdk/libs/javascript/br-services/src/br/services/xml/BRXmlResourceService.js
@@ -18,7 +18,7 @@ var i18n = require('br/I18n');
 * @param {String} sUrl The url to load to retrieve the XML resource.
 * @implements module:br/services/XmlResourceService
 */
-function BRXmlResourceService(sUrl) {
+function BRXmlResourceService(url) {
 	var ServiceRegistry = require("br/ServiceRegistry");
 	/** @private */
 	this.url = url || ServiceRegistry.getService('br.app-meta-service').getVersionedBundlePath("xml/bundle.xml");
