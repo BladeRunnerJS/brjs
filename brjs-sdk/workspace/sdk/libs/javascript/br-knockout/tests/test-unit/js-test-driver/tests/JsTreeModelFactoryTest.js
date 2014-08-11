0802e257fc19fa3b73848e46088706e7ce1feaa2
Dominic Chambers <dominic.chambers@gmail.com>
2014-08-11 17:39:31 +0100

    Added missing view-model to tree model tests.

--- brjs-sdk/workspace/sdk/libs/javascript/br-knockout/tests/test-unit/js-test-driver/tests/JsTreeModelFactoryTest.js
+++ brjs-sdk/workspace/sdk/libs/javascript/br-knockout/tests/test-unit/js-test-driver/tests/JsTreeModelFactoryTest.js
@@ -18,4 +18,27 @@
 		
 		assertEquals([{text: 'Knockout View Model', state:{opened: true}, children: [{text: 'foo: 42'}]}], treeModel.core.data);
 	};
+	
+	JsTreeModelFactoryTest.prototype.testViewModelWithOneNonObservable = function() {
+		var treeModel = jsTreeModelFactory.createTreeModelFromKnockoutViewModel({foo: 42});
+		
+		assertEquals([{text: 'Knockout View Model', state:{opened: true}, children: [{text: 'foo: 42'}]}], treeModel.core.data);
+	};
+	
+	JsTreeModelFactoryTest.prototype.testViewModelWithTwoObservables = function() {
+		var treeModel = jsTreeModelFactory.createTreeModelFromKnockoutViewModel({foo:ko.observable('value #1'), bar:ko.observable('value #2')});
+		
+		assertEquals([{text: 'Knockout View Model', state:{opened: true}, children: [{text: 'foo: value #1'}, {text: 'bar: value #2'}]}], treeModel.core.data);
+	};
+	
+	JsTreeModelFactoryTest.prototype.testViewModelWithAnObjectContainingObservables = function() {
+		var treeModel = jsTreeModelFactory.createTreeModelFromKnockoutViewModel({obj: {foo:ko.observable('value #1'), bar:ko.observable('value #2')}});
+		
+		assertEquals([{text: 'Knockout View Model', state:{opened: true}, children: [
+			{text: 'obj', state:{opened: true}, children: [
+				{text: 'foo: value #1'},
+				{text: 'bar: value #2'}
+			]}
+		]}], treeModel.core.data);
+	};
 })();
