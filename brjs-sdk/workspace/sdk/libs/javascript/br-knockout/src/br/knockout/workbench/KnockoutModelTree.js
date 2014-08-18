ce51ed1887370bb7e687aaaeafc3ff1aa2a51f92
thecapdan <daniel.owen@caplin.com>
2014-08-18 11:09:02 +0100

    #875 adding comment to explain presence of empty classes

--- brjs-sdk/workspace/sdk/libs/javascript/br-knockout/src/br/knockout/workbench/KnockoutModelTree.js
+++ brjs-sdk/workspace/sdk/libs/javascript/br-knockout/src/br/knockout/workbench/KnockoutModelTree.js
@@ -1,3 +1,7 @@
+/*
+ * This empty constructor is necessary to maintain backwards compatibility for Workbenches constructed before brjs v0.12. see issue #875
+ */
+
 "use strict";
 
 function KnockoutModelTree() {
