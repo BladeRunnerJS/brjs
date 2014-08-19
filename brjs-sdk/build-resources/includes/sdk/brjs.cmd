32b09086eefe52a2d1500dfa644e0de6346d2b95
Andy Berry <andyberry88@gmail.com>
2014-08-19 12:23:39 +0100

    adding java install and version checking to windows script

--- brjs-sdk/build-resources/includes/sdk/brjs.cmd
+++ brjs-sdk/build-resources/includes/sdk/brjs.cmd
@@ -1,5 +1,29 @@
 @echo off
 
+for /f %%j in ("java.exe") do (
+    set FOUND_JAVA_EXEC=%%~dp$PATH:j
+)
+
+if %FOUND_JAVA_EXEC%.==. (
+	set JAVA_INSTALL_EXCEPTION_MESSAGE=Java is either not installed or not available on the path.
+    goto displayJavaInstallMessage
+)
+
+set REQUIRED_JAVA_VERSION=1.7
+java -version:%REQUIRED_JAVA_VERSION%+ -version > nul 2>&1
+if %ERRORLEVEL% == 0 goto runBrjs
+set JAVA_INSTALL_EXCEPTION_MESSAGE=Could not find Java %REQUIRED_JAVA_VERSION% or greater.
+goto displayJavaInstallMessage
+
+
+:displayJavaInstallMessage
+echo %JAVA_INSTALL_EXCEPTION_MESSAGE%
+echo Please install Java 7 or Java 8 and ensure it's available on the path.
+echo You can find more information about how to do this at 'http://bladerunnerjs.org/docs/use/install/'.
+exit /B 1
+
+
+:runBrjs
 set THIS_DIR=%CD%
 set SCRIPT_DIR=%~dp0
 cd %SCRIPT_DIR%
