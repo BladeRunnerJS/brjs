a30448de5e781af90306b8a4884a04b182da32af
thecapdan <daniel.owen@caplin.com>
2014-08-20 15:05:28 +0100

    #872 capitalising PATH + fixing comparison when path to java contains spaces + fix for issues when multiple version of java exist

--- brjs-sdk/build-resources/includes/sdk/brjs.cmd
+++ brjs-sdk/build-resources/includes/sdk/brjs.cmd
@@ -4,8 +4,8 @@ for /f %%j in ("java.exe") do (
     set FOUND_JAVA_EXEC=%%~dp$PATH:j
 )
 
-if %FOUND_JAVA_EXEC%.==. (
-	set JAVA_INSTALL_EXCEPTION_MESSAGE=Java is either not installed or not available on the path.
+if "%FOUND_JAVA_EXEC%"=="" (
+	set JAVA_INSTALL_EXCEPTION_MESSAGE=Java is either not installed or not available on the PATH.
     goto displayJavaInstallMessage
 )
 
@@ -18,7 +18,7 @@ goto displayJavaInstallMessage
 
 :displayJavaInstallMessage
 echo %JAVA_INSTALL_EXCEPTION_MESSAGE%
-echo Please install Java 7 or Java 8 and ensure it's available on the path.
+echo Please install Java 7 or Java 8 and ensure it's available on the PATH.
 echo You can find more information about how to do this at 'http://bladerunnerjs.org/docs/use/install/'.
 exit /B 1
 
@@ -33,4 +33,4 @@ FOR /F "delims=" %%I in ('echo %filename%') do set SHORT_SCRIPT_DIR=%%~sI
 cd %THIS_DIR%
 set BRJS_CLASSPATH="%SCRIPT_DIR%/libs/java/system/*;%SCRIPT_DIR%/../conf/java/*;"
 
-java %JAVA_OPTS% -cp %BRJS_CLASSPATH% org.bladerunnerjs.runner.CommandRunner "%SHORT_SCRIPT_DIR% " %*
+java %JAVA_OPTS% -version:%REQUIRED_JAVA_VERSION%+ -cp %BRJS_CLASSPATH% org.bladerunnerjs.runner.CommandRunner "%SHORT_SCRIPT_DIR% " %*
