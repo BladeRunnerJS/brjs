bd11d0857eb5ce5cceb76fe29e37e105adee2277
Andy Berry <andyberry88@gmail.com>
2014-09-03 15:51:00 +0100

    fixing failing tests now build-app doesnt create the extra app directory

--- brjs-core/src/test/java/org/bladerunnerjs/spec/plugin/bundler/composite/CompositeJsContentPluginTest.java
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/plugin/bundler/composite/CompositeJsContentPluginTest.java
@@ -137,9 +137,9 @@ public class CompositeJsContentPluginTest extends SpecTest {
 			.and(defaultAspect).hasClass("appns/Class")
 			.and(brjs).hasProdVersion("1234")
 			.and(app).hasBeenBuilt(targetDir);
-		then(targetDir).containsFileWithContents("app1/en/index.html", "v/1234/js/prod/combined/bundle.js")
-			.and(targetDir).containsFile("app1/v/1234/js/prod/combined/bundle.js")
-			.and(targetDir).doesNotContainFile("app1/v/1234/js/prod/closure-whitespace/bundle.js");
+		then(targetDir).containsFileWithContents("en/index.html", "v/1234/js/prod/combined/bundle.js")
+			.and(targetDir).containsFile("v/1234/js/prod/combined/bundle.js")
+			.and(targetDir).doesNotContainFile("v/1234/js/prod/closure-whitespace/bundle.js");
 	}
 	
 }
