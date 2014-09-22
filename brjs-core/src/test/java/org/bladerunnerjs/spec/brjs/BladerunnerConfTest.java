e9570eb79369f5ccf6226940de9ddd29454a0743
Andy Berry <andyberry88@gmail.com>
2014-09-22 12:41:18 +0100

    fixing spec tests after brjs.conf was changed again

--- brjs-core/src/test/java/org/bladerunnerjs/spec/brjs/BladerunnerConfTest.java
+++ brjs-core/src/test/java/org/bladerunnerjs/spec/brjs/BladerunnerConfTest.java
@@ -17,7 +17,7 @@ public class BladerunnerConfTest extends SpecTest {
 	@Test
 	public void bladerunnerConfWillHaveSensibleDefaultsIfItDoesntAlreadyExist() throws Exception {
 		when(brjs).bladerunnerConf().write();
-		then(brjs).fileHasContents("conf/brjs.conf", "defaultFileCharacterEncoding: UTF-8\nignoredPaths: .svn, .git\njettyPort: 7070\nloginRealm: BladeRunnerLoginRealm\nnodePath: ");
+		then(brjs).fileHasContents("conf/brjs.conf", "defaultFileCharacterEncoding: UTF-8\nignoredPaths: .svn, .git\njettyPort: 7070\nloginRealm: BladeRunnerLoginRealm");
 	}
 	
 	@Test
@@ -30,7 +30,7 @@ public class BladerunnerConfTest extends SpecTest {
 	public void bladerunnerConfThatAlreadyExistsCanBeReadAndModified() throws Exception {
 		given(brjs).containsFileWithContents("conf/brjs.conf", "defaultFileCharacterEncoding: UTF-8\njettyPort: 7070\nloginRealm: BladeRunnerLoginRealm");
 		when(brjs).bladerunnerConf().setJettyPort(8888).setDefaultFileCharacterEncoding("ISO-8859-1").write();
-		then(brjs).fileHasContents("conf/brjs.conf", "defaultFileCharacterEncoding: ISO-8859-1\nignoredPaths: .svn, .git\njettyPort: 8888\nloginRealm: BladeRunnerLoginRealm\nnodePath: ");
+		then(brjs).fileHasContents("conf/brjs.conf", "defaultFileCharacterEncoding: ISO-8859-1\nignoredPaths: .svn, .git\njettyPort: 8888\nloginRealm: BladeRunnerLoginRealm");
 	}
 	
 	@Test
