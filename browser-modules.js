8a9e83a2dc32eb7c020a9adad3e9e678e462fa86
Dominic Chambers <dominic.chambers@gmail.com>
2014-10-14 10:15:34 +0100

    Added a Function.bind() polyfill to browser-modules so the tests pass in PhantomJs and IE8.

--- browser-modules.js
+++ browser-modules.js
@@ -21,6 +21,32 @@
 	 * this is likely to be a problem, you might want to avoid calling .install().
 	 */
 
+	// bind() polyfill taken from <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind>
+	if (!Function.prototype.bind) {
+		Function.prototype.bind = function(oThis) {
+			if (typeof this !== 'function') {
+				// closest thing possible to the ECMAScript 5
+				// internal IsCallable function
+				throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+			}
+
+			var aArgs = Array.prototype.slice.call(arguments, 1),
+					fToBind = this,
+					fNOP = function() {},
+					fBound = function() {
+						return fToBind.apply(this instanceof fNOP && oThis
+							? this
+							: oThis,
+							aArgs.concat(Array.prototype.slice.call(arguments)));
+					};
+
+			fNOP.prototype = this.prototype;
+			fBound.prototype = new fNOP();
+
+			return fBound;
+		};
+	}
+
 	var global = Function("return this;")();
 	
 	var create = Object.create || function(proto, attributes) {
