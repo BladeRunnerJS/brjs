a45dfe869a135190c2f5cec9395d6e2020d3f599
Andy Berry <andyberry88@gmail.com>
2014-10-16 16:07:27 +0100

    adding methods from FileIterator on to MemoizedFile

--- brjs-core/src/main/java/org/bladerunnerjs/memoization/MemoizedFile.java
+++ brjs-core/src/main/java/org/bladerunnerjs/memoization/MemoizedFile.java
@@ -1,8 +1,18 @@
 package org.bladerunnerjs.memoization;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.FileFileFilter;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.bladerunnerjs.model.engine.RootNode;
+
 
 public class MemoizedFile extends File
 {
@@ -10,15 +20,17 @@ public class MemoizedFile extends File
 	private MemoizedValue<Boolean> exists;
 	private MemoizedValue<Boolean> isDirectory;
 	private MemoizedValue<Boolean> isFile;
-	private MemoizedValue<File[]> listFiles;
+	private MemoizedValue<List<File>> filesAndDirs;
+	private RootNode rootNode;
 	
 	public MemoizedFile(RootNode rootNode, String file) {
 		super(file);
+		this.rootNode = rootNode;
 		String className = this.getClass().getSimpleName();
 		exists = new MemoizedValue<>(className+".exists", rootNode, this);
 		isDirectory = new MemoizedValue<>(className+".isDirectory", rootNode, this);
 		isFile = new MemoizedValue<>(className+".isFile", rootNode, this);
-		listFiles = new MemoizedValue<>(className+".listFiles", rootNode, this);
+		filesAndDirs = new MemoizedValue<>(className+".filesAndDirs", rootNode, this);
 	}
 	
 	public MemoizedFile(RootNode rootNode, File file)
@@ -31,37 +43,135 @@ public class MemoizedFile extends File
 		this( rootNode, new File(parent, child).getAbsolutePath() );		
 	}
 	
+	
+	// ---- Methods Using Memoized Values ----
+	
+	@Override
 	public boolean exists() {
 		return exists.value(() -> {
 			return super.exists();
 		});
 	}
 	
+	@Override
 	public boolean isDirectory() {
 		return isDirectory.value(() -> {
 			return super.isDirectory();
 		});
 	}
 	
+	@Override
 	public boolean isFile() {
 		return isFile.value(() -> {
 			return super.isFile();
 		});
 	}
 	
-	public File[] listFiles() {
-		return listFiles.value(() -> {
-			return super.listFiles();
-		});
+	public List<File> filesAndDirs(IOFileFilter fileFilter) {
+		List<File> returnedFilesAndDirsCopy = new ArrayList<>();
+		returnedFilesAndDirsCopy.addAll( filesAndDirs.value(() -> {
+			List<File> returnedFilesAndDirs = new ArrayList<>();
+			for (File file : super.listFiles()) {
+				if (fileFilter.accept(file)) {
+					returnedFilesAndDirs.add(file);				
+				}
+			}
+			return returnedFilesAndDirs;
+		}) );
+		return returnedFilesAndDirsCopy; // return a copy so multiple callers dont have the same object by reference
 	}
 	
-	public String[] list() {
-		File[] files = listFiles();
-		String[] names = new String[files.length];
-		for (int i = 0; i < files.length; i++) {
-			names[i] = files[i].getName();
+	// ---- End Methods Using Memoized Values ----
+	
+	@Override
+	public File[] listFiles(FileFilter filter) {
+		List<File> listedFiles = new ArrayList<>();
+		List<File> filesAndDirs = filesAndDirs();
+		for (File file : filesAndDirs) {
+			if (file.isDirectory() && filter.accept(file)) {
+				listedFiles.add(file);
+			}
 		}
-		return names;
+		return listedFiles.toArray(new File[0]);
+	}
+	
+	@Override
+	public File[] listFiles(FilenameFilter filter) {
+		return listFiles( (FileFilter) FileFilterUtils.asFileFilter(filter) );
+	}
+	
+	@Override
+	public File[] listFiles() {
+		return listFiles( (FileFilter) TrueFileFilter.INSTANCE);
+	}
+	
+	
+	
+	
+	@Override
+	public String[] list(FilenameFilter filter) {
+		List<String> listedNames = new ArrayList<>();
+		List<File> filesAndDirs = filesAndDirs();
+		for (File file : filesAndDirs) {
+			if (file.isDirectory() && filter.accept(file.getParentFile(), file.getName())) {
+				listedNames.add(file.getName());
+			}
+		}
+		return listedNames.toArray(new String[0]);
+	}
+	
+	@Override
+	public String[] list() {
+		return list( TrueFileFilter.INSTANCE );
+	}	
+	
+	public List<File> filesAndDirs() {
+		return filesAndDirs(TrueFileFilter.INSTANCE);
+	}
+	
+	public List<File> files() {
+		return filesAndDirs(FileFileFilter.FILE);
+	}
+	
+	public List<File> dirs() {
+		return filesAndDirs(DirectoryFileFilter.DIRECTORY);
+	}
+	
+	public List<File> nestedFilesAndDirs() {
+		List<File> nestedFilesAndDirs = new ArrayList<>();
+		populateNestedFilesAndDirs(this, nestedFilesAndDirs);
+		return nestedFilesAndDirs;
+	}
+	
+	public List<File> nestedFiles() {
+		List<File> nestedFiles = new ArrayList<>();
+		for(File file : nestedFilesAndDirs()) {
+			if(!file.isDirectory()) {
+				nestedFiles.add(file);
+			}
+		}
+		return nestedFiles;
+	}
+	
+	public List<File> nestedDirs() {
+		List<File> nestedDirs = new ArrayList<>();
+		for(File file : nestedFilesAndDirs()) {
+			if(file.isDirectory()) {
+				nestedDirs.add(file);
+			}
+		}
+		return nestedDirs;
+	}
+	
+	
+	
+	private void populateNestedFilesAndDirs(MemoizedFile file, List<File> nestedFilesAndDirs) {
+		nestedFilesAndDirs.addAll(file.filesAndDirs());
+		
+		for(File dir : file.dirs()) {
+			MemoizedFile memoizedFile = rootNode.getMemoizedFile(dir);
+			populateNestedFilesAndDirs(memoizedFile, nestedFilesAndDirs);
+		}
 	}
 	
 }
