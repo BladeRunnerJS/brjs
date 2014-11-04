f3087fbc4f335eeae6bd6f34780a92fac1c9f207
Andy Berry <andyberry88@gmail.com>
2014-11-04 13:23:47 +0000

    removing a redundant 'getMemoizedFile' call

--- brjs-core/src/main/java/org/bladerunnerjs/plugin/utility/AssetLocator.java
+++ brjs-core/src/main/java/org/bladerunnerjs/plugin/utility/AssetLocator.java
@@ -1,6 +1,5 @@
 package org.bladerunnerjs.plugin.utility;
 
-import java.io.File;
 import java.util.TreeMap;
 import java.util.List;
 import java.util.Map;
@@ -27,21 +26,20 @@ public class AssetLocator {
 		assetsList = new MemoizedValue<>(assetLocation.dir()+" - AssetLocation.assets", assetLocation.root(), assetLocation.root().dir());
 	}
 	
-	public Assets assets(List<? extends File> assetFiles) {
+	public Assets assets(List<? extends MemoizedFile> assetFiles) {
 		return assetsList.value(new Getter<RuntimeException>() {
 			@Override
 			public Object get() {
 				Assets assets = new Assets(assetLocation.root());
 				
-				for(File assetFile : assetFiles) {
-					MemoizedFile memoizedFile = assetLocation.root().getMemoizedFile(assetFile);
+				for(MemoizedFile assetFile : assetFiles) {
 					for(AssetPlugin assetPlugin : assetLocation.root().plugins().assetPlugins()) {
-						if(assetPlugin.canHandleAsset(memoizedFile, assetLocation)) {
+						if(assetPlugin.canHandleAsset(assetFile, assetLocation)) {
 							String assetFilePath = assetFile.getAbsolutePath();
 							
 							if(!cachedAssets.containsKey(assetFilePath)) {
 								try {
-									Asset createdAsset = assetPlugin.createAsset(memoizedFile, assetLocation);
+									Asset createdAsset = assetPlugin.createAsset(assetFile, assetLocation);
 									assetLocation.root().logger(this.getClass()).debug("creating new asset for the path '%s'", 
 											createdAsset.getAssetPath());
 									String assetPrimaryRequirePath = createdAsset.getPrimaryRequirePath();
