3e51f2eae778ea25f40f17dffbecc592b6f67a13
Andy Berry <andyberry88@gmail.com>
2014-11-18 11:01:10 +0000

    increasing the watch service sensitivity if the neccessary class is available

--- brjs-core/src/main/java/org/bladerunnerjs/memoization/WatchService.java
+++ brjs-core/src/main/java/org/bladerunnerjs/memoization/WatchService.java
@@ -5,8 +5,11 @@ import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchEvent.Modifier;
 import java.nio.file.WatchKey;
 
 
@@ -20,11 +23,23 @@ public class WatchService
 	}
 	
 	public WatchKey createWatchKeyForDir(Path dirPath) throws IOException {
-		return dirPath.register(watchService,ENTRY_CREATE,ENTRY_DELETE,ENTRY_MODIFY);
+		Modifier high = get_com_sun_nio_file_SensitivityWatchEventModifier_HIGH();
+		return (high == null) ? dirPath.register(watchService,ENTRY_CREATE,ENTRY_DELETE,ENTRY_MODIFY) : dirPath.register(watchService, new WatchEvent.Kind<?>[]{ENTRY_CREATE,ENTRY_DELETE,ENTRY_MODIFY}, high);
 	}
 	
 	public void close() throws IOException {
 		watchService.close();
 	}
 	
+	
+	// from https://github.com/HotswapProjects/HotswapAgent/issues/41
+	private Modifier get_com_sun_nio_file_SensitivityWatchEventModifier_HIGH() {
+		try {
+			Class<?> c = Class.forName("com.sun.nio.file.SensitivityWatchEventModifier");
+			Field f = c.getField("HIGH");
+			return (Modifier) f.get(c);
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
