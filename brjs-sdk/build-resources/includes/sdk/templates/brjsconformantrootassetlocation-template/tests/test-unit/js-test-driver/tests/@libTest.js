4279ea0f0434e53791eadd2715cab543b8dca477
Ioana Balas <ioana.balas@caplin.com>
2014-11-24 15:39:14 +0000

    templates for classes having a 'populate' method are in conf/templates/default

--- brjs-sdk/build-resources/includes/sdk/templates/brjsconformantrootassetlocation-template/tests/test-unit/js-test-driver/tests/@libTest.js
+++
@@ -1,8 +0,0 @@
-ExampleClassTest = TestCase("ExampleClassTest");
-
-var @lib = require("@libns/@lib");
-
-ExampleClassTest.prototype.testHelloWorldUtil = function()
-{
-	assertEquals( "Hello World!", @lib.helloWorldUtil() );
-};
