41632bf8f6836b4c0ac3e5e14484d89068cf8f3b
Ioana Balas <ioana.balas@caplin.com>
2014-12-04 14:34:11 +0000

    Moved the j2eeify-app folder to sdk.

--- brjs-core/src/main/java/org/bladerunnerjs/testing/specutility/BRJSBuilder.java
+++ brjs-core/src/main/java/org/bladerunnerjs/testing/specutility/BRJSBuilder.java
@@ -331,11 +331,12 @@ public class BRJSBuilder extends NodeBuilder<BRJS> {
 	
 	public BuilderChainer usesProductionTemplates() throws IOException {
 		verifyBrjsIsSet();
-		File templateDir = new File("../brjs-sdk/build-resources/includes/sdk/templates");
-		FileUtils.copyDirectory(brjs, templateDir, brjs.file("sdk/templates"));
 		
-		File templateDirConf = new File("../brjs-sdk/build-resources/includes/conf/templates/default"); 
-		FileUtils.copyDirectory(brjs, templateDirConf, brjs.file("conf/templates/default"));
+		File templateDir = new File("../brjs-sdk/build-resources/includes/conf/templates/default"); 
+		FileUtils.copyDirectory(brjs, templateDir, brjs.file("conf/templates/default"));
+		
+		File j2eeify = new File("../brjs-sdk/build-resources/includes/sdk/j2eeify-app"); 
+		FileUtils.copyDirectory(brjs, j2eeify, brjs.file("sdk/j2eeify-app"));
 		
 		File jsdocResourcesDir = new File("../brjs-sdk/build-resources/includes/sdk/jsdoc-toolkit-resources");
 		File jsdocResourcesDest = brjs.sdkRoot().file("jsdoc-toolkit-resources");
