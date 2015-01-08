e44a61030abf82379ea04688407f741d8632872e
Ioana Balas <ioana.balas@caplin.com>
2015-01-08 11:08:02 +0000

    Removed unnecessary imported packages.

--- brjs-core/src/main/java/org/bladerunnerjs/model/NodeImporter.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/NodeImporter.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
