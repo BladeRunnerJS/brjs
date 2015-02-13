aa8ad4bd84619b4fbf92063700ca0f33a77376c8
Andy Berry <andyberry88@gmail.com>
2015-02-13 10:43:02 +0000

    adding logging to MemoizedValue to identify whether values have changed

--- brjs-core/src/main/java/org/bladerunnerjs/memoization/MemoizedValue.java
+++ brjs-core/src/main/java/org/bladerunnerjs/memoization/MemoizedValue.java
@@ -4,17 +4,23 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bladerunnerjs.logging.Logger;
 import org.bladerunnerjs.model.FileAccessLimitScope;
 import org.bladerunnerjs.model.engine.Node;
 import org.bladerunnerjs.model.engine.RootNode;
 
 public class MemoizedValue<T extends Object> {
+	
+	public static final String RECALCULATING_VALUE_MSG = "Recalculating memoized value for '%s'";
+	public static final String USING_MEMOIZED_VALUE_MSG = "Using memoized value for '%s' as no files have changed";
+	
 	private final List<FileModifiedChecker> watchList = new ArrayList<>();
 	private final File[] watchItems;
 	private boolean exceptionThrownOnLastCompute;
 	private T value;
 	private final RootNode rootNode;
 	private final String valueIdentifier;
+	private Logger logger;
 	
 	
 	public MemoizedValue(String valueIdentifier, Node node) {
@@ -37,11 +43,14 @@ public class MemoizedValue<T extends Object> {
 			watchItemsList.add( file );
 		}
 		this.watchItems = watchItemsList.toArray(new File[0]);
+		logger = rootNode.logger(this.getClass());
 	}
 	
 	@SuppressWarnings("unchecked")
 	public <E extends Exception> T value(Getter<E> getter) throws E {
 		if (valueNeedsToBeRecomputed()) {
+			
+			logger.debug(RECALCULATING_VALUE_MSG, valueIdentifier);
 			
 			try (FileAccessLimitScope scope = rootNode.io().limitAccessToWithin(valueIdentifier, watchItems)) {
 				exceptionThrownOnLastCompute = false;
@@ -51,6 +60,8 @@ public class MemoizedValue<T extends Object> {
 				exceptionThrownOnLastCompute = true;
 				throw e;
 			}
+		} else {
+			logger.debug(USING_MEMOIZED_VALUE_MSG, valueIdentifier);
 		}
 		
 		return value;
