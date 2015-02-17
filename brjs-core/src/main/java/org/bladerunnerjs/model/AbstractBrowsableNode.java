20a989069046b75c67744409262757402590c158
Andy Berry <andyberry88@gmail.com>
2015-02-17 10:27:25 +0000

    making the index seed locator a plugin

--- brjs-core/src/main/java/org/bladerunnerjs/model/AbstractBrowsableNode.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/AbstractBrowsableNode.java
@@ -2,29 +2,19 @@ package org.bladerunnerjs.model;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
 
-import org.bladerunnerjs.api.LinkedAsset;
 import org.bladerunnerjs.api.memoization.MemoizedFile;
 import org.bladerunnerjs.api.model.exception.ModelOperationException;
 import org.bladerunnerjs.api.plugin.Locale;
 import org.bladerunnerjs.model.engine.Node;
 import org.bladerunnerjs.model.engine.RootNode;
-import org.bladerunnerjs.plugin.utility.IndexPageSeedLocator;
 import org.bladerunnerjs.utility.NoTagHandlerFoundException;
 import org.bladerunnerjs.utility.TagPluginUtility;
 
 public abstract class AbstractBrowsableNode extends AbstractBundlableNode implements BrowsableNode {
 	
-	private IndexPageSeedLocator indexPageSeedLocator;
-	
 	public AbstractBrowsableNode(RootNode rootNode, Node parent, MemoizedFile dir) {
 		super(rootNode, parent, dir);
-		indexPageSeedLocator = new IndexPageSeedLocator(root());
 	}
 	
 	@Override
@@ -35,13 +25,5 @@ public abstract class AbstractBrowsableNode extends AbstractBundlableNode implem
 		catch (IOException | NoTagHandlerFoundException e) {
 			throw new ModelOperationException(e);
 		}
-	}
-	
-	@Override
-	public List<LinkedAsset> seedAssets() {
-		Set<LinkedAsset> seedAssets = new HashSet<>();
-		seedAssets.addAll( indexPageSeedLocator.seedAssets(this) );
-		seedAssets.addAll( super.seedAssets() );
-		return new ArrayList<>( seedAssets );
 	}
 }
