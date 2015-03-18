057b340acb9df7f7406b55ea875fd3959809f2bb
Peter Colley <peter.colley@caplin.com/>
2015-03-18 09:26:26 +0000

    include aliases in application

---
+++ brjs-sdk/brjs-apps/it-app/itbladeset-bladeset/blades/itblade/src/TestingClass.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var TestingClass = function() {
+};
+
+TestingClass.prototype.foo = function() {
+	return "bar";
+};
+
+module.exports = TestingClass;
--- brjs-sdk/brjs-apps/it-app/src/App.js
+++ brjs-sdk/brjs-apps/it-app/src/App.js
@@ -7,9 +7,20 @@ var App = function() {
     var element = document.getElementById("hello-world");
     element.innerHTML="Successfully loaded the application";
 
+	this.addItBladeToView();
+	console.log(this.playWithAliases());
+};
+
+App.prototype.addItBladeToView = function() {
 	var itBladeViewModel = new ItbladeViewModel();
 	var koComponent = new KnockoutComponent('itapp.itbladeset.itblade.view-template', itBladeViewModel);
 	document.getElementById("Itblade").appendChild(koComponent.getElement());
+};
+
+App.prototype.playWithAliases = function() {
+	var AliasedClass = require('alias!itapp.itbladeset.itblade.NewName');
+	var obj = new AliasedClass();
+	return obj.foo();
 };
 
 module.exports = App;
