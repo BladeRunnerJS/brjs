d003718e4daf68f21334390a366a9980917d6bf9
Peter Colley <peter.colley@caplin.com/>
2015-03-18 09:35:56 +0000

    alias interface included

--- brjs-sdk/brjs-apps/it-app/itbladeset-bladeset/blades/itblade/src/TestingClass.js
+++ brjs-sdk/brjs-apps/it-app/itbladeset-bladeset/blades/itblade/src/TestingClass.js
@@ -1,10 +1,18 @@
 "use strict";
+var br = require( 'br/Core' );
+var TestingInterface = require('./TestingInterface');
 
 var TestingClass = function() {
 };
 
+br.implement(TestingClass, TestingInterface);
+
 TestingClass.prototype.foo = function() {
 	return "bar";
+};
+
+TestingClass.prototype.implementMe = function() {
+	return "I have been implemented";
 };
 
 module.exports = TestingClass;
