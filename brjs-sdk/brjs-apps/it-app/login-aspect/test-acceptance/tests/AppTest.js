f5b39c0ddaccf13f83ce3a0bbf18ce44e61dc4cb
Peter Colley <peter.colley@caplin.com/>
2015-03-19 11:34:36 +0000

    login aspect added, login with user, password

---
+++ brjs-sdk/brjs-apps/it-app/login-aspect/test-acceptance/tests/AppTest.js
@@ -0,0 +1,23 @@
+(function() {
+	'use strict';
+	
+	require( 'jasmine' );
+	
+	var App = require("itapp/App");
+	
+	describe('App Tests', function() {
+	
+		beforeEach(function() {
+
+		});
+	
+		afterEach(function() {
+
+		});
+	
+		it( 'Should say hello', function() {
+
+		});
+	
+	});
+}());
