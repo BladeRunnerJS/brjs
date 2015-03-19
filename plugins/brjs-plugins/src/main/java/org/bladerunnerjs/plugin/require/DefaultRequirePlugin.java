f2b70d8bb3f174ecdf47779c6298134f75a7e5e4
Ioana Balas <ioana.balas@caplin.com>
2015-03-19 15:27:17 +0000

    Added OutOfScopeRequirePathException, associated test and logic.

--- plugins/brjs-plugins/src/main/java/org/bladerunnerjs/plugin/require/DefaultRequirePlugin.java
+++ plugins/brjs-plugins/src/main/java/org/bladerunnerjs/plugin/require/DefaultRequirePlugin.java
@@ -4,6 +4,7 @@ import org.bladerunnerjs.api.Asset;
 import org.bladerunnerjs.api.BRJS;
 import org.bladerunnerjs.api.LinkedAsset;
 import org.bladerunnerjs.api.model.exception.AmbiguousRequirePathException;
+import org.bladerunnerjs.api.model.exception.OutOfScopeRequirePathException;
 import org.bladerunnerjs.api.model.exception.RequirePathException;
 import org.bladerunnerjs.api.model.exception.UnresolvableRequirePathException;
 import org.bladerunnerjs.api.plugin.RequirePlugin;
@@ -25,9 +26,10 @@ public class DefaultRequirePlugin extends AbstractRequirePlugin implements Requi
 	@Override
 	public Asset getAsset(BundlableNode bundlableNode, String requirePathSuffix) throws RequirePathException {
 		LinkedAsset asset = null;
+		String scopedLocations = "";
 		for(AssetContainer assetContainer : bundlableNode.scopeAssetContainers()) {
 			LinkedAsset locationAsset = assetContainer.linkedAsset(requirePathSuffix);
-
+			scopedLocations += assetContainer.dir().getPath() + "\n";
 			if(locationAsset != null) {
 				if(asset == null) {
 					asset = locationAsset;
@@ -39,10 +41,17 @@ public class DefaultRequirePlugin extends AbstractRequirePlugin implements Requi
 				}
 			}
 		}
-		
 		if(asset == null) {
+			for (AssetContainer assetContainer : bundlableNode.app().getAllAssetContainers()) {
+				LinkedAsset locationAsset = assetContainer.linkedAsset(requirePathSuffix);
+				if (locationAsset != null) {
+					throw new OutOfScopeRequirePathException(requirePathSuffix, bundlableNode.getClass().getSimpleName(), scopedLocations, locationAsset.dir().getPath());
+				}
+			}
 			throw new UnresolvableRequirePathException(requirePathSuffix);
 		}
 		return asset;
 	}
+	
+	
 }
