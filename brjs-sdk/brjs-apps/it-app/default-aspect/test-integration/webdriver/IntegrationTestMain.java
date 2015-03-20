464a5d22338a7639328b50ea293d057dd761ba87
Peter Colley <peter.colley@caplin.com/>
2015-03-20 14:46:20 +0000

    make the IT runnable

--- brjs-sdk/brjs-apps/it-app/default-aspect/test-integration/webdriver/IntegrationTestMain.java
+++ brjs-sdk/brjs-apps/it-app/default-aspect/test-integration/webdriver/IntegrationTestMain.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
@@ -12,7 +11,6 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.WebDriver;
 
-import com.caplin.cutlass.testIntegration.WebDriverProvider;
 
 @RunWith(Parameterized.class)
 
@@ -20,18 +18,14 @@ public class IntegrationTestMain {
 
     private static WebDriver driver;
     private static String baseUrl;
-    private static Utility utility;
-    private static FxTradingPortal fxTradingPortal = null;
-    private static TriggerManager triggerManager = null;
     private static String currentlyLoggedInUser = null;
 
     @Parameters
     public static Collection<Object[]> getUsers()
     {
-        return Arrays.asList(new Object[][] {
-                {"user1@caplin.com"},
-                //	{"user2@caplin.com"}
-        });
+        return null;
+
+
     }
 
 
