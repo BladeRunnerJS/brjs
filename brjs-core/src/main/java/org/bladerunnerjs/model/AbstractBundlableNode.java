237d4222dcd46a58c8ad08df04ff68cd58ec0ead
Andy Berry <andyberry88@gmail.com>
2015-04-13 16:25:13 +0100

    changing bundlable node memoized value so it also uses the node dir for the scope

--- brjs-core/src/main/java/org/bladerunnerjs/model/AbstractBundlableNode.java
+++ brjs-core/src/main/java/org/bladerunnerjs/model/AbstractBundlableNode.java
@@ -27,7 +27,7 @@ import org.bladerunnerjs.utility.BundleSetRequestHandler;
 
 public abstract class AbstractBundlableNode extends AbstractAssetContainer implements BundlableNode {
 
-	private final MemoizedValue<BundleSet> bundleSet = new MemoizedValue<>("BundlableNode.bundleSet", root(), root().dir());
+	private final MemoizedValue<BundleSet> bundleSet = new MemoizedValue<>("BundlableNode.bundleSet", root(), root().sdkJsLibsDir().dir(), dir());
 	private RequirePlugin defaultRequirePlugin;
 	
 	public AbstractBundlableNode(RootNode rootNode, Node parent, MemoizedFile dir) {
