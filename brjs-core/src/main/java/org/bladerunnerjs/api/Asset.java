391c3b3883626a8b452958c9fcfa71686c179166
Ioana Balas <ioana.balas@caplin.com>
2015-04-21 17:34:54 +0100

    Adding initial JavaDoc for Asset and BRJSNode.

--- brjs-core/src/main/java/org/bladerunnerjs/api/Asset.java
+++ brjs-core/src/main/java/org/bladerunnerjs/api/Asset.java
@@ -8,17 +8,83 @@ import org.bladerunnerjs.api.memoization.MemoizedFile;
 import org.bladerunnerjs.model.AssetContainer;
 
 /**
- * Any file that lives within an App
- *
+ * An Asset is any file that lives within an App. Assets are responsible for transforming their input into a readable stream
+ * (e.g. compiling CoffeeScript to JavaScript). They also provide string names (decoupled from their location on disk)
+ * by which they can be referenced. This interface is used by classes that implement methods such as reading the file, 
+ * calculating the require path of the Asset, or obtaining the MemoizedFile version of an Asset for monitoring it for changes.
  */
+
 public interface Asset {
+	
+	/**
+	 * The method returns a Reader object for obtaining the content of the Asset.
+	 * 
+	 * @return Reader a Reader object for reading the current Asset's character stream
+	 */
 	Reader getReader() throws IOException;
+	
+	/**
+	 * The method returns a MemoizedFile object that provides similar methods to File and wraps a File object.
+	 * Several of the methods' return values are 'memoized' and only regenerated if properties on the underlying file on disk change. 
+	 * Changes are detected by using MemoizedValue and the FileModificationRegistry.
+	 * 
+	 * @return MemoizedFile a MemoizedFile object for obtaining the Asset within the BRJS structure and monitoring it for changes.
+	 */
 	MemoizedFile file();
+	
+	/**
+	 * The method returns the name of the Asset file.
+	 * 
+	 * @return String a String object representing the name of the Asset file.
+	 */
 	String getAssetName();
+	
+	/**
+	 * The method returns the path to the Asset relative to the App that comprises it.
+	 * 
+	 * @return String a String object representing the path to the Asset file relative to the App that comprises it.
+	 */
 	String getAssetPath();
+	
+	/**
+	 * The method returns the available require paths to the Asset. An Asset may have one or more valid require paths, as desired.
+	 * This list will include the primary require path for the Asset.
+	 * 
+	 * @return List<String> a List<String> object representing the available require paths to the Asset.
+	 */
 	List<String> getRequirePaths();
+	
+	/**
+	 * The method returns the primary require path to the Asset, which is calculated based on the require prefix of its AssetContainer 
+	 * and the Asset name.
+	 * 
+	 * @return String a String object representing the primary require path to the Asset.
+	 */
 	String getPrimaryRequirePath();
+	
+	/**
+	 * The method returns the AssetContainer in which the Asset is contained. An AssetContainer may be e.g. an Aspect, a Workbench,
+	 * a TestPack, a Blade, a Bladeset or a library.
+	 * 
+	 * @return AssetContainer an AssetContainer object that contains the Asset.
+	 */
 	AssetContainer assetContainer();
+	
+	/**
+	 * In order to encourage modularity, BRJS does not allow lower level entities to depend on higher level entities. For example,
+	 * a blade may not require an asset from the default aspect. This behaviour is called 'scope enforced' and may be set on a per 
+	 * AssetContainer basis. The method returns true if the scope is enforced, meaning that warnings about such out of scope require paths
+	 * will be logged, or false, if these will be ignored and retrieved.  
+	 * 
+	 * @return boolean a boolean object stating whether eventual invalid require path warnings will be logged.
+	 */
 	boolean isScopeEnforced();
+	
+	/**
+	 * BRJS distinguishes between Assets that may be required and those they may not. Those that may not include Assets on the
+	 * root directory level or directories. The method returns true if the current Asset may be required and false otherwise.
+	 * 
+	 * @return boolean a boolean object stating whether the Asset may be required by another one.
+	 */
 	boolean isRequirable();
 }
--- brjs-core/src/main/java/org/bladerunnerjs/api/BRJSNode.java
+++ brjs-core/src/main/java/org/bladerunnerjs/api/BRJSNode.java
@@ -8,10 +8,32 @@ import org.bladerunnerjs.api.model.exception.modelupdate.ModelUpdateException;
 import org.bladerunnerjs.api.model.exception.template.TemplateInstallationException;
 import org.bladerunnerjs.model.engine.Node;
 
+/**
+ * A BRJSNode is a generic term for a folder under BRJS. Among others, BRJSNodes encompass AssetContainers, Apps, TemplateGroups or Themes.
+ */
 
 public interface BRJSNode extends Node {
+	
+	/**
+	 * The method returns a {@link BRJS} object that represents the root of the model. 
+	 * 
+	 * @return BRJS a BRJS object that represents the root of the model.
+	 */
 	BRJS root();
+	
+	/**
+	 * Any BRJSNode may be populated according to a pre-existing template. The method will create the files and folders existing in the 
+	 * specified template, including their content, within the BRJSNode. This method is to help with consistency among your entities
+	 * and may be used, for example, to adhere to certain programming or modular styles. 
+	 * 
+	 * @param templateGroup a String that  represents the name by which the templates therein will be referenced when creating new nodes. 
+	 * The templateGroup may contain templates for various BRJS entities, and the one corresponding to your current BRJSNode type 
+	 * will be retrieved automatically.
+	 * 
+	 * @exception InvalidNameException if the node may not be created due to an invalid name
+	 */
 	void populate(String templateGroup) throws InvalidNameException, ModelUpdateException, TemplateInstallationException;
+	
 	String getTemplateName();
 	void addTemplateTransformations(Map<String, String> transformations) throws ModelUpdateException;
 }
