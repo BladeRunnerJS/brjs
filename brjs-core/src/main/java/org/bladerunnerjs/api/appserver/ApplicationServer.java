4e4ab55313667b6d5b721c33c4c6087c2f3d426c
Andy Berry <andyberry88@gmail.com>
2015-04-22 14:32:47 +0100

    adding javadoc for ApplicationServer

--- brjs-core/src/main/java/org/bladerunnerjs/api/appserver/ApplicationServer.java
+++ brjs-core/src/main/java/org/bladerunnerjs/api/appserver/ApplicationServer.java
@@ -1,11 +1,36 @@
 package org.bladerunnerjs.api.appserver;
 
+import org.bladerunnerjs.api.App;
 
+
+/**
+ * The development application server. 
+ */
 public interface ApplicationServer
 {
 	int getPort();
+	
+	/**
+	 * Start the application server. This method should start the server in a new thread so the call does not block.
+	 * @throws Exception
+	 */
 	void start() throws Exception;
+	
+	/**
+	 * Stop the application server thread.
+	 * @throws Exception
+	 */
 	void stop() throws Exception;
+	
+	/**
+	 * Wait for the application server thread to terminate. This method behaves in a similar way to {@link Thread#join}
+	 * @throws Exception
+	 */
 	void join() throws Exception;
+	
+	/**
+	 * Set how often the {@link App} deployment watcher should check for new apps that need deploying to the application server.
+	 * @param The check interval
+	 */
 	void setAppDeploymentWatcherInterval(long interval);
 }
