c8eb82fc1084f9b5f34dbf0b80fa51e0bcb34619
dchambers <dominic.chambers@gmail.com>
2015-04-24 11:03:32 +0100

    Changed Clock to a class so that we can more easilly install and uninstall it.

--- brjs-sdk/sdk/libs/javascript/jsunitextensions/Clock.js
+++ brjs-sdk/sdk/libs/javascript/jsunitextensions/Clock.js
@@ -1,61 +1,68 @@
-var Clock = {
-			timeoutsMade: 0,
-			scheduledFunctions: {},
-			nowMillis: 0,
-			reset: function() {
-					this.scheduledFunctions = {};
-					this.nowMillis = 0;
-					this.timeoutsMade = 0;
-			},
-			tick: function(millis) {
-					var oldMillis = this.nowMillis;
-					var newMillis = oldMillis + millis;
-					this.runFunctionsWithinRange(oldMillis, newMillis);
-					this.nowMillis = newMillis;
-			},
-			runFunctionsWithinRange: function(oldMillis, nowMillis) {
-					var scheduledFunc;
-					var funcsToRun = [];
-					for (var timeoutKey in this.scheduledFunctions) {
-							scheduledFunc = this.scheduledFunctions[timeoutKey];
-							if (scheduledFunc != undefined &&
-									scheduledFunc.runAtMillis >= oldMillis &&
-									scheduledFunc.runAtMillis <= nowMillis) {
-									funcsToRun.push(scheduledFunc);
-									this.scheduledFunctions[timeoutKey] = undefined;
-							}
-					}
+function ClockClass() {
+	this.timeoutsMade = 0;
+	this.scheduledFunctions = {};
+	this.nowMillis = 0;
+}
 
-					if (funcsToRun.length > 0) {
-							funcsToRun.sort(function(a, b) {
-									return a.runAtMillis - b.runAtMillis;
-							});
-							for (var i = 0; i < funcsToRun.length; ++i) {
-									try {
-											this.nowMillis = funcsToRun[i].runAtMillis;
-											funcsToRun[i].funcToCall();
-											if (funcsToRun[i].recurring) {
-													Clock.scheduleFunction(funcsToRun[i].timeoutKey,
-																	funcsToRun[i].funcToCall,
-																	funcsToRun[i].millis,
-																	true);
-											}
-									} catch(e) {
-									}
-							}
-							this.runFunctionsWithinRange(oldMillis, nowMillis);
-					}
-			},
-			scheduleFunction: function(timeoutKey, funcToCall, millis, recurring) {
-					Clock.scheduledFunctions[timeoutKey] = {
-							runAtMillis: Clock.nowMillis + millis,
-							funcToCall: funcToCall,
-							recurring: recurring,
-							timeoutKey: timeoutKey,
-							millis: millis
-					};
-			}
+ClockClass.prototype.reset = function() {
+		this.scheduledFunctions = {};
+		this.nowMillis = 0;
+		this.timeoutsMade = 0;
 };
+
+
+ClockClass.prototype.tick = function(millis) {
+		var oldMillis = this.nowMillis;
+		var newMillis = oldMillis + millis;
+		this.runFunctionsWithinRange(oldMillis, newMillis);
+		this.nowMillis = newMillis;
+};
+
+ClockClass.prototype.runFunctionsWithinRange = function(oldMillis, nowMillis) {
+		var scheduledFunc;
+		var funcsToRun = [];
+		for (var timeoutKey in this.scheduledFunctions) {
+				scheduledFunc = this.scheduledFunctions[timeoutKey];
+				if (scheduledFunc != undefined &&
+						scheduledFunc.runAtMillis >= oldMillis &&
+						scheduledFunc.runAtMillis <= nowMillis) {
+						funcsToRun.push(scheduledFunc);
+						this.scheduledFunctions[timeoutKey] = undefined;
+				}
+		}
+
+		if (funcsToRun.length > 0) {
+				funcsToRun.sort(function(a, b) {
+						return a.runAtMillis - b.runAtMillis;
+				});
+				for (var i = 0; i < funcsToRun.length; ++i) {
+						try {
+								this.nowMillis = funcsToRun[i].runAtMillis;
+								funcsToRun[i].funcToCall();
+								if (funcsToRun[i].recurring) {
+										Clock.scheduleFunction(funcsToRun[i].timeoutKey,
+														funcsToRun[i].funcToCall,
+														funcsToRun[i].millis,
+														true);
+								}
+						} catch(e) {
+						}
+				}
+				this.runFunctionsWithinRange(oldMillis, nowMillis);
+		}
+};
+
+ClockClass.prototype.scheduleFunction = function(timeoutKey, funcToCall, millis, recurring) {
+		Clock.scheduledFunctions[timeoutKey] = {
+				runAtMillis: Clock.nowMillis + millis,
+				funcToCall: funcToCall,
+				recurring: recurring,
+				timeoutKey: timeoutKey,
+				millis: millis
+		};
+};
+
+var Clock = new ClockClass();
 
 window.setTimeout = function(funcToCall, millis) {
 		Clock.timeoutsMade = Clock.timeoutsMade + 1;
