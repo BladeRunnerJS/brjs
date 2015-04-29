7d8d63efdf3e84638a243135cd8f9a4a037e9294
Thanh Cam <thanh.cam@caplin.com>
2015-04-29 16:07:16 +0100

    adding some more unit test around setInterval and reset

--- brjs-sdk/sdk/libs/javascript/jsunitextensions/test-unit/tests/ClockTest.js
+++ brjs-sdk/sdk/libs/javascript/jsunitextensions/test-unit/tests/ClockTest.js
@@ -6,10 +6,15 @@ ClockTest.setUp = function() {
 	Clock.install();
 	
 	this.timeoutInvoked = false;
-	
+	this.intervalInvoked = false;
+
 	setTimeout(function() {
 		this.timeoutInvoked = true;
 	}.bind(this), 1000);
+
+	setInterval(function() {
+		this.intervalInvoked = true;
+	}.bind(this), 2000);
 };
 
 ClockTest.tearDown = function() {
@@ -20,12 +25,51 @@ ClockTest.testThatTimersDontFireByDefault = function() {
 	assertFalse(this.timeoutInvoked);
 };
 
+ClockTest.testThatIntervalsDontFireByDefault = function() {
+	assertFalse(this.intervalInvoked);
+};
+
 ClockTest.testThatTimersFireIfEnoughTimeElapses = function() {
 	Clock.tick(1000);
 	assertTrue(this.timeoutInvoked);
 };
 
-ClockTest.testThatTimersFireIfNotEnoughTimeHasElapsed = function() {
+ClockTest.testThatTimersOnlyFireOnce = function() {
+	Clock.tick(1000);
+	assertTrue(this.timeoutInvoked);
+	this.timeoutInvoked = false;
+	Clock.tick(1000);
+	assertFalse(this.timeoutInvoked);
+};
+
+ClockTest.testThatTimersDontFireIfNotEnoughTimeHasElapsed = function() {
 	Clock.tick(999);
 	assertFalse(this.timeoutInvoked);
+};
+
+ClockTest.testThatTimersAccumulateCorrectly = function() {
+	Clock.tick(999);
+	assertFalse(this.timeoutInvoked);
+	Clock.tick(1);
+	assertTrue(this.timeoutInvoked);
+};
+
+ClockTest.testThatIntervalsAreFiredAtEachInterval = function () {
+	Clock.tick(2000);
+	assertTrue(this.intervalInvoked);
+	this.intervalInvoked = false;
+	Clock.tick(2000);
+	assertTrue(this.intervalInvoked);
+};
+
+ClockTest.testThatIntervalsDontFireIfNotEnoughTimeHasElapsed = function () {
+	Clock.tick(1999);
+	assertFalse(this.intervalInvoked);
+};
+
+ClockTest.testThatTimersAreResetCorrectly = function() {
+	Clock.reset();
+	Clock.tick(2000);
+	assertFalse(this.timeoutInvoked);
+	assertFalse(this.intervalInvoked);
 };
