531814ea6c354cf3fd3645bbcf7529657f4927f4
Jan Hancic <jan.hancic@gmail.com>
2015-05-02 15:01:02 +0100

    Fix code style in templates so that they conform to the Caplin JS style guide.

--- brjs-sdk/sdk/templates/default/blade/src/@bladeTitleViewModel.js
+++ brjs-sdk/sdk/templates/default/blade/src/@bladeTitleViewModel.js
@@ -1,23 +1,23 @@
 'use strict';
 
-var ko = require( 'ko' );
-var i18n = require( 'br/I18n' );
+var ko = require('ko');
+var i18n = require('br/I18n');
 
 function @bladeTitleViewModel() {
-	this.eventHub = require('service!br.event-hub');
-	this.welcomeMessage = ko.observable( 'Welcome to your new Blade.' );
-	this.buttonClickMessage = ko.observable( i18n( '@bladeNamespace.button.click.message' ) );
-	this.logWelcome();
+	this._eventHub = require('service!br.event-hub');
+	this.welcomeMessage = ko.observable('Welcome to your new Blade.');
+	this.buttonClickMessage = ko.observable(i18n('@bladeNamespace.button.click.message'));
+	this._logWelcome();
 }
 
 @bladeTitleViewModel.prototype.buttonClicked = function() {
-	console.log( 'button clicked' );
-	var channel = this.eventHub.channel('@blade-channel');
-	channel.trigger( 'hello-event', { some: 'Hello World!' } );
+	console.log('button clicked');
+	var channel = this._eventHub.channel('@blade-channel');
+	channel.trigger('hello-event', {some: 'Hello World!'});
 };
 
-@bladeTitleViewModel.prototype.logWelcome = function() {
-	console.log(  this.welcomeMessage() );
-}
+@bladeTitleViewModel.prototype._logWelcome = function() {
+	console.log(this.welcomeMessage());
+};
 
 module.exports = @bladeTitleViewModel;
