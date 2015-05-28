4ed1d309f92ad96b3ec7cb727fe546eaf5395901
Ioana Balas <ioana.balas@caplin.com>
2015-05-28 11:17:16 +0100

    Fixing and refactoring tests.

--- brjs-core-tests/src/test/java/org/bladerunnerjs/spec/observer/JsDocModelObserverTest.java
+++ brjs-core-tests/src/test/java/org/bladerunnerjs/spec/observer/JsDocModelObserverTest.java
@@ -1,8 +1,5 @@
 package org.bladerunnerjs.spec.observer;
 
-import java.io.File;
-
-import org.apache.commons.io.FileUtils;
 import org.bladerunnerjs.api.spec.engine.SpecTest;
 import org.bladerunnerjs.plugin.jsdoc.JsDocObserver;
 import org.junit.Test;
@@ -14,12 +11,11 @@ public class JsDocModelObserverTest extends SpecTest {
 		given(brjs).hasModelObserverPlugins(new JsDocObserver())
 			.and(testSdkDirectory).containsFileWithContents("sdk/jsdoc-toolkit-resources/jsdoc-placeholders/index.html", "PLACEHOLDER");
 		when(brjs).hasBeenCreated()
-			.and(brjs).pluginsAreAccessed();
-		
-			FileUtils.writeStringToFile(new File(testSdkDirectory, "brjs-apps/app1/src/MyClass.js"), "// my class");
-			FileUtils.writeStringToFile(new File(testSdkDirectory, "brjs-apps/app1/index.html"), "");
+			.and(brjs).pluginsAreAccessed()
+			.and(testSdkDirectory).containsFileWithContents("brjs-apps/app1/src/MyClass.js", "// my class")
+			.and(testSdkDirectory).containsFile("brjs-apps/app1/index.html");
 			brjs.getFileModificationRegistry().incrementAllFileVersions();
-			/* and */ brjs.apps();
+			when(brjs).discoverApps();
 		then(brjs).hasDir("generated/app/app1/jsdoc")
 			.and(brjs.app("app1").storageDir("jsdoc")).containsFileWithContents("index.html", "PLACEHOLDER");
 	}
