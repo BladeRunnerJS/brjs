8388d266cdf1bd22ace0ed07f01b8d8bb9dbcbb3
Andy Berry <andyberry88@gmail.com>
2015-06-15 17:13:41 +0100

    removing the repated attempt to create an empty bundle file

--- brjs-legacy/src/main/java/org/bladerunnerjs/legacy/command/test/testrunner/BundlerHandler.java
+++ brjs-legacy/src/main/java/org/bladerunnerjs/legacy/command/test/testrunner/BundlerHandler.java
@@ -10,7 +10,6 @@ import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.bladerunnerjs.api.App;
 import org.bladerunnerjs.api.BRJS;
-import org.bladerunnerjs.api.logging.Logger;
 import org.bladerunnerjs.api.model.exception.ModelOperationException;
 import org.bladerunnerjs.api.model.exception.request.ContentProcessingException;
 import org.bladerunnerjs.api.model.exception.request.MalformedRequestException;
@@ -58,7 +57,7 @@ public class BundlerHandler
 		
 		String modelRequestPath = getModelRequestPath(bundlePath);
 		
-		repeatedlyAttemptToCreateBundleFile(brjs, bundleFile);
+		createBundleFile(brjs, bundleFile);
 		
 		try (OutputStream bundleFileOutputStream = new FileOutputStream(bundleFile, false);
 			ResponseContent content = BundleSetRequestHandler.handle(new JsTestDriverBundleSet(bundlableNode.getBundleSet()), modelRequestPath, new StaticContentAccessor(app), version); )
@@ -71,29 +70,17 @@ public class BundlerHandler
 	
 	// this is a workaround for the scenario where Windows indexing service or virus scanners etc can lock the file when we try to create it
 	// see http://stackoverflow.com/a/10516563/2634854 for more info
-	private void repeatedlyAttemptToCreateBundleFile(BRJS brjs, File bundleFile) throws IOException
+	private void createBundleFile(BRJS brjs, File bundleFile) throws IOException
 	{
 		if (bundleFile.exists()) {
 			throw new IOException( String.format("The bundle file '%s' already exists and should not. It should have previously been deleted so new content can be written to it", bundleFile.getAbsolutePath()) );
 		}
 		bundleFile.getParentFile().mkdirs();
+		bundleFile.createNewFile();
 		
-		Logger logger = brjs.logger(this.getClass());
-		for (int i = 0; i < 100; i++) {
-			try {
-				if (i % 10 == 0) { // print log line every 10 iterations
-					logger.debug("Attempting to create an empty bundle file at '%s'.", bundleFile.getAbsolutePath());
-				}
-    			bundleFile.createNewFile();
-    			if (bundleFile.isFile()) {
-    				return;
-    			}
-    			Thread.sleep(100);
-			} catch (IOException | InterruptedException ex) {
-				// ignore the exception from creating the file or thread interupted
-			}
+		if (!bundleFile.isFile()) {
+			throw new IOException("Unable to create an empty bundle file at " + bundleFile.getAbsolutePath());
 		}
-		throw new IOException("Unable to create an empty bundle file at " + bundleFile.getAbsolutePath());
 	}
 
 
