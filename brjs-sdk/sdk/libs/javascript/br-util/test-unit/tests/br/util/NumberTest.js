7c240ac4c9df24353d45804edab3b1fd7e069146
Andy Berry <andyberry88@gmail.com>
2015-06-18 16:48:20 +0100

    wrapping tests in an IIFE

--- brjs-sdk/sdk/libs/javascript/br-util/test-unit/tests/br/util/NumberTest.js
+++ brjs-sdk/sdk/libs/javascript/br-util/test-unit/tests/br/util/NumberTest.js
@@ -1,13 +1,15 @@
-var Number = require('br/util/Number');
+(function() {
+	var Number = require('br/util/Number');
 
-NumberTest = TestCase('NumberTest');
+	NumberTest = TestCase('NumberTest');
 
-NumberTest.prototype.test_decimalPlaces = function() {
-	assertEquals(2, Number.decimalPlaces('.05'));
-	assertEquals(1, Number.decimalPlaces('.5'));
-	assertEquals(0, Number.decimalPlaces('1'));
-	assertEquals(100, Number.decimalPlaces('25e-100'));
-	assertEquals(100, Number.decimalPlaces('2.5e-99'));
-	assertEquals(0, Number.decimalPlaces('.5e1'));
-	assertEquals(1, Number.decimalPlaces('.25e1'));
-};
+	NumberTest.prototype.test_decimalPlaces = function() {
+		assertEquals(2, Number.decimalPlaces('.05'));
+		assertEquals(1, Number.decimalPlaces('.5'));
+		assertEquals(0, Number.decimalPlaces('1'));
+		assertEquals(100, Number.decimalPlaces('25e-100'));
+		assertEquals(100, Number.decimalPlaces('2.5e-99'));
+		assertEquals(0, Number.decimalPlaces('.5e1'));
+		assertEquals(1, Number.decimalPlaces('.25e1'));
+	};
+})();
