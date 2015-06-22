0b57e3df50c11089d08aeeb369a8cd0249976b58
Andy Berry <andyberry88@gmail.com>
2015-06-22 21:42:22 +0100

    converting dashboard tests to common-js

--- brjs-sdk/sdk/system-applications/dashboard/dashboard-bladeset/blades/app/tests/test-unit/js-test-driver/tests/brjs/dashboard/app/testing/RequestUrlFixtureTest.js
+++ brjs-sdk/sdk/system-applications/dashboard/dashboard-bladeset/blades/app/tests/test-unit/js-test-driver/tests/brjs/dashboard/app/testing/RequestUrlFixtureTest.js
@@ -1,19 +1,20 @@
-RequestUrlFixtureTest = TestCase("RequestUrlFixtureTest");
+(function() {
+    RequestUrlFixtureTest = TestCase("RequestUrlFixtureTest");
 
-RequestUrlFixtureTest.prototype.setUp = function()
-{
-	this.m_oXhrFactoryStub = new brjs.dashboard.app.testing.XhrFactoryStub();
-	this.m_oRequestUrlFixture = new brjs.dashboard.app.testing.RequestUrlFixture();
-	this.m_oRequestUrlFixture.setXhrFactory(this.m_oXhrFactoryStub);
-};
+    RequestUrlFixtureTest.prototype.setUp = function()
+    {
+        this.m_oXhrFactoryStub = new brjs.dashboard.app.testing.XhrFactoryStub();
+        this.m_oRequestUrlFixture = new brjs.dashboard.app.testing.RequestUrlFixture();
+        this.m_oRequestUrlFixture.setXhrFactory(this.m_oXhrFactoryStub);
+    };
 
-RequestUrlFixtureTest.prototype.testWeCanMakeAssertionsAboutRequestsBeforeTheResponseHasComeBack = function()
-{
-	var oXhrStub = this.m_oXhrFactoryStub.getRequestObject();
-	
-	oXhrStub.open("GET", "/the-url");
-	oXhrStub.send(null);
-	this.m_oRequestUrlFixture.doThen("requestSent", "GET /the-url"); 
-};
-
-// TODO: add the other tests
+    // TODO: add the other tests
+    RequestUrlFixtureTest.prototype.testWeCanMakeAssertionsAboutRequestsBeforeTheResponseHasComeBack = function()
+    {
+        var oXhrStub = this.m_oXhrFactoryStub.getRequestObject();
+        
+        oXhrStub.open("GET", "/the-url");
+        oXhrStub.send(null);
+        this.m_oRequestUrlFixture.doThen("requestSent", "GET /the-url"); 
+    };
+})();
