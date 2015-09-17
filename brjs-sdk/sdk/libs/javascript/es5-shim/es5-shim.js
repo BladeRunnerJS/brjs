e76138e58d7eeac0f2b408ca81aed14487fda8bb
danielo <daniel.owen@caplin.com>
2015-09-17 16:14:21 +0100

    improving comment on patched method

--- brjs-sdk/sdk/libs/javascript/es5-shim/es5-shim.js
+++ brjs-sdk/sdk/libs/javascript/es5-shim/es5-shim.js
@@ -497,9 +497,8 @@ if (!Array.prototype.lastIndexOf) {
 // ES5 15.2.3.2
 // http://es5.github.com/#x15.2.3.2
 if (!Object.getPrototypeOf) {
-    // https://github.com/kriskowal/es5-shim/issues#issue/2
-    // http://ejohn.org/blog/objectgetprototypeof/
-    // recommended by fschaefer on github
+    // This is a patched version for https://github.com/BladeRunnerJS/brjs/issues/1530
+    //If we upgrade this fix may need to be ported across
     Object.getPrototypeOf = function getPrototypeOf(object) {
         // this is what most shams do, but sometimes it's wrong.
         if (object.constructor && object.constructor.prototype && object.constructor.prototype !== object) {
