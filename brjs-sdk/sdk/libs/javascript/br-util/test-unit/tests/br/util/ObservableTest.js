f11cecc5bc4d0af0c3c7b7e5e66426557bf1d1c1
Andy Berry <andyberry88@gmail.com>
2015-10-20 15:38:49 +0100

    removing a br-util test that tested a private method that no one used
    the test isnt part of the API, no other code paths use it and it breaks
    when using closure-medium

--- brjs-sdk/sdk/libs/javascript/br-util/test-unit/tests/br/util/ObservableTest.js
+++ brjs-sdk/sdk/libs/javascript/br-util/test-unit/tests/br/util/ObservableTest.js
@@ -381,18 +381,6 @@
 		this.m_oObservable.notifyObserversWithTryCatch("method2", [ "good" ]);
 	};
 
-
-	ObservableTest.prototype.test_getAllObservers = function()
-	{
-		this.m_oObservable.addObserver(this.m_oObserver1.proxy());
-		this.m_oObservable.addObserver(this.m_oObserver2.proxy());
-		
-		var pAllObservers = this.m_oObservable._$getAllObservers();
-		assertEquals(2, pAllObservers.length);
-		assertTrue(this.m_oObserver1.proxy() == pAllObservers[0]);
-		assertTrue(this.m_oObserver2.proxy() == pAllObservers[1]);
-	};
-
 	ObservableTest.prototype.test_getCount = function()
 	{
 		var oObserver1 = this.m_oObserver1.proxy();
