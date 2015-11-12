9d2421c9dbf94f2b4e529746dc6da3f2c8ae4196
thecapdan <daniel.owen@caplin.com>
2015-11-12 17:37:54 +0000

    i18n command now checks html assets #1580

--- plugins/brjs-check-i18n-plugin/src/main/java/org/bladerunnerjs/plugin/checki18n/CheckI18nCommand.java
+++ plugins/brjs-check-i18n-plugin/src/main/java/org/bladerunnerjs/plugin/checki18n/CheckI18nCommand.java
@@ -1,16 +1,32 @@
 package org.bladerunnerjs.plugin.checki18n;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
 import org.bladerunnerjs.api.App;
+import org.bladerunnerjs.api.Aspect;
+import org.bladerunnerjs.api.Asset;
 import org.bladerunnerjs.api.BRJS;
+import org.bladerunnerjs.api.BundleSet;
 import org.bladerunnerjs.api.model.exception.ConfigException;
+import org.bladerunnerjs.api.model.exception.ModelOperationException;
 import org.bladerunnerjs.api.model.exception.command.CommandArgumentsException;
 import org.bladerunnerjs.api.model.exception.command.CommandOperationException;
+import org.bladerunnerjs.api.model.exception.request.ContentProcessingException;
 import org.bladerunnerjs.api.plugin.JSAPArgsParsingCommandPlugin;
+import org.bladerunnerjs.api.plugin.Locale;
 
 import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPException;
 import com.martiansoftware.jsap.JSAPResult;
-import com.martiansoftware.jsap.Switch;
 import com.martiansoftware.jsap.UnflaggedOption;
 
 import org.bladerunnerjs.api.logging.Logger;
@@ -19,7 +35,8 @@ public class CheckI18nCommand extends JSAPArgsParsingCommandPlugin {
 
 	private BRJS brjs;
 	private Logger logger;
-	private String[] missingTokens = new String[0];
+	private List<String> missingTokens = new ArrayList<String>();
+	private final static Pattern I18N_TOKEN_PATTERN = Pattern.compile("@\\{(.*?)\\}");
 
 	@Override
 	public String getCommandName() {
@@ -35,12 +52,6 @@ public class CheckI18nCommand extends JSAPArgsParsingCommandPlugin {
 	@Override
 	public String getCommandDescription() {
 		return "Show missing i18n translations for a given application/locale.";
-	}
-
-	@Override
-	public String getCommandHelp() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
@@ -62,16 +73,60 @@ public class CheckI18nCommand extends JSAPArgsParsingCommandPlugin {
 		
 		findMissingTranslationsForAppWithLocale(app, localeToBeChecked);
 		
-		String missingTokensMessage = this.missingTokens.length == 0 ? " has no missing translations" : " has no translations defined for the following token:";
+		String missingTokensMessage = this.missingTokens.size() == 0 ? " has no missing translations" : " has no translations defined for the following token:";
 		String firstLogLine = "For the locale " + localeToBeChecked + ", " + appName + missingTokensMessage;
-		logger.println(firstLogLine);		
+		logger.println(firstLogLine);
+		for (String missingToken : missingTokens) {
+			logger.println(missingToken);
+		}
 	}
 
 	private void findMissingTranslationsForAppWithLocale(App app, String localeToBeChecked) {
-		//todo html assets
-		//todo js assets
-		//todo xml assets
+		//todo js assets... bundleset.sourceModules();
+		//todo xml
+		for(Aspect aspect : app.aspects()) {
+			//todo check tests
+			BundleSet bundleSet = null;
+			try {
+				bundleSet = aspect.getBundleSet();
+			} catch (ModelOperationException e) {
+				e.printStackTrace();
+			}			
+			
+			List<Asset> htmlAssets = new ArrayList<>();
+			htmlAssets.addAll(bundleSet.seedAssets());
+			htmlAssets.addAll(bundleSet.assets("html!"));
+			for(Asset htmlAsset : htmlAssets){
+				String htmlContent = null;
+				try {
+					htmlContent = IOUtils.toString(htmlAsset.getReader());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				checkAssetForMissingTokens(bundleSet, htmlContent, localeToBeChecked);
+			}						
+		}
+	}
+
+	//todo write a version that works for js
+	private void checkAssetForMissingTokens(BundleSet bundleSet, String content, String localeToBeChecked) {
+		Locale locale = new Locale(localeToBeChecked);
+		Map<String,String> propertiesMap = null;
+		try {
+			//todo - not retrieving properties
+			propertiesMap = I18nPropertiesUtils.getI18nProperties(bundleSet, locale);
+		} catch (ContentProcessingException e) {
+			e.printStackTrace();
+		}
+		Matcher i18nTokenMatcher = I18N_TOKEN_PATTERN.matcher(content);
 		
+		while (i18nTokenMatcher.find()) {
+			String i18nKey = i18nTokenMatcher.group(1).toLowerCase();
+			String keyReplacement = propertiesMap.get(i18nKey);
+			if (keyReplacement == null) {
+				this.missingTokens.add(i18nKey);
+			}
+		}		
 	}
 
 	private String getLocaleToBeChecked(App app, String locale) {
--- plugins/brjs-check-i18n-plugin/src/test/java/org/bladerunnerjs/plugin/checki18n/CheckI18nCommandTest.java
+++ plugins/brjs-check-i18n-plugin/src/test/java/org/bladerunnerjs/plugin/checki18n/CheckI18nCommandTest.java
@@ -1,9 +1,5 @@
 package org.bladerunnerjs.plugin.checki18n;
 
-
-import static org.mockito.Mockito.*;
-
-
 import org.bladerunnerjs.api.App;
 import org.bladerunnerjs.api.AppConf;
 import org.bladerunnerjs.api.Aspect;
@@ -18,23 +14,20 @@ import org.junit.Test;
 
 public class CheckI18nCommandTest extends SpecTest
 {
-
 	private App app;
 	private AppConf appConf;
 	private Aspect aspect;
-	private StringBuffer response = new StringBuffer();
 	private Bladeset bladeset;
 	private Blade blade;
 
 	@Before
 	public void initTestObjects() throws Exception
-	{
-		
+	{		
 		given(brjs).automaticallyFindsAllPlugins()
 			.and(brjs).hasBeenCreated();
 			app = brjs.app("app1");
-			app = brjs.app("app1");
 			appConf = app.appConf();
+			aspect = app.aspect("default");
 			bladeset = app.bladeset("bs");
 			blade = bladeset.blade("b1");
 	}	
@@ -60,4 +53,25 @@ public class CheckI18nCommandTest extends SpecTest
 		then(logging).containsConsoleText("For the locale en, app1 has no missing translations");		
 	}
 
+	@Test
+	public void testAppWithAMissingToken() throws Exception
+	{
+		given(app).hasBeenCreated()
+			.and(aspect).indexPageHasContent("<p>@{appns.bs.b1.missingtoken}</p>")
+			.and(aspect).containsResourceFileWithContents("en.properties", "appns.bs.b1.dummytoken=dummy value");	
+		when(brjs).runCommand("check-i18n", "app1");
+		then(logging).containsConsoleText("For the locale en, app1 has no translations defined for the following token:",
+										"appns.bs.b1.missingtoken");		
+	}
+	
+	@Test
+	public void testWhenHasAppHasAllTranslatedTokensLoggerShowsNoMissingTokens() throws Exception
+	{
+		given(app).hasBeenCreated()
+			.and(aspect).indexPageHasContent("<p>@{appns.bs.b1.atoken}</p>")
+			.and(aspect).containsResourceFileWithContents("en.properties", "appns.bs.b1.atoken=some value");	
+		when(brjs).runCommand("check-i18n", "app1");
+		then(logging).containsConsoleText("For the locale en, app1 has no missing translations");		
+	}	
+	
 }
