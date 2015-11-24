6dd0ebe7531c56ef4f6da1c31441318c64916f0d
thecapdan <daniel.owen@caplin.com>
2015-11-24 15:09:16 +0000

    ensuring ie8 doesnt blow up on i18n warnings when console isnt open

--- brjs-sdk/sdk/libs/javascript/br-i18n/src/br/i18n/Translator.js
+++ brjs-sdk/sdk/libs/javascript/br-i18n/src/br/i18n/Translator.js
@@ -386,6 +386,10 @@ Translator.prototype._getTranslationForKeyOrUndefined = function(token) {
 	token = token.toLowerCase();
 
 	if (!this.tokenExists(token)) {
+		if (typeof window.console === "undefined") {
+			window.console = {};
+			window.console.warn = function(){};
+		}
 		var logConsole = (window.jstestdriver) ? jstestdriver.console : window.console;
 		logConsole.warn('Unable to find a replacement for the i18n key "' + token + '"');
 	}
