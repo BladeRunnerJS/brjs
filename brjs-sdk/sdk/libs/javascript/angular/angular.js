1693ebd8c6e984ec659c50380f2e08c89a401c3f
Ryan Mills <ryanamills@gmail.com>
2016-02-23 12:58:12 +0900

    removed offending jsdoc from formatNumber

--- brjs-sdk/sdk/libs/javascript/angular/angular.js
+++ brjs-sdk/sdk/libs/javascript/angular/angular.js
@@ -19649,22 +19649,23 @@ function roundNumber(parsedNumber, fractionSize, minFrac, maxFrac) {
 /**
  * Format a number into a string
  * @param  {number} number       The number to format
- * @param  {{
- *           minFrac, // the minimum number of digits required in the fraction part of the number
- *           maxFrac, // the maximum number of digits required in the fraction part of the number
- *           gSize,   // number of digits in each group of separated digits
- *           lgSize,  // number of digits in the last group of digits before the decimal separator
- *           negPre,  // the string to go in front of a negative number (e.g. `-` or `(`))
- *           posPre,  // the string to go in front of a positive number
- *           negSuf,  // the string to go after a negative number (e.g. `)`)
- *           posSuf   // the string to go after a positive number
- *         }} pattern
+ * @param  {string } pattern
  * @param  {string} groupSep     The string to separate groups of number (e.g. `,`)
  * @param  {string} decimalSep   The string to act as the decimal separator (e.g. `.`)
  * @param  {[type]} fractionSize The size of the fractional part of the number
  * @return {string}              The number formatted as a string
  */
 function formatNumber(number, pattern, groupSep, decimalSep, fractionSize) {
+    /* where pattern is: 
+            minFrac, // the minimum number of digits required in the fraction part of the number
+           maxFrac, // the maximum number of digits required in the fraction part of the number
+           gSize,   // number of digits in each group of separated digits
+           lgSize,  // number of digits in the last group of digits before the decimal separator
+          negPre,  // the string to go in front of a negative number (e.g. `-` or `(`))
+           posPre,  // the string to go in front of a positive number
+           negSuf,  // the string to go after a negative number (e.g. `)`)
+           posSuf   // the string to go after a positive number
+           */
 
   if (!(isString(number) || isNumber(number)) || isNaN(number)) return '';
 
