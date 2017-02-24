a98970e0ab7d1bea9ea62ac6f991ada170d42e48
Brian Di Palma <offler@gmail.com>
2017-02-24 11:15:26 +0000

    fix(br-presenter): Remove usage of subrealm from JQueryDatePicker test

--- brjs-sdk/sdk/libs/javascript/br-presenter/test-unit/tests/br/presenter/control/datefield/JQueryDatePickerLocaleUtilTest.js
+++ brjs-sdk/sdk/libs/javascript/br-presenter/test-unit/tests/br/presenter/control/datefield/JQueryDatePickerLocaleUtilTest.js
@@ -2,62 +2,38 @@
     var jQuery = require("jquery");
     require('jsmockito');
 
-    var subrealm;
-    var spyI18n;
+    var JQueryDatePickerLocaleUtil = require('br/presenter/control/datefield/JQueryDatePickerLocaleUtil');
+
     var capturedDefaults;
-    var translation = 'stock translation';
-
-    var jQuery;
-    var JQueryDatePickerLocaleUtil;
-
+	var originalSetDefaults = jQuery.datepicker.setDefaults;
     var testCaseName = 'JQueryDatePickerLocaleUtilTest';
+
     var testCase = {
 		'setUp': function() {
 			JsHamcrest.Integration.JsTestDriver();
 			JsMockito.Integration.JsTestDriver();
 
-			subrealm = realm.subrealm();
-			subrealm.install();
-
-			spyI18n = mockFunction();
-			when(spyI18n)().thenReturn(translation);
-
-			define('br/I18n', function(require, exports, module) {
-				module.exports = spyI18n;
-			});
-
-			define('jquery', function(require, exports, module) {
-				module.exports = {
-					datepicker: {
-						setDefaults: function(defaults) {
-							capturedDefaults = defaults;
-						}
-					}
-				};
-			});
-            
-            jQuery = require('jquery');
-			JQueryDatePickerLocaleUtil = require('br/presenter/control/datefield/JQueryDatePickerLocaleUtil');
+			jQuery.datepicker.setDefaults = function(defaults) {
+				capturedDefaults = defaults;
+			}
 		},
 
 		'tearDown': function() {
-			subrealm.uninstall();
-			subrealm = null;
+			jQuery.datepicker.setDefaults = originalSetDefaults;
 		},
 
 		'test that JQueryDatePickerLocaleUtil.getDefaultLocales() provides valid defaults for date picker': function() {
 			jQuery.datepicker.setDefaults(JQueryDatePickerLocaleUtil.getDefaultLocales());
 
-			assertEquals('closeText should equal translation', translation, capturedDefaults.closeText);
-			assertEquals('prevText should equal translation', translation, capturedDefaults.prevText);
-			assertEquals('nextText should equal translation', translation, capturedDefaults.nextText);
-			assertEquals('currentText should equal translation', translation, capturedDefaults.currentText);
-			assertEquals('weekHeader should equal translation', translation, capturedDefaults.weekHeader);
-			assertEquals('dateFormat should equal translation', translation, capturedDefaults.dateFormat);
-			assertEquals('firstDay should equal translation', 0, capturedDefaults.firstDay);
+			assertEquals('closeText should equal translation', "Done", capturedDefaults.closeText);
+			assertEquals('prevText should equal translation', "Prev", capturedDefaults.prevText);
+			assertEquals('nextText should equal translation', "Next", capturedDefaults.nextText);
+			assertEquals('currentText should equal translation', "Today", capturedDefaults.currentText);
+			assertEquals('weekHeader should equal translation', "Wk", capturedDefaults.weekHeader);
+			assertEquals('dateFormat should equal translation', "mm/dd/yy", capturedDefaults.dateFormat);
 			assertEquals('isRTL should equal translation', false, capturedDefaults.isRTL);
 			assertEquals('showMonthAfterYear should equal translation', false, capturedDefaults.showMonthAfterYear);
-			assertEquals('yearSuffix should equal translation', translation, capturedDefaults.yearSuffix);
+			assertEquals('yearSuffix should equal translation', "", capturedDefaults.yearSuffix);
 
 			assertEquals('monthNames array length', 12, capturedDefaults.monthNames.length);
 			assertEquals('monthNamesShort array length', 12, capturedDefaults.monthNamesShort.length);
